ab011f786156748ffbcbed5b2fde9a07235b98e7
Dylan Zhang <987883389@qq.com>
2022-12-21 00:37:18 +0800

    chore: init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,14 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier',
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript'],
+  rules: {
+    semi: 'off',
+    'vue/multi-word-component-names': ['error', { ignores: ['index'] }],
+    'no-empty-function': [1],
+    '@typescript-eslint/no-empty-function': [1],
+    'no-undef': [0],
+  },
   overrides: [
     {
       files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
