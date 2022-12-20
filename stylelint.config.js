342807472fcb9ddb8565035fa73741980a9f7092
Dylan Zhang <987883389@qq.com>
2022-12-21 05:03:01 +0800

    chore: init

---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-rational-order', 'stylelint-config-prettier'],
+  defaultSeverity: 'warning',
+  plugins: ['stylelint-order'],
+  rules: {
+    'at-rule-no-unknown': [true, { ignoreAtRules: ['plugin', 'apply', 'tailwind'] }],
+    'rule-empty-line-before': ['always', { except: ['after-single-line-comment', 'first-nested'] }],
+    'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['deep'] }],
+    'color-function-notation': 'legacy',
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-env node */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
