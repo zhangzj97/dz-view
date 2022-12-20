3235eb4079e6243b88faf1a24077d1e4044f8cf3
Dylan Zhang <987883389@qq.com>
2022-12-21 05:40:24 +0800

    chore: init

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      dts: './types/auto-imports.d.ts',
+      imports: ['vue', 'vue-i18n'],
+      resolvers: [],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
