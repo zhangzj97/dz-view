51a387ac575ecd6b405fd98da7e08171a2c620bd
Dylan Zhang <987883389@qq.com>
2022-12-21 06:01:06 +0800

    chore: init

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import Components from 'unplugin-vue-components/vite';
 import AutoImport from 'unplugin-auto-import/vite';
+import { ArcoResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +16,21 @@ export default defineConfig({
       dts: './types/auto-imports.d.ts',
       imports: ['vue', 'vue-i18n'],
       resolvers: [],
+    }),
+    // Doc: https://www.npmjs.com/package/unplugin-vue-components
+    Components({
+      dts: './types/auto-imports-components.d.ts',
+      dirs: ['./src/components'],
+      extensions: ['vue'],
+      deep: true,
+      resolvers: [ArcoResolver()],
+      directoryAsNamespace: false,
+      globalNamespaces: [],
+      directives: true,
+      importPathTransform: v => v,
+      allowOverrides: false,
+      include: [/\.vue$/, /\.vue\?vue/],
+      exclude: [/[\\/]node_modules[\\/]/, /[\\/]\.git[\\/]/, /[\\/]\.nuxt[\\/]/],
     }),
   ],
   resolve: {
