548412d716c21c1bae4d260ff472c8094ebdbbe6
Dylan Zhang <987883389@qq.com>
2022-12-21 15:27:14 +0800

    chore

--- src/views/StudyApp/layouts/LayoutPage/components/LayoutRight/index.vue
+++ src/views/StudyApp/layouts/LayoutPage/components/LayoutRight/index.vue
@@ -10,6 +10,6 @@ const changeWidth = () => {
 </script>
 
 <template>
-  <DzFlex v-if="!styleState.hidden" class="flex-grow" w="384px" @click="changeWidth" />
-  <DzFlex v-else class="flex-grow" w="10px" @click="changeWidth" />
+  <DzFlex v-if="!styleState.hidden" class="flex-grow" w="384px" @click="changeWidth" testTitle="Other" />
+  <DzFlex v-else class="flex-grow" w="10px" @click="changeWidth" testTitle="Other2" />
 </template>
