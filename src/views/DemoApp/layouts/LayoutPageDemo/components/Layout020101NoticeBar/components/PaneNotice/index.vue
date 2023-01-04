3a096c29534d6fa831489027c2b6798ff8e0d4ca
Dylan Zhang <987883389@qq.com>
2023-01-04 15:01:56 +0800

    chore

--- src/views/DemoApp/layouts/LayoutPageDemo/components/Layout020101NoticeBar/components/PaneNotice/index.vue
+++ src/views/DemoApp/layouts/LayoutPageDemo/components/Layout020101NoticeBar/components/PaneNotice/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts" name="PaneNotice"></script>
 
 <template>
-  <dz-flex class="bg-green-500 cursor-pointer flex-cc h-grow">
+  <dz-flex class="items-center justify-center bg-green-500 cursor-pointer h-grow">
     <div class="text-sm italic text-white">新版本预览</div>
   </dz-flex>
 </template>
