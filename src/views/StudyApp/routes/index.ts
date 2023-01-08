e9247c4e54abf2d2cc66838e67dccf7f53a30e0f
Dylan Zhang <987883389@qq.com>
2023-01-08 13:55:42 +0800

    chore

--- src/views/StudyApp/routes/index.ts
+++ src/views/StudyApp/routes/index.ts
@@ -6,11 +6,28 @@ const fileHandler = ([key, value]: [string, any]) => {
   const routeName = `${config.ViewName}/${pageName}`;
   result.modules[routeName] = value;
   const meta = {
+    id: [config.ViewName, 'route', config.ViewName, pageName].join(config.i18nSeparator),
     code: [config.ViewName, 'route', config.ViewName, pageName].join(config.i18nSeparator),
+    url: `/${routeName}`,
+    target: 'self',
   };
   result.list.push({ name: routeName, path: `/${routeName}`, component: value?.default, meta });
 };
 const fileMap = import.meta.glob('../pages/*/index.vue', { eager: true });
 Object.entries(fileMap).forEach(fileHandler);
 
-export const route = [{ name: config.ViewName, path: `/${config.ViewName}`, redirect: `/${config.ViewName}/${config.HomePageName}`, component: () => import('../index.vue'), children: result.list }];
+export const route = [
+  {
+    name: config.ViewName,
+    path: `/${config.ViewName}`,
+    redirect: `/${config.ViewName}/${config.HomePageName}`,
+    component: () => import('../index.vue'),
+    children: result.list,
+    meta: {
+      id: [config.ViewName, 'route', config.ViewName].join(config.i18nSeparator),
+      code: [config.ViewName, 'route', config.ViewName].join(config.i18nSeparator),
+      url: `/${config.ViewName}`,
+      target: 'self',
+    },
+  },
+];
