295a4deede629b20ff13464cf4349758fb098746
Dylan Zhang <987883389@qq.com>
2023-01-08 13:52:37 +0800

    chore

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,14 @@
-const RootRoute = { name: 'Home', path: '/', redirect: '/StudyApp/Dashboard' };
+const RootRoute = {
+  name: 'Home',
+  path: '/',
+  redirect: '/StudyApp/Dashboard',
+  meta: {
+    id: 'root',
+    code: 'StudyApp.route.StudyApp',
+    url: `/`,
+    target: 'self',
+  },
+};
 
 export const config = {
   RootRoute,
