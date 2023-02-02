d650c37e066a651174ef66510db3a2dde58530fa
Dylan Zhang <987883389@qq.com>
2023-02-02 12:54:46 +0800

    feat: notice bar

--- src/views/RefArcoAdmin/layouts/LayoutPageToBArcoAdmin/components/Layout200NoticeBar/components/PaneMain/index.vue
+++ src/views/RefArcoAdmin/layouts/LayoutPageToBArcoAdmin/components/Layout200NoticeBar/components/PaneMain/index.vue
@@ -1,7 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useNoticeStyle } from './hooks/useNoticeStyle';
+const { noticeStyleState, toggleNotice } = useNoticeStyle();
+
+const noticeState = reactive({
+  title: 'Dz Admin 发布新版本, 前去查看',
+});
+</script>
+
+<script lang="ts"></script>
 
 <template>
   <dz-view one size="h-grow">
-    <div class="text-red-500">PaneMain</div>
+    <dz-view
+      row
+      :class="[
+        //
+        noticeStyleState.height,
+        noticeStyleState.opacity,
+      ]"
+      pointer
+      transition
+      position="5"
+      bg="bg-green-300"
+      overflow-hidden
+      @click="toggleNotice"
+    >
+      <dz-font>{{ noticeState.title }}</dz-font>
+    </dz-view>
   </dz-view>
 </template>
