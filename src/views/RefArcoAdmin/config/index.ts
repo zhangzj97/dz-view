050a3203d3efca6588215a6bb69f76989a7fef40
Dylan Zhang <987883389@qq.com>
2023-02-02 11:04:00 +0800

    chore: ref-arco-admin

--- src/views/RefArcoAdmin/config/index.ts
+++ src/views/RefArcoAdmin/config/index.ts
@@ -1,9 +1,11 @@
 const ViewName = 'RefAcroAdmin';
 const HomePageName = 'Main';
 const i18nSeparator = '.';
+const logoTitle = 'Acro Admin';
 
 export const config = {
   ViewName,
   HomePageName,
   i18nSeparator,
+  logoTitle,
 };
