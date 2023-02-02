d47766b8c4856c13a8bac6c97ffd1138c1ee0d22
Dylan Zhang <987883389@qq.com>
2023-02-02 20:53:41 +0800

    feat: tool bar

---
+++ src/views/RefArcoAdmin/layouts/LayoutPageToBArcoAdmin/components/Layout300AppNavBar/components/PaneToolBar/components/PaneDarkMode/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core';
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+  <dz-view one size="h-grow" position="5">
+    <a-tooltip position="bottom">
+      <template #title>{{ !isDark ? '点击进入暗色模式' : '点击进入暗色模式' }}</template>
+      <dz-icon v-if="!isDark" pointer size="6" icon="ic:round-light-mode" @click="toggleDark()" />
+      <dz-icon v-else pointer size="6" icon="ic:round-dark-mode" @click="toggleDark()" />
+    </a-tooltip>
+  </dz-view>
+</template>
