e3d599f06a55642f4dc24aac88e98f0a5385c892
Dylan Zhang <987883389@qq.com>
2023-02-02 20:53:36 +0800

    feat: tool bar

---
+++ src/views/RefArcoAdmin/layouts/LayoutPageToBArcoAdmin/components/Layout300AppNavBar/components/PaneToolBar/components/PaneFullScreen/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useFullscreen } from '@vueuse/core';
+
+const { isFullscreen, toggle } = useFullscreen();
+</script>
+
+<template>
+  <dz-view one size="h-grow" position="5">
+    <a-tooltip position="bottom">
+      <template #title>{{ !isFullscreen ? '点击进入全屏模式' : '点击退出全屏模式' }}</template>
+      <dz-icon v-if="!isFullscreen" pointer size="6" icon="ic:round-fullscreen" @click="toggle" />
+      <dz-icon v-else pointer size="6" icon="ic:round-fullscreen-exit" @click="toggle" />
+    </a-tooltip>
+  </dz-view>
+</template>
