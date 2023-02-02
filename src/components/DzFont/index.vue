537121e8183c030b7545d44bedc9cf7ac4221afd
Dylan Zhang <987883389@qq.com>
2023-02-02 11:02:20 +0800

    feat: dz-font

---
+++ src/components/DzFont/index.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const props = defineProps<{
+  xs?: boolean;
+  sm?: boolean;
+  base?: boolean;
+  lg?: boolean;
+  xl?: boolean;
+
+  left?: boolean;
+  right?: boolean;
+  center?: boolean;
+
+  size?: string;
+
+  bold?: boolean;
+
+  color?: string;
+}>();
+</script>
+
+<template>
+  <div
+    :class="[
+      //
+      'flex',
+      'relative',
+      xs && 'text-xs',
+      sm && 'text-sm',
+      base && 'text-base',
+      lg && 'text-lg',
+      xl && 'text-xl',
+
+      left && 'text-left',
+      right && 'text-right',
+      center && 'text-center',
+
+      bold && 'font-bold',
+
+      size,
+      color,
+    ]"
+  >
+    <slot></slot>
+  </div>
+</template>
