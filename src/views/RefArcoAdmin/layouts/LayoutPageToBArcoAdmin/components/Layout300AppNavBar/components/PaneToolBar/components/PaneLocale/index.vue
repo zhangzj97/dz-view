c512b89d92fe005d7c74f1cc110f6b6ed8dd518c
Dylan Zhang <987883389@qq.com>
2023-02-03 10:24:52 +0800

    chore

---
+++ src/views/RefArcoAdmin/layouts/LayoutPageToBArcoAdmin/components/Layout300AppNavBar/components/PaneToolBar/components/PaneLocale/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const languageState = reactive({
+  list: [
+    //
+    { title: '中文' },
+    { title: '英文' },
+    { title: '日文' },
+    { title: '韩文' },
+  ],
+});
+</script>
+
+<template>
+  <dz-view one size="h-grow" position="5" wrapper-class="my-2 transition-all rounded-full hover:bg-gray-100">
+    <dz-popover placement="bottom" rounded>
+      <dz-icon pointer size="6" icon="ic:round-translate" />
+      <template #content>
+        <dz-view col size="w-24" bg="bg-white">
+          <template v-for="(item, index) of languageState.list" :key="index">
+            <dz-view one pointer size="h-8" position="5" class="hover:bg-gray-100">
+              <dz-font pointer>{{ item.title }}</dz-font>
+            </dz-view>
+          </template>
+        </dz-view>
+      </template>
+    </dz-popover>
+  </dz-view>
+</template>
