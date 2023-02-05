074f8bb2a8da1a4bbfa89544a92c9e9e3327897c
Dylan Zhang <987883389@qq.com>
2023-02-05 10:40:16 +0800

    chore: navbar

--- src/views/RefArcoAdmin/layouts/LayoutPageToBArcoAdmin/components/Layout300AppNavBar/components/PaneAppNav/index.vue
+++ src/views/RefArcoAdmin/layouts/LayoutPageToBArcoAdmin/components/Layout300AppNavBar/components/PaneAppNav/index.vue
@@ -1,7 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const appState = reactive({
+  list: [
+    //
+    { title: '工单系统' },
+    { title: '用户系统' },
+    { title: '模型系统' },
+    { title: '工单系统' },
+  ],
+});
+</script>
 
 <template>
-  <dz-view one size="h-grow">
-    <div class="text-red-500">PaneAppNav</div>
+  <dz-view row size="h-grow">
+    <template v-for="(item, index) of appState.list" :key="index">
+      <dz-view pointer one size="w-20" position="5" wrapper-class="transition-all border-green-500 hover:border-b-4">
+        <dz-font bold>{{ item.title }}</dz-font>
+      </dz-view>
+    </template>
   </dz-view>
 </template>
