2468896828454ff0be209b1c386e0a0c247edb97
Dylan Zhang <987883389@qq.com>
2023-02-05 22:36:39 +0800

    fix: max width of menu

--- src/views/RefArcoAdmin/layouts/LayoutPageToBArcoAdmin/components/Layout404Left/components/PaneMenu/components/MenuAntd/index.vue
+++ src/views/RefArcoAdmin/layouts/LayoutPageToBArcoAdmin/components/Layout404Left/components/PaneMenu/components/MenuAntd/index.vue
@@ -14,17 +14,19 @@ defineProps<{
 </script>
 
 <template>
-  <a-menu mode="inline">
-    <template v-for="(item, index) of menuList" :key="index">
-      <template v-if="item.children && item.children?.length > 0">
-        <sub-menu :menuInfo="item" @handleClick="handleClick" />
+  <div class="max-w-[16rem]">
+    <a-menu mode="inline">
+      <template v-for="(item, index) of menuList" :key="index">
+        <template v-if="item.children && item.children?.length > 0">
+          <sub-menu :menuInfo="item" @handleClick="handleClick" />
+        </template>
+        <template v-else>
+          <a-menu-item :key="item.mate?.id" @click="handleClick(item)">
+            <template #icon><dz-icon :icon="String(item.meta?.icon)" /></template>
+            <div>{{ t(String(item.meta?.code)) }}</div>
+          </a-menu-item>
+        </template>
       </template>
-      <template v-else>
-        <a-menu-item :key="item.mate?.id" @click="handleClick(item)">
-          <template #icon><dz-icon :icon="String(item.meta?.icon)" /></template>
-          <div>{{ t(String(item.meta?.code)) }}</div>
-        </a-menu-item>
-      </template>
-    </template>
-  </a-menu>
+    </a-menu>
+  </div>
 </template>
