7b7f43dcf18c1af3d72b4f38d7b40f5653eabd69
Dylan Zhang <987883389@qq.com>
2023-02-05 17:27:30 +0800

    chore: bread crumb

---
+++ src/views/RefArcoAdmin/layouts/LayoutPageToBArcoAdmin/components/Layout406BreadCrumb/components/PaneBreadCrumb/components/BreadCrumb/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import BreadCrumbAntd from './components/BreadCrumbAntd/index.vue';
+
+defineProps<{
+  separator: string;
+  routeList: any[];
+}>();
+</script>
+
+<template>
+  <BreadCrumbAntd :separator="separator" :base-list="routeList">
+    <template #separator>
+      <slot name="separator"></slot>
+    </template>
+  </BreadCrumbAntd>
+</template>
--- src/views/RefArcoAdmin/layouts/LayoutPageToBArcoAdmin/components/Layout406BreadCrumb/layouts/index.ts
+++ src/views/RefArcoAdmin/layouts/LayoutPageToBArcoAdmin/components/Layout406BreadCrumb/layouts/index.ts
@@ -1,3 +1,14 @@
-import PaneMain from '../components/PaneMain/index.vue';
+import PaneBack from '../components/PaneBack/index.vue';
+import PaneTitle from '../components/PaneTitle/index.vue';
+import PaneBreadCrumb from '../components/PaneBreadCrumb/index.vue';
+import PaneToolBar from '../components/PaneToolBar/index.vue';
 
-export const layoutData = { option: { layout: 'one', size: 'h-grow' }, component: () => PaneMain };
+export const layoutData = {
+  option: { layout: 'row', size: 'h-grow' },
+  children: [
+    { option: { layout: 'one', size: 'w-8' }, component: () => PaneBack },
+    { option: { layout: 'one', size: 'w-fit' }, component: () => PaneBreadCrumb },
+    { option: { layout: 'one', size: 'w-grow' }, component: () => PaneTitle },
+    { option: { layout: 'one', size: 'w-fit' }, component: () => PaneToolBar },
+  ],
+};
