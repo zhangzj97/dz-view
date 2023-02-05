76ce91af436a424ff2a59f706e2fa32d32ae1505
Dylan Zhang <987883389@qq.com>
2023-02-05 17:27:15 +0800

    chore

--- src/views/RefArcoAdmin/layouts/LayoutPageToBArcoAdmin/components/Layout405AppNavBar/components/PaneToolBar/components/PaneLocale/components/PaneMain/index.vue
+++ src/views/RefArcoAdmin/layouts/LayoutPageToBArcoAdmin/components/Layout405AppNavBar/components/PaneToolBar/components/PaneLocale/components/PaneMain/index.vue
@@ -5,10 +5,10 @@ defineProps<{
 </script>
 
 <template>
-  <dz-view col size="w-24" bg="bg-white">
+  <dz-view col size="w-32" class="max-h-48" overflow bg="bg-white">
     <template v-for="(item, index) of baseList" :key="index">
-      <dz-view one pointer size="h-8" position="5" class="hover:bg-gray-100">
-        <dz-font pointer>{{ item.title }}</dz-font>
+      <dz-view one pointer size="h-8" position="5" class="hover:bg-gray-100 shrink-0">
+        <dz-font pointer sm>{{ item.title }}</dz-font>
       </dz-view>
     </template>
   </dz-view>
