f6c5f13926e7d575a3ed5f1e95a6f516d2f98e67
Dylan Zhang <987883389@qq.com>
2023-02-08 11:00:48 +0800

    chore: change file path

---
+++ src/views/RefArcoAdmin/pages/Common/Exception403/components/PaneMain/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const exceptionState = reactive({
+  icon: 'icon-exception-403',
+  title: '403',
+  description: '抱歉, 您无权访问此页面',
+});
+</script>
+
+<template>
+  <dz-view col size="h-grow" position="5">
+    <dz-view one size="h-96" position="5">
+      <dz-icon size="64" :icon="exceptionState.icon" />
+    </dz-view>
+    <dz-view one size="h-fit" position="5">
+      <dz-font bold class="text-6xl">{{ exceptionState.title }}</dz-font>
+    </dz-view>
+    <dz-view one size="h-20" position="5">
+      <dz-font lg>{{ exceptionState.description }}</dz-font>
+    </dz-view>
+    <dz-view row size="h-16" position="5" wrapper-class="gap-2">
+      <a-button>返回首页</a-button>
+      <a-button>刷新</a-button>
+    </dz-view>
+  </dz-view>
+</template>
