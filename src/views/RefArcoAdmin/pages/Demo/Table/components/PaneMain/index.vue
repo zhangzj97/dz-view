8afa9ded79eb516f49f13b6ce60636d5d555c4f7
Dylan Zhang <987883389@qq.com>
2023-02-08 12:21:30 +0800

    chore

---
+++ src/views/RefArcoAdmin/pages/Demo/Table/components/PaneMain/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { schemaTable } from './schemas';
+</script>
+
+<template>
+  <dz-view col size="h-grow">
+    <dz-view one>
+      <dz-font>Page Tab</dz-font>
+    </dz-view>
+
+    <dz-view one size="h-grow">
+      <dz-page-table :schemaTable="schemaTable"> </dz-page-table>
+    </dz-view>
+  </dz-view>
+</template>
---
+++ src/views/RefArcoAdmin/pages/Demo/Table/layouts/index.ts
@@ -0,0 +1,3 @@
+import PaneMain from '../components/PaneMain/index.vue';
+
+export const layoutData = { option: { layout: 'row', size: 'h-grow', overflow: true }, component: () => PaneMain };
