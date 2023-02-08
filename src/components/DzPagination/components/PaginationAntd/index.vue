184a0bdb6497b0d382485d9767dfc67b3ddcc740
Dylan Zhang <987883389@qq.com>
2023-02-08 11:10:41 +0800

    feat: dz-pagination

---
+++ src/components/DzPagination/components/PaginationAntd/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  current: number;
+  total: number;
+}>();
+
+const paginationState = reactive({
+  current: 0,
+});
+
+const emit = defineEmits<{
+  (e: 'change', page: any, pageSize: any): void;
+}>();
+
+const hanlderChange = (page: any, pageSize: any): void => {
+  emit('change', page, pageSize);
+};
+</script>
+
+<template>
+  <a-pagination v-model:current="paginationState.current" show-quick-jumper :total="total" @change="hanlderChange" />
+</template>
---
+++ src/components/DzPagination/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import PaginationAntd from './components/PaginationAntd/index.vue';
+
+defineProps<{
+  current?: any;
+  total?: any;
+}>();
+</script>
+
+<template>
+  <PaginationAntd :current="current" show-quick-jumper :total="total" />
+</template>
