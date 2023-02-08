5634fddc737cec64664ad82e9856af264bf3d40a
Dylan Zhang <987883389@qq.com>
2023-02-08 11:00:32 +0800

    chore: change file path

--- src/views/RefArcoAdmin/pages/Exception500/index.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { layoutData } from './layouts/index';
-
-const layoutState = reactive({ data: layoutData });
-</script>
-
-<template>
-  <dz-data-layout :layout-data="layoutState.data" />
-</template>
