993d18aed34c5c70a225572ca991ed9c51652444
Dylan Zhang <987883389@qq.com>
2023-02-11 09:40:38 +0800

    chore: locale

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,16 +1,12 @@
 import { createI18n } from 'vue-i18n';
 
-import enUS from './modules/common/en-US.json';
-import jaJP from './modules/common/ja-JP.json';
-import zhCN from './modules/common/zh-CN.json';
+import enUS from './modules/common/en-US';
+import jaJP from './modules/common/ja-JP';
+import zhCN from './modules/common/zh-CN';
 
-/**
- * setup vue-i18n with i18n resources with global type definition.
- * if you define the i18n resource schema in your `*.d.ts`, these is checked with typeScript.
- */
 type MessageSchema = typeof zhCN;
 
-export const i18n = createI18n<[MessageSchema], 'zh-CN' | 'en-US' | 'ja-JP'>({
+export const i18n = createI18n<[MessageSchema, any, any], 'zh-CN' | 'en-US' | 'ja-JP'>({
   legacy: false,
   locale: 'zh-CN',
   fallbackLocale: 'en-US',
