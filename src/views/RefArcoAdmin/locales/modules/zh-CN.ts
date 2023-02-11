986f0a75f05a9d406cfb905c82a3a8fe6cd45643
Dylan Zhang <987883389@qq.com>
2023-02-11 09:42:30 +0800

    chore: locale

---
+++ src/views/RefArcoAdmin/locales/modules/zh-CN.ts
@@ -0,0 +1,24 @@
+export default {
+  'RefAcroAdmin.ROUTE': 'ArcoAdmin',
+  'RefAcroAdmin.ROUTE.Main': 'ArcoAdmin',
+  'RefAcroAdmin.ROUTE.Common/Exception403': 'Exception403',
+  'RefAcroAdmin.ROUTE.Common/Exception404': 'Exception404',
+  'RefAcroAdmin.ROUTE.Common/Exception500': 'Exception500',
+  'RefAcroAdmin.ROUTE.Common/ExceptionNetwork': 'ExceptionNetwork',
+  'RefAcroAdmin.ROUTE.Common/ExceptionNoData': 'ExceptionNoData',
+  'RefAcroAdmin.ROUTE.Common/ExceptionWait': 'ExceptionWait',
+  'RefAcroAdmin.ROUTE.Demo/Form': 'Form',
+  'RefAcroAdmin.ROUTE.Demo/Table': 'Table',
+
+  'RefAcroAdmin.Demo/Form.SCHEMA.Text01': 'Text01',
+  'RefAcroAdmin.Demo/Form.SCHEMA.Text02': 'Text02',
+  'RefAcroAdmin.Demo/Form.SCHEMA.Text03': 'Text03',
+  'RefAcroAdmin.Demo/Form.SCHEMA.Text04': 'Text04',
+  'RefAcroAdmin.Demo/Form.SCHEMA.Text05': 'Text05',
+
+  'RefAcroAdmin.DICT.Demo/Table.payType': '支付方式',
+  'RefAcroAdmin.DICT.Demo/Table.payType/1': '一号支付',
+
+  'RefAcroAdmin.Demo/Table.DICT.payType': '支付方式',
+  'RefAcroAdmin.Demo/Table.DICT.payType/1': '一号支付',
+};
