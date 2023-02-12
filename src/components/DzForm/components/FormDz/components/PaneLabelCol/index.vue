66db34075629c6e1cfee32abae12b64efa2cccc6
Dylan Zhang <987883389@qq.com>
2023-02-12 10:43:04 +0800

    chore: label

--- src/components/DzForm/components/FormDz/components/PaneLabelCol/index.vue
+++ src/components/DzForm/components/FormDz/components/PaneLabelCol/index.vue
@@ -1,30 +1,48 @@
 <script setup lang="ts">
+const { t } = useI18n();
+
 defineProps<{
-  title?: string;
+  k: string;
+
+  label: string;
+  text: string | false;
+  tooltip?: string | false;
+
   visible?: boolean;
-  tooltip?: string;
+  readonly?: boolean;
 
   required?: boolean;
+
+  propsRow: any;
+  conventionOption: any;
+  customOption: any;
 }>();
 </script>
 
 <template>
-  <dz-view row size="h-grow" wrapperClass="" position="6">
-    <dz-view row size="w-fit h-12" wrapperClass="gap-1" position="5">
+  <dz-view col size="h-grow" wrapperClass="" position="6">
+    <!-- 主体内容 -->
+    <dz-view row size="h-fit">
       <template v-if="required">
-        <dz-font color="text-red-500">*</dz-font>
+        <dz-view one v-if="required" size="w-fit" position="3">
+          <dz-font color="text-red-500">*</dz-font>
+        </dz-view>
       </template>
-      <dz-font>{{ title }}</dz-font>
+
+      <dz-view one size="w-fit" position="3">
+        <dz-font>{{ t(label) }}</dz-font>
+      </dz-view>
+
+      <template v-if="tooltip">
+        <dz-view one size="w-6" position="5">
+          <dz-popover :tooltip="t(tooltip)">
+            <dz-icon size="4" icon="ic:outline-info" />
+          </dz-popover>
+        </dz-view>
+      </template>
     </dz-view>
 
-    <template v-if="tooltip">
-      <dz-view one size="w-6 h-6" position="5">
-        <dz-popover :tooltip="tooltip">
-          <dz-icon size="4" icon="ic:outline-info" />
-        </dz-popover>
-      </dz-view>
-    </template>
-
-    <dz-view one size="w-2" position="5"> </dz-view>
+    <!-- 占位 错误提示文字  -->
+    <dz-view one size="h-2"></dz-view>
   </dz-view>
 </template>
--- src/components/DzTable/components/TableVxe/components/PaneLabelRow/index.vue
+++ src/components/DzTable/components/TableVxe/components/PaneLabelRow/index.vue
@@ -1,33 +1,48 @@
 <script setup lang="ts">
+const { t } = useI18n();
+
 defineProps<{
+  k: string;
+
   label: string;
-  tooltip?: string;
+  text: string | false;
+  tooltip?: string | false;
 
   visible?: boolean;
   readonly?: boolean;
 
   required?: boolean;
-}>();
 
-const { t } = useI18n();
+  propsRow: any;
+  conventionOption: any;
+  customOption: any;
+}>();
 </script>
 
 <template>
-  <dz-view row size="h-grow" wrapperClass="" position="6">
-    <dz-view row v-if="required" size="w-fit h-12" wrapperClass="gap-1" position="5">
-      <dz-font color="text-red-500">*</dz-font>
-    </dz-view>
+  <dz-view col size="h-grow" wrapperClass="" position="6">
+    <!-- 主体内容 -->
+    <dz-view row size="h-fit">
+      <template v-if="required">
+        <dz-view one v-if="required" size="w-fit" position="3">
+          <dz-font color="text-red-500">*</dz-font>
+        </dz-view>
+      </template>
 
-    <dz-view row size="w-fit h-12" wrapperClass="gap-1" position="5">
-      <dz-font>{{ t(label) }}</dz-font>
-    </dz-view>
-
-    <template v-if="tooltip">
-      <dz-view one size="w-6 h-6" position="5">
-        <dz-popover :tooltip="t(tooltip)">
-          <dz-icon size="4" icon="ic:outline-info" />
-        </dz-popover>
+      <dz-view one size="w-fit" position="3">
+        <dz-font>{{ t(label) }}</dz-font>
       </dz-view>
-    </template>
+
+      <template v-if="tooltip">
+        <dz-view one size="w-6" position="5">
+          <dz-popover :tooltip="t(tooltip)">
+            <dz-icon size="4" icon="ic:outline-info" />
+          </dz-popover>
+        </dz-view>
+      </template>
+    </dz-view>
+
+    <!-- 占位 错误提示文字  -->
+    <dz-view one size="h-2"></dz-view>
   </dz-view>
 </template>
