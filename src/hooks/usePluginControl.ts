434b2b5ff781d371d8806b1ba84371b03822ff32
Dylan Zhang <987883389@qq.com>
2023-02-12 10:44:46 +0800

    chore

--- src/hooks/usePluginControl.ts
+++ src/hooks/usePluginControl.ts
@@ -1,15 +1,11 @@
 export interface PluginControlProps {
   k: string;
-
   label: string;
   text: string | false;
   tooltip?: string | false;
-
   visible?: boolean;
   readonly?: boolean;
-
   required?: boolean;
-
   propsRow: any;
   conventionOption: any;
   customOption: any;
