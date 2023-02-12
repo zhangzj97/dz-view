fbac80d005d567a1a015ffceee1d3bc53577a16a
Dylan Zhang <987883389@qq.com>
2023-02-12 11:03:27 +0800

    chore

--- src/components/DzForm/components/FormDz/index.vue
+++ src/components/DzForm/components/FormDz/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import PaneLabelRow from './components/PaneLabelRow/index.vue';
+import PaneLabelCol from './components/PaneLabelCol/index.vue';
 
 defineProps<{
   schema?: any[] | any;
@@ -16,35 +17,53 @@ defineProps<{
     <!-- dz-form -->
     <dz-view grid size="h-fit">
       <template v-for="(item, index) of schema" :key="index">
-        <template v-if="item.formItemOption.layout === 'row'">
-          <dz-view
-            :row="item.formItemOption.layout === 'row'"
-            :col="item.formItemOption.layout === 'col'"
-            :one="item.formItemOption.layout === 'one'"
-            :size="item.formItemOption.size"
-            wrapper-class="border-b-[0.5px] first:border-t-[0.5px]"
-          >
-            <dz-view one v-if="item.formItemOption.layout === 'row'" :size="item.labelOption.size" :position="item.labelOption.position">
-              <PaneLabelRow :label="item.label" :tooltip="item.tooltip" :required="item.required" />
-            </dz-view>
-            <dz-view one v-if="item.formItemOption.layout === 'col'" :size="item.labelOption.size" :position="item.labelOption.position">
-              <PaneLabelCol :label="item.label" :tooltip="item.tooltip" :required="item.required" />
-            </dz-view>
-            <dz-view one :size="item.controlOption.size" :position="item.controlOption.position">
-              <Component
-                :class="[
-                  //
-                ]"
-                :is="item.component()"
-                :conventionOption="item.conventionOption"
-                :customOption="item.customOption"
-                :propsRow="dataModel"
-                :key="item.key"
-                :label="item.label"
-              />
-            </dz-view>
+        <dz-view :row="item.formItemOption.layout === 'row'" :col="item.formItemOption.layout === 'col'" :one="item.formItemOption.layout === 'one'" :size="item.formItemOption.size">
+          <dz-view one v-if="item.formItemOption.layout === 'row'" :size="item.labelOption.size" :position="item.labelOption.position" class="shrink-0" wrapper-class="pr-2">
+            <PaneLabelRow
+              :class="[
+                //
+              ]"
+              :k="item.key"
+              :label="item.label"
+              :tooltip="item.tooltip"
+              :text="item.text"
+              :propsRow="dataModel"
+              :conventionOption="item.conventionOption"
+              :customOption="item.customOption"
+            />
           </dz-view>
-        </template>
+          <dz-view one v-else-if="item.formItemOption.layout === 'col'" :size="item.labelOption.size" :position="item.labelOption.position">
+            <PaneLabelCol
+              :class="[
+                //
+              ]"
+              :k="item.key"
+              :label="item.label"
+              :tooltip="item.tooltip"
+              :text="item.text"
+              :propsRow="dataModel"
+              :conventionOption="item.conventionOption"
+              :customOption="item.customOption"
+            />
+          </dz-view>
+          <dz-view one v-else-if="item.formItemOption.layout === 'one'" :size="item.labelOption.size" :position="item.labelOption.position"></dz-view>
+
+          <dz-view one :size="item.controlOption.size" :position="item.controlOption.position">
+            <Component
+              :class="[
+                //
+              ]"
+              :is="item.component()"
+              :k="item.key"
+              :label="item.label"
+              :tooltip="item.tooltip"
+              :text="item.text"
+              :propsRow="dataModel"
+              :conventionOption="item.conventionOption"
+              :customOption="item.customOption"
+            />
+          </dz-view>
+        </dz-view>
       </template>
     </dz-view>
     <!-- dz-form -->
