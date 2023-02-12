04a68e25cef7f47ee9a4631668c3e90de23f72c1
Dylan Zhang <987883389@qq.com>
2023-02-12 10:09:13 +0800

    chore: add plugin

---
+++ src/hooks/usePluginControl.ts
@@ -0,0 +1,18 @@
+export interface PluginControlProps {
+  k: string;
+
+  label: string;
+  text: string | false;
+  tooltip?: string | false;
+
+  visible?: boolean;
+  readonly?: boolean;
+
+  required?: boolean;
+
+  propsRow: any;
+  conventionOption: any;
+  customOption: any;
+}
+
+export const usePluginControl = () => {};
