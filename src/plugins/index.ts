ef2c6342ba78dae3d387f7bb4c9b3cd84b70b65d
Dylan Zhang <987883389@qq.com>
2023-02-12 10:09:22 +0800

    chore: add plugin

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,19 +1,19 @@
-const result: any = { modules: { Cell: {}, FormItem: {}, Action: {}, Header: {} }, list: [] };
+const result: any = { modules: { Cell: {}, Control: {}, Action: {}, Header: {} }, list: [] };
 const fileHandler = ([key, value]: [string, any]) => {
   const componentName = `@${key.replace(/\.\/|\/index\.vue/g, '')}`;
-  if (componentName.match(/^Cell/)) {
+  if (componentName.match(/\/Cell/)) {
     result.modules.Cell[componentName] = value.default;
-  } else if (componentName.match(/^FormItem/)) {
-    result.modules.FormItem[componentName] = value.default;
-  } else if (componentName.match(/^Action/)) {
+  } else if (componentName.match(/\/Control/)) {
+    result.modules.Control[componentName] = value.default;
+  } else if (componentName.match(/\/Action/)) {
     result.modules.Action[componentName] = value.default;
-  } else if (componentName.match(/^Header/)) {
+  } else if (componentName.match(/\/Header/)) {
     result.modules.Header[componentName] = value.default;
   }
 };
-const fileMap = import.meta.glob('./*/*/index.vue', { eager: true });
+const fileMap = import.meta.glob('./**/*/index.vue', { eager: true });
 Object.entries(fileMap).forEach(fileHandler);
 
 export const findComponent = ({ name, type, scope }) => {
-  return () => result.modules[type][`${scope}/${type}${name}`];
+  return result.modules[type][`${scope}/${type}${name}`];
 };
