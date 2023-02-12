06e720c256224e0eb617bd62d87b59c1892d8440
Dylan Zhang <987883389@qq.com>
2023-02-12 10:09:47 +0800

    feat: add dz-divider

---
+++ src/components/DzDivider/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  title?: string;
+}>();
+</script>
+
+<template>
+  <dz-view row size="h-fit w-full">
+    <dz-view one size="w-8" class="min-w-[2rem]">
+      <dz-view one size="h-1/2" wrapperClass="border-b-[1px] border-gray-300" />
+    </dz-view>
+    <dz-view one size="w-fit" wrapperClass="px-2">
+      <slot></slot>
+    </dz-view>
+    <dz-view one size="w-grow">
+      <dz-view one size="h-1/2" class="min-w-[2rem]" wrapperClass="border-b-[1px] border-gray-300" />
+    </dz-view>
+  </dz-view>
+</template>
