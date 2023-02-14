32788f5a626e39174be4eca69f4f59e0247579af
Dylan Zhang <987883389@qq.com>
2023-02-14 16:46:14 +0800

    feat: auto import

---
+++ src/services/index.ts
@@ -0,0 +1,13 @@
+import Dz from './modules/Dz';
+import Custom from './modules/Custom';
+import Static from './modules/Static';
+import Remote from './modules/Remote';
+
+export default {
+  Dz,
+  Custom,
+  Static,
+  ...Remote,
+};
+
+console.log({ Dz, Custom, Static, ...Remote });
