7f12b8604317f913b40ba3b1f9d97ce041a99c96
Dylan Zhang <987883389@qq.com>
2023-02-14 16:47:26 +0800

    feat: auto import

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,18 +1,13 @@
-import { createI18n } from 'vue-i18n';
+import Dz from './modules/Dz';
+import Custom from './modules/Custom';
+import Static from './modules/Static';
+import Remote from './modules/Remote';
 
-import enUS from './modules/common/en-US';
-import jaJP from './modules/common/ja-JP';
-import zhCN from './modules/common/zh-CN';
+export default {
+  Dz,
+  Custom,
+  Static,
+  ...Remote,
+};
 
-type MessageSchema = typeof zhCN;
-
-export const i18n = createI18n<[MessageSchema, any, any], 'zh-CN' | 'en-US' | 'ja-JP'>({
-  legacy: false,
-  locale: 'zh-CN',
-  fallbackLocale: 'zh-CN',
-  messages: {
-    'zh-CN': zhCN,
-    'en-US': enUS,
-    'ja-JP': jaJP,
-  },
-});
+console.log({ Dz, Custom, Static, ...Remote });
