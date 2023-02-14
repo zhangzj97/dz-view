1dc400c267261e168b31cf1e88bf2df46a1de0e1
Dylan Zhang <987883389@qq.com>
2023-02-14 16:47:39 +0800

    feat: auto import

---
+++ src/views/Three/config/index.ts
@@ -0,0 +1,30 @@
+const ViewName = 'Three';
+const HomePageName = 'Main';
+const i18nSeparator = '.';
+const logoTitle = 'Acro Admin';
+
+const DzAlertOption = {
+  themeOption: {
+    primary: 'green',
+    default: 'gray',
+  },
+};
+
+const DzButtonOption = {
+  themeOption: {
+    primary: 'blue',
+    default: 'gray',
+  },
+};
+
+export const config = {
+  // 当前业务配置
+  ViewName,
+  HomePageName,
+  i18nSeparator,
+  logoTitle,
+
+  // 组件配置
+  DzAlertOption,
+  DzButtonOption,
+};
