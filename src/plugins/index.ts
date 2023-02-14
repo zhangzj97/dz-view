6293fd0df2911c6fd2c0f88f379d0a43f1a97707
Dylan Zhang <987883389@qq.com>
2023-02-14 16:46:22 +0800

    feat: auto import

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,19 +1,13 @@
-const result: any = { modules: { Cell: {}, Control: {}, Action: {}, Header: {} }, list: [] };
-const fileHandler = ([key, value]: [string, any]) => {
-  const componentName = `@${key.replace(/\.\/|\/index\.vue/g, '')}`;
-  if (componentName.match(/\/Cell/)) {
-    result.modules.Cell[componentName] = value.default;
-  } else if (componentName.match(/\/Control/)) {
-    result.modules.Control[componentName] = value.default;
-  } else if (componentName.match(/\/Action/)) {
-    result.modules.Action[componentName] = value.default;
-  } else if (componentName.match(/\/Header/)) {
-    result.modules.Header[componentName] = value.default;
-  }
-};
-const fileMap = import.meta.glob('./**/*/index.vue', { eager: true });
-Object.entries(fileMap).forEach(fileHandler);
+import Dz from './modules/Dz';
+import Custom from './modules/Custom';
+import Static from './modules/Static';
+import Remote from './modules/Remote';
 
-export const findComponent = ({ name, type, scope }) => {
-  return result.modules[type][`${scope}/${type}${name}`];
+export default {
+  Dz,
+  Custom,
+  Static,
+  ...Remote,
 };
+
+console.log({ Dz, Custom, Static, ...Remote });
