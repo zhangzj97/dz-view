dbb7e35a328fe2d5f2683bbe69a0d64e6749ccd0
Dylan Zhang <987883389@qq.com>
2023-02-16 09:09:02 +0800

    chore: router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
-import { route as RemoteRoute } from './modules/Remote';
-import { route as LocalRoute } from './modules/Local';
-import { route as CustomRoute } from './modules/Custom';
 import { createRouteGuard } from './guard';
+import { useSourceRoute, useSourceRouteTool } from '@/hooks/useSourceRoute';
+import { RootRoute } from './modules/Static';
 
-export const routes: RouteRecordRaw[] = [...RemoteRoute, ...LocalRoute, ...CustomRoute];
+const { SourceRouteList } = useSourceRoute();
+const { routeWithScopeRoot } = useSourceRouteTool({ raw: SourceRouteList });
+
+export const routes: RouteRecordRaw[] = [RootRoute, ...routeWithScopeRoot];
 
 export const router = createRouter({ history: createWebHistory(import.meta.env.BASE_URL), routes });
 
 createRouteGuard(router);
-
-localStorage.setItem('routes', JSON.stringify(routes || []));
