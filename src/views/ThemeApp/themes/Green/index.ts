5d535d51a3c80b709c6a5479671c270aab78c316
Dylan Zhang <987883389@qq.com>
2023-02-17 19:17:54 +0800

    chore

--- src/views/ThemeApp/themes/Green/index.ts
+++ src/views/ThemeApp/themes/Green/index.ts
@@ -1,10 +1,49 @@
 export default {
-  // Dz组件
-  DzAlertOption: {
-    bg: 'bg-green-50',
-    border: 'border-green-300',
-    iconColor: 'text-green-400',
-    textColor: 'text-gray-800',
+  DzButtonPrimaryOption: {
+    bg: 'bg-green-400',
+    bgHover: 'hover:bg-green-300',
+    border: 'border-green-400',
+    borderHover: '',
+    iconColor: 'text-green-50',
+    textColor: 'text-green-50',
+    effectBg: 'bg-green-100',
   },
-  DzButtonOption: {},
+
+  DzButtonDangerOption: {
+    bg: 'bg-red-50',
+    bgHover: 'hover:bg-red-100',
+    border: 'border-red-200',
+    borderHover: '',
+    iconColor: 'text-red-600',
+    textColor: 'text-red-600',
+    effectBg: 'bg-red-50',
+  },
+
+  DzButtonDefaultOption: {
+    bg: 'bg-white',
+    bgHover: 'hover:bg-gray-100',
+    border: 'border-gray-200',
+    borderHover: 'hover:border-green-400',
+    iconColor: 'text-gray-800',
+    textColor: 'text-gray-800',
+    effectBg: 'bg-green-100',
+  },
+
+  DzButtonTextOption: {
+    bg: 'bg-transparent',
+    bgHover: 'hover:bg-gray-100',
+    border: 'border-transparent',
+    iconColor: 'text-gray-800',
+    textColor: 'text-gray-800',
+    effectBg: 'bg-transparent',
+  },
+
+  DzButtonLinkOption: {
+    bg: 'bg-transparent',
+    bgHover: 'hover:bg-gray-100',
+    border: 'border-transparent',
+    iconColor: 'text-gray-800',
+    textColor: 'text-gray-800',
+    effectBg: 'bg-transparent',
+  },
 };
