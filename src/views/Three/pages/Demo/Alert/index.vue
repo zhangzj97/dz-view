3ed6f5bbb2e6cc8e87dd64a0f915154dcaed909e
Dylan Zhang <987883389@qq.com>
2023-02-17 19:53:52 +0800

    chore: demo page

--- src/views/Three/pages/Demo/Alert/index.vue
+++ src/views/Three/pages/Demo/Alert/index.vue
@@ -1,26 +1,32 @@
 <script setup lang="ts"></script>
 
 <template>
-  <dz-view col size="h-grow" position="5">
-    <dz-view one size="h-fit w-full" position="5">
-      <dz-alert primary title="tile" />
-      <dz-alert danger />
-      <dz-alert />
-      <dz-alert title="tile" message="123" />
-      <dz-alert size="w-96" theme="@ThemeApp/Blue" title="中文表头" message="123" />
-      <dz-alert size="w-96" theme="@ThemeApp/Blue" title="中文表头" />
-      <dz-alert size="w-96" primary message="123" />
-      <dz-alert
-        size="w-96"
-        theme="@ThemeApp/Yellow"
-        title="Title"
-        message="123阿斯顿撒旦撒旦阿斯顿撒旦撒旦阿斯顿撒旦撒旦阿斯顿撒旦撒旦阿斯顿撒旦撒旦阿斯顿撒旦撒旦阿斯顿撒旦撒旦阿斯顿撒旦撒旦阿斯顿撒旦撒旦阿斯顿撒旦撒旦阿斯顿撒旦撒旦阿斯顿撒旦撒旦"
-      />
-    </dz-view>
+  <dz-view col size="h-grow" position="1" wrapper-class="p-4 m-4 border rounded-md shadow">
+    <dz-font>type</dz-font>
+    <dz-alert type="success" />
+    <dz-alert type="error" />
+    <dz-alert type="warn" />
+    <dz-alert type="info" />
 
-    <dz-view row size="h-16" position="5" wrapper-class="gap-2">
-      <a-button>返回首页</a-button>
-      <a-button>刷新</a-button>
-    </dz-view>
+    <dz-font>size</dz-font>
+    <dz-alert size="w-96" type="success" />
+    <dz-alert size="w-96" type="error" />
+    <dz-alert size="w-96" type="warn" />
+    <dz-alert size="w-96" type="info" />
+
+    <dz-font>title</dz-font>
+    <dz-alert size="w-96" type="success" title="短短短短" />
+    <dz-alert size="w-96" type="error" title="长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长" />
+
+    <dz-font>message</dz-font>
+    <dz-alert size="w-96" type="success" message="短短短短" />
+    <dz-alert size="w-96" type="error" message="长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长" />
+
+    <dz-font>title & message</dz-font>
+    <dz-alert size="w-96" type="success" title="短短短短" message="短短短短" />
+    <dz-alert size="w-96" type="error" title="短短短短" message="长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长" />
+
+    <dz-alert size="w-96" type="success" title="长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长" message="短短短短" />
+    <dz-alert size="w-96" type="error" title="长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长" message="长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长长" />
   </dz-view>
 </template>
