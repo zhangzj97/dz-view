4fd965f40b5c7214c6daa7a2ff282ff1538a2298
Dylan Zhang <987883389@qq.com>
2023-02-18 14:52:43 +0800

    chore

--- src/hooks/useTheme/index.ts
+++ src/hooks/useTheme/index.ts
@@ -1,54 +1,15 @@
-export const useTheme = ({ props, config, themePart }) => {
+export const useTheme = ({ config, themePart }) => {
   const SourceThemeMap = JSON.parse(localStorage.getItem('SourceThemeMap') || '');
-  const configThemeOption = config?.[themePart].themeOption;
 
-  let themeScope = configThemeOption.default;
-  if (props.primary) {
-    themeScope = configThemeOption.primary;
-  } else if (props.info) {
-    themeScope = configThemeOption.info;
-  } else if (props.danger) {
-    themeScope = configThemeOption.danger;
-  } else if (props.warn) {
-    themeScope = configThemeOption.warn;
-  } else if (props.theme) {
-    themeScope = props.theme;
-  } else if (!props.theme) {
-    themeScope = configThemeOption.default;
-  }
+  const { SourceThemeCurrentCode } = config;
 
-  const toThemeOption = ({ themeScope }) => {
-    return SourceThemeMap[themeScope].content[themePart];
+  const toThemeOption = ({ themeCode }) => {
+    return SourceThemeMap[themeCode].content[themePart];
   };
 
-  const themeOption = toThemeOption({ themeScope });
-
-  // Todo
-  const toIcon = () => {
-    const configIconOption = config?.[themePart].iconOption;
-    let icon = configIconOption.default;
-    if (props.primary) {
-      icon = configIconOption.primary;
-    } else if (props.info) {
-      icon = configIconOption.info;
-    } else if (props.danger) {
-      icon = configIconOption.danger;
-    } else if (props.warn) {
-      icon = configIconOption.warn;
-    } else if (props.icon) {
-      icon = props.icon;
-    } else if (!props.theme) {
-      icon = configIconOption.default;
-    }
-
-    return { icon };
-  };
-  // Todo
-  const { icon } = toIcon();
+  const themeOption = toThemeOption({ themeCode: SourceThemeCurrentCode });
 
   return {
     themeOption,
-
-    icon,
   };
 };
