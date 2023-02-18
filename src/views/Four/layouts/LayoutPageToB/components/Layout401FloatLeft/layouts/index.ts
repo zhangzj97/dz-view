0cffa130f1c80e661b583f6d61213e6e9ae5a73c
Dylan Zhang <987883389@qq.com>
2023-02-18 15:30:52 +0800

    chore

--- src/views/Four/layouts/LayoutPageToB/components/Layout401FloatLeft/layouts/index.ts
+++
@@ -1,3 +0,0 @@
-import PaneMain from '../components/PaneMain/index.vue';
-
-export const layoutData = [{ option: { layout: 'one', size: 'w-grow' }, component: () => PaneMain }];
--- src/views/Four/layouts/LayoutPageToB/components/Layout406BreadCrumb/layouts/index.ts
+++
@@ -1,3 +0,0 @@
-import PaneMain from '../components/PaneMain/index.vue';
-
-export const layoutData = [{ option: { layout: 'one', size: 'h-grow' }, component: () => PaneMain }];
