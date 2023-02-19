bdf25fa136fd3b99cf1f578b8882710e969fbd07
Dylan Zhang <987883389@qq.com>
2023-02-19 19:04:51 +0800

    chore: menu

--- src/views/Three/layouts/LayoutPageToBArcoAdmin/components/Layout404Left/components/PaneLogo/index.vue
+++ src/views/Three/layouts/LayoutPageToBArcoAdmin/components/Layout404Left/components/PaneLogo/index.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import { useSourceMenuStore, useSourceMenuStateStore } from '@/hooks/useSourceStore';
+import { storeToRefs } from 'pinia';
 const configState = reactive<any>(inject('config') || {});
+
+const { collapsed, mode, theme } = storeToRefs(useSourceMenuStateStore());
 </script>
 
 <template>
@@ -8,8 +12,8 @@ const configState = reactive<any>(inject('config') || {});
       <dz-icon size="10" icon="icon-logo-icon" />
     </dz-view>
 
-    <dz-view row size="w-48" position="4">
-      <dz-font xl bold>{{ configState.logoTitle }}</dz-font>
+    <dz-view transition row :size="!collapsed ? 'w-48' : 'w-0'" overflow-hidden position="4">
+      <dz-font xl bold class="whitespace-nowrap">{{ configState.logoTitle }}</dz-font>
     </dz-view>
   </dz-view>
 </template>
