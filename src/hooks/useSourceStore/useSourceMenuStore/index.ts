7fdc8112578c6888a37c74829a84f23a23a332a0
Dylan Zhang <987883389@qq.com>
2023-02-19 13:52:33 +0800

    chore: source

--- src/hooks/useSourceStore/useSourceMenuStore/index.ts
+++ src/hooks/useSourceStore/useSourceMenuStore/index.ts
@@ -12,6 +12,9 @@ export const useSourceMenuStore = defineStore('SourceMenu', () => {
   const list = computed(() => sourceState.list);
   const map = computed(() => sourceState.map);
 
+  // 初始化
+  // 只会触发一次
+  // 通常在 入口组件触发
   const initSourceImportRaw = async () => {
     sourceState.list = Object.values(SourceRaw).reduce((previousValue: any[], currentValue: any) => {
       previousValue.push(...Object.values(currentValue));
@@ -20,6 +23,9 @@ export const useSourceMenuStore = defineStore('SourceMenu', () => {
     sourceState.map = Object.fromEntries(sourceState.list.map(item => [item.id, item]));
   };
 
+  // 刷新资源
+  // 具体使用资源的地方触发
+  // 通常 由 version 响应
   const refreshSource = async payload => {
     const { data } = (await fetch('Demo/Menu.Select', payload)) as any;
 
@@ -27,11 +33,13 @@ export const useSourceMenuStore = defineStore('SourceMenu', () => {
     sourceState.map = Object.fromEntries(sourceState.list.map(item => [item.id, item]));
   };
 
+  // 强制改变资源
   const setSource = ({ list, map }) => {
     sourceState.list = list;
     sourceState.map = map;
   };
 
+  // 触发刷新资源
   const changeVersion = () => {
     sourceState.version = String(new Date().getTime());
   };
--- src/hooks/useSourceStore/useSourceRouteTabTagStore/index.ts
+++ src/hooks/useSourceStore/useSourceRouteTabTagStore/index.ts
@@ -12,28 +12,36 @@ export const useSourceRouteTabTagStore = defineStore('SourceRouteTabTag', () => 
   const list = computed(() => sourceState.list);
   const map = computed(() => sourceState.map);
 
-  const refreshSourceImportRaw = async () => {
+  // 初始化
+  // 只会触发一次
+  // 通常在 入口组件触发
+  const initSourceImportRaw = async () => {
     sourceState.list = Object.values(SourceRaw).reduce((previousValue: any[], currentValue: any) => {
       previousValue.push(...Object.values(currentValue));
       return previousValue;
     }, []);
     sourceState.map = Object.fromEntries(sourceState.list.map(item => [item.id, item]));
-
-    sourceState.version = String(new Date().getTime());
   };
 
+  // 刷新资源
+  // 具体使用资源的地方触发
+  // 通常 由 version 响应
   const refreshSource = async payload => {
     const { data } = (await fetch('Demo/Menu.Select', payload)) as any;
 
     sourceState.list = data.list;
     sourceState.map = Object.fromEntries(sourceState.list.map(item => [item.id, item]));
-
-    sourceState.version = String(new Date().getTime());
   };
 
-  const pushRouteTabTag = payload => {
-    sourceState.list.push(payload);
-    useLocalStorage('SourceRouteTabTag', sourceState.list);
+  // 强制改变资源
+  const setSource = ({ list, map }) => {
+    sourceState.list = list;
+    sourceState.map = map;
+  };
+
+  // 触发刷新资源
+  const changeVersion = () => {
+    sourceState.version = String(new Date().getTime());
   };
 
   return {
@@ -41,8 +49,10 @@ export const useSourceRouteTabTagStore = defineStore('SourceRouteTabTag', () => 
     list,
     map,
 
-    refreshSourceImportRaw,
+    initSourceImportRaw,
     refreshSource,
-    pushRouteTabTag,
+    setSource,
+
+    changeVersion,
   };
 });
