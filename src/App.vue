392a33ab6007c1e9044e5fcc48e7494cf0d9f45c
Dylan Zhang <987883389@qq.com>
2023-02-20 13:37:53 +0800

    chore

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,4 @@
-<script setup lang="ts" name="App">
-import { useSource } from '@/hooks/useSource';
-
-onBeforeMount(async () => {
-  await useSource({ strategy: 'import', sourceName: 'theme' });
-  await useSource({ strategy: 'import', sourceName: 'menu' });
-});
-</script>
+<script setup lang="ts" name="App"></script>
 
 <template>
   <RouterView />
