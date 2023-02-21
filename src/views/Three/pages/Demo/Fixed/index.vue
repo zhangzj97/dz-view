12454500b890dd1bc7b1eee36899408e8316f0d4
Dylan Zhang <987883389@qq.com>
2023-02-21 17:19:17 +0800

    chore

---
+++ src/views/Three/pages/Demo/Fixed/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <dz-view
+    col
+    size="h-grow"
+    position="1"
+    wrapper-class="p-4 m-4 border rounded-md shadow"
+  >
+    <dz-font>Fixed</dz-font>
+  </dz-view>
+</template>
