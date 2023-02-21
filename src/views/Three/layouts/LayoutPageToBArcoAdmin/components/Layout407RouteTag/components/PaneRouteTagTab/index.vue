9406ff7f7f3cfa7ef8e6a00a2f3074af26b9824e
Dylan Zhang <987883389@qq.com>
2023-02-21 17:18:10 +0800

    chore

--- src/views/Three/layouts/LayoutPageToBArcoAdmin/components/Layout407RouteTag/components/PaneRouteTagTab/index.vue
+++ src/views/Three/layouts/LayoutPageToBArcoAdmin/components/Layout407RouteTag/components/PaneRouteTagTab/index.vue
@@ -1,33 +1,94 @@
 <script setup lang="ts">
 import PaneTagTab from './components/PaneTagTab/index.vue';
 
-const routeState = reactive({
-  list: [
-    //
-    { title: '首页', fixed: true },
-    { title: '普通工单-激活', actived: true },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-  ],
+import { useRoute, useRouter } from 'vue-router';
+import {
+  useSourceRouteTabTagStore,
+  useSourceRouteTabTagStateStore,
+} from '@/hooks/useSourceStore';
+import { storeToRefs } from 'pinia';
+
+const router = useRouter();
+const route = useRoute();
+
+const { version } = storeToRefs(useSourceRouteTabTagStore());
+const { changeSelectedKeys } = useSourceRouteTabTagStateStore();
+const { selectedKeys } = storeToRefs(useSourceRouteTabTagStateStore());
+
+const refresh = () => {
+  const { getSource } = useSourceRouteTabTagStore();
+  const { list } = getSource();
+  routeState.list = list;
+};
+
+const refreshRouteTabTagKey = () => {
+  if (!route.meta.menu) return;
+  const { id } = route.meta.menu as any;
+  changeSelectedKeys({ selectedKeys: [String(id)] });
+};
+
+const routeState = reactive<any>({
+  list: [],
+  selectedKeys: [],
 });
+
+// 接收通知
+watch(version, refresh);
+watch(() => router.currentRoute.value.fullPath, refreshRouteTabTagKey);
+
+// 初始化刷新
+onMounted(async () => {
+  refresh();
+  refreshRouteTabTagKey();
+});
+
+const handleClick = (item: any) => {
+  const { route } = item;
+  router.push({ path: route });
+};
+
+const handleCloseUnactive = (item: any) => {
+  const { code } = item;
+  const { setSource, getSource, changeVersion } = useSourceRouteTabTagStore();
+  let { list } = getSource();
+
+  list = list.filter((item: any) => item.code !== code);
+  const map = Object.fromEntries(list.map((item: any) => [item.id, item]));
+
+  setSource({ list, map });
+  changeVersion();
+};
+
+const config = inject('config');
+const handleCloseActive = (item: any) => {
+  const { RouteHomePageName, ViewName } = config as any;
+  console.log(config);
+  const scope = ViewName;
+  const { code } = item;
+  const { setSource, getSource, changeVersion } = useSourceRouteTabTagStore();
+  let { list } = getSource();
+
+  list = list.filter((item: any) => item.code !== code);
+  const map = Object.fromEntries(list.map((item: any) => [item.id, item]));
+
+  setSource({ list, map });
+  changeVersion();
+
+  router.push({ name: [`@${scope}`, 'ROUTE', RouteHomePageName].join('.') });
+};
+
+const updateSelectedKeys = (item: any) => {
+  changeSelectedKeys({ selectedKeys: item });
+};
 </script>
 
 <template>
-  <dz-view one size="h-grow">
-    <PaneTagTab :base-list="routeState.list" />
-  </dz-view>
+  <PaneTagTab
+    :base-list="routeState.list"
+    :selectedKeys="selectedKeys"
+    @handleClick="handleClick"
+    @handleUpdateSelectedKeys="updateSelectedKeys"
+    @handleCloseUnactive="handleCloseUnactive"
+    @handleCloseActive="handleCloseActive"
+  />
 </template>
