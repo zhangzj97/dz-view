ba2db92cdb61e28e2d927da5c4585043d150bf08
Dylan Zhang <987883389@qq.com>
2023-02-21 17:19:41 +0800

    feat: add a route for catchAll

--- src/views/Three/sources/routes/modules/Custom/index.ts
+++ src/views/Three/sources/routes/modules/Custom/index.ts
@@ -5,8 +5,20 @@ const scopeKey = config.ViewName;
 export default [
   {
     name: scopeKey,
-    path: `/@${scopeKey}`,
+    path: '/',
     component: () => import('../../../../index.vue'),
-    children: [],
+    children: [
+      {
+        name: 'ExceptionPage404',
+        path: '/404',
+        redirct: '/404',
+        component: () =>
+          import('../../../../pages/Demo/Exception404/index.vue'),
+      },
+    ],
+  },
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('../../../../index.vue'),
   },
 ];
