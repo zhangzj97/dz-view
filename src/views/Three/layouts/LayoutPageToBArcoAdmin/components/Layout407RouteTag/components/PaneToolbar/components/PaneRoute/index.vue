7e619b798885f6abf5566ce195b35e7058dbfade
Dylan Zhang <987883389@qq.com>
2023-02-21 19:14:44 +0800

    chore

--- src/views/Three/layouts/LayoutPageToBArcoAdmin/components/Layout407RouteTag/components/PaneToolbar/components/PaneRoute/index.vue
+++ src/views/Three/layouts/LayoutPageToBArcoAdmin/components/Layout407RouteTag/components/PaneToolbar/components/PaneRoute/index.vue
@@ -1,31 +1,66 @@
 <script setup lang="ts">
-import PaneMain from './components/PaneMain/index.vue';
+import { useRouter } from 'vue-router';
+import { useSourceRouteTabTagStore } from '@/hooks/useSourceStore';
+import { storeToRefs } from 'pinia';
 
-const routeActionState = reactive({
-  list: [
-    //
-    { title: '首页', fixed: true },
-    { title: '普通工单-激活', actived: true },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-    { title: '普通工单' },
-  ],
+const router = useRouter();
+
+const { version } = storeToRefs(useSourceRouteTabTagStore());
+const refresh = () => {
+  const { getSource } = useSourceRouteTabTagStore();
+  const { list } = getSource();
+  routeState.list = list;
+};
+
+const routeState = reactive<any>({
+  list: [],
+  selectedKeys: [],
 });
+
+// 接收通知
+watch(version, refresh);
+
+// 初始化刷新
+onMounted(async () => {
+  refresh();
+});
+
+const handleClick = (item: any) => {
+  const { route } = item;
+  router.push({ path: route });
+};
+
+const { t } = useI18n();
 </script>
 
 <template>
-  <dz-view one size="h-grow" position="5" wrapper-class="border-l-[1px] rouned border-gray-300 hover:bg-gray-100">
+  <dz-view
+    one
+    size="h-grow"
+    position="5"
+    wrapper-class="border-l-[1px] rouned border-gray-300 hover:bg-gray-100"
+  >
     <dz-popover placement="bottom" rounded>
       <dz-icon pointer size="6" icon="ic:outline-file-copy" />
       <template #content>
-        <PaneMain :base-list="routeActionState.list" />
+        <!-- 菜单式路由选择 -->
+        <dz-view col size="w-32" class="max-h-48" overflow bg="bg-white">
+          <template v-for="(item, index) of routeState.list" :key="index">
+            <dz-view
+              one
+              pointer
+              size="h-8"
+              position="5"
+              class="hover:bg-gray-100 shrink-0"
+              @click="handleClick(item)"
+            >
+              <dz-font pointer sm>
+                {{ t(String(`@${item.scope}.ROUTE.${item.code}`)) }}
+              </dz-font>
+            </dz-view>
+          </template>
+        </dz-view>
+        <!-- 菜单式路由选择 -->
       </template>
     </dz-popover>
   </dz-view>
