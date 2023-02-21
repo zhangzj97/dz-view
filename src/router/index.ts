f4ddf8593b52b235263cc970e7692ea68e252680
Dylan Zhang <987883389@qq.com>
2023-02-21 09:03:26 +0800

    chore: route 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,14 +4,14 @@ import {
   type RouteRecordRaw,
 } from 'vue-router';
 import { createRouteGuard } from './guard';
-import { RootRoute } from './modules/Static';
+import { RootRoute, OtherRoute } from './modules/Static';
 
 // 注册 基本的路由 , 路由是不需要 Store 的资源
 import { useSourceRouteStore } from '@/hooks/useSourceStore';
 const { initSourceImportRaw } = useSourceRouteStore();
 const { list: routeList } = await initSourceImportRaw();
 
-export const routes: RouteRecordRaw[] = [RootRoute, ...routeList];
+export const routes: RouteRecordRaw[] = [RootRoute, ...routeList, OtherRoute];
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
