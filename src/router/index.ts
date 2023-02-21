b17bac4ffb11b460d3c4df458f8db0a9bcc35c76
Dylan Zhang <987883389@qq.com>
2023-02-21 17:15:53 +0800

    chore: remove static routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,14 +4,13 @@ import {
   type RouteRecordRaw,
 } from 'vue-router';
 import { createRouteGuard } from './guard';
-import { RootRoute, OtherRoute } from './modules/Static';
 
 // 注册 基本的路由 , 路由是不需要 Store 的资源
 import { useSourceRouteStore } from '@/hooks/useSourceStore';
 const { initSourceImportRaw } = useSourceRouteStore();
 const { list: routeList } = await initSourceImportRaw();
 
-export const routes: RouteRecordRaw[] = [RootRoute, ...routeList, OtherRoute];
+export const routes: RouteRecordRaw[] = [...routeList];
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- src/router/modules/Static/index.ts
+++ src/router/modules/Static/index.ts
@@ -1,11 +1,6 @@
 import type { RouteRecordRaw } from 'vue-router';
 
-export const RootRoute: RouteRecordRaw = {
-  path: '/',
-  redirect: '/@Three',
-};
-
 export const OtherRoute: RouteRecordRaw = {
   path: '/:catchAll(.*)*',
-  component: () => import('../../../views/Three/index.vue'),
+  component: () => import('../../../App.vue'),
 };
