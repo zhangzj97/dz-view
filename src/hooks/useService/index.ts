df7bb3c1aaf65a30f15a9047fcef4c6dd863901e
Dylan Zhang <987883389@qq.com>
2023-02-24 10:54:02 +0800

    chore: useService

--- src/hooks/useService/index.ts
+++ src/hooks/useService/index.ts
@@ -1,7 +1,18 @@
-import SourceService from '@/services';
+import { useSourceServiceStore } from '@/hooks/useSourceStore';
 
 export const useService = () => {
+  const { getSource } = useSourceServiceStore();
+
+  const dispatch = async (code, payload) => {
+    const { map } = await getSource();
+    const [moduleName, apiName] = code.split('.');
+
+    const { data } = await map[moduleName]()[apiName](payload);
+
+    return { data };
+  };
+
   return {
-    SourceService,
+    dispatch,
   };
 };
