225c0d3e0be738e7b6cee343ef5be4f7d9213cf1
Dylan Zhang <987883389@qq.com>
2023-02-24 10:59:02 +0800

    chore: api

--- src/views/Three/services/modules/Demo/apis/index.ts
+++ src/views/Three/services/modules/Demo/apis/index.ts
@@ -1,14 +1,14 @@
 export default {
-  'Demo/Form1': {
-    Select: { url: 'Demo/Form1/Select' },
-    Create: { url: 'Demo/Form1/Create' },
-    Update: { url: 'Demo/Form1/Update' },
-    Delete: { url: 'Demo/Form1/Delete' },
+  'AuUser/Form1': {
+    Select: { url: '/Demo/Form1/Select' },
+    Create: { url: '/Demo/Form1/Create' },
+    Update: { url: '/Demo/Form1/Update' },
+    Delete: { url: '/Demo/Form1/Delete' },
   },
   'Demo/Form2': {
-    Select: { url: 'Demo/Form2/Select' },
-    Create: { url: 'Demo/Form2/Create' },
-    Update: { url: 'Demo/Form2/Update' },
-    Delete: { url: 'Demo/Form2/Delete' },
+    Select: { url: '/Demo/Form2/Select' },
+    Create: { url: '/Demo/Form2/Create' },
+    Update: { url: '/Demo/Form2/Update' },
+    Delete: { url: '/Demo/Form2/Delete' },
   },
 };
