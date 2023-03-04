b0519f2e826a46005ae228f6341e353e88e2be51
Dylan Zhang <987883389@qq.com>
2023-03-04 21:21:15 +0800

    chore: `access`

---
+++ src/views/Three/sources/access/modules/Auto/index.ts
@@ -0,0 +1,61 @@
+import { useAutoImport } from '@/hooks/useAutoImport';
+import { config } from '../../../../config';
+
+const scopeKey = config.ViewName;
+
+const fileMap = import.meta.glob(['../../../../pages/**/index.vue', '!../../../../pages/**/components/**/index.vue'], { eager: true });
+
+const toKey = ({ path }) => {
+  const replaceStrategy = [
+    { search: /__.*__\//g, replace: '' },
+    { search: /\.+\//g, replace: '' },
+    { search: /pages\//, replace: '' },
+    { search: /\/index\.vue$/, replace: '' },
+    { search: /^/, replace: `@${scopeKey}/` },
+  ];
+
+  return replaceStrategy.reduce((previousValue, currentValue) => {
+    const { search, replace } = currentValue;
+    return previousValue.replace(search, replace);
+  }, path);
+};
+
+const toValue = ({ path }) => {
+  const replaceStrategy = [
+    { search: /__.*__\//g, replace: '' },
+    { search: /\.+\//g, replace: '' },
+    { search: /pages\//, replace: '' },
+    { search: /\/index\.vue$/, replace: '' },
+    { search: /^/, replace: `@${scopeKey}/` },
+  ];
+
+  const key = replaceStrategy.reduce((previousValue, currentValue) => {
+    const { search, replace } = currentValue;
+    return previousValue.replace(search, replace);
+  }, path);
+
+  const code = key.replace(/^@.*?\//, '');
+
+  return {
+    id: code,
+    code,
+    scope: scopeKey,
+
+    component: code,
+    icon: 'ic:twotone-yard',
+    redirct: `/${code}`,
+    target: '_self',
+    url: '',
+    showMenu: true,
+    parentId: scopeKey,
+    parentCode: scopeKey,
+    // Auto import 使用的上级 route code is `scopeKey`
+    path: `${scopeKey},${code}`,
+  };
+};
+
+const { result } = useAutoImport({ fileMap, toKey, toValue });
+
+export default result.modules;
+
+export { scopeKey };
