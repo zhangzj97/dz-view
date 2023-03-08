af660c8426304be67fc135ad7e72880ab42460a3
Dylan Zhang <987883389@qq.com>
2023-03-08 20:03:37 +0800

    chore

--- src/components/TForm2/components/FormItem/index.vue
+++ src/components/TForm2/components/FormItem/index.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
 defineProps<{
   code: string;
-  propsRow?: any;
-
-  version: any;
-
   component: any;
 }>();
 
@@ -12,27 +8,35 @@ const emit = defineEmits<{
   (e: 'updateValue', value: any): void;
 }>();
 
-const updateValue = value => {
+const updateValue = (value: any) => {
   emit('updateValue', value);
 };
+
+const dom = ref();
+
+const validate = () => {
+  return dom.value.validate();
+};
+
+defineExpose({ validate });
 </script>
 
 <template>
   <dz-view col size="h-fit w-grow">
-    <dz-view row size="h-fit" bg="bg-red-400">
+    <dz-view row size="h-fit" bg="bg-red-100">
       <!-- <PaneLabelCol /> -->
     </dz-view>
     <dz-view row size="h-fit">
-      <dz-view one size="w-24" class="shrink-0" bg="bg-blue-400">
+      <dz-view one size="w-24" class="shrink-0" bg="bg-blue-100">
         <!-- <PaneLabelRow /> -->
 
         {{ code }}
       </dz-view>
-      <dz-view one size="w-grow" bg="bg-pink-400">
+      <dz-view one size="w-grow" bg="bg-pink-100">
         <Component
+          ref="dom"
           :is="component()"
           :code="code"
-          :version="version"
           @updateValue="updateValue"
         />
       </dz-view>
