dc01a1454a1af94964bf7cc93f08f7f77d5efaaf
Dylan Zhang <987883389@qq.com>
2023-03-08 22:16:21 +0800

    chore

--- src/views/Three/pages/Demo/Form/index.vue
+++ src/views/Three/pages/Demo/Form/index.vue
@@ -1,45 +1,56 @@
 <script setup lang="ts">
-import { useForm } from './useForm';
+import { useForm } from '@/hooks/useForm';
 
 const schema = reactive({
-  list: [
-    { code: 'demo01', rule: [{ pattern: /@/, message: '@@' }] },
-    { code: 'demo02' },
-    { code: 'demo03' },
-    { code: 'demo04' },
-    { code: 'demo05' },
-  ],
+  config: { readonly: false, required: true, visible: true },
+  defaultValue: { demo01: '999', demo02: '' },
+  data: {
+    demo01: { component: 'Input1' },
+    demo02: { component: 'Input1' },
+    demo03: { component: 'Input1' },
+    demo04: { component: 'Input1' },
+    demo0411: { component: 'Input1' },
+  },
+
+  rule: {},
+
+  layout: {
+    demo03: {},
+  },
+
+  callback: {},
 });
 
-const { entityState, setValue, getEntity, setEntity, formDom } = useForm({
-  schema: schema.list,
-});
+const { formState, setValue, getEntity, setEntity, resetEntity, formDom } =
+  useForm({
+    moduleName: 'Demo/Form',
+    schema,
+  });
+
+console.log(formState);
 
 const validate = () => {
   formDom.value.validate();
 };
 
-const getModel = async () => {
-  console.log(await getEntity());
-};
-
-const setModel = async () => {
-  setValue('demo01', String('@999'));
-  setValue('demo02', String(Math.random()));
-};
-
-const setModel2 = async () => {
-  setValue('demo01', String('999'));
-  setValue('demo02', String(Math.random()));
-};
-
-const setModel3 = async () => {
-  setEntity({
+const t1 = async () => {
+  await setEntity({
     entity: {
-      demo01: String('999'),
-      demo02: String(Math.random()),
+      demo01: '123',
     },
   });
+};
+
+const t2 = async () => {
+  await setEntity({
+    entity: {
+      demo02: '321',
+    },
+  });
+};
+
+const t3 = () => {
+  resetEntity();
 };
 </script>
 
@@ -50,19 +61,26 @@ const setModel3 = async () => {
     position="1"
     wrapper-class="p-4 m-4 border rounded-md shadow"
   >
-    <dz-button title="validate" @click="validate"></dz-button>
-    <dz-button title="getEntity" @click="getModel"></dz-button>
-    <dz-button title="setModel" @click="setModel"></dz-button>
-    <dz-button title="setModel2" @click="setModel2"></dz-button>
-    <dz-button title="setModel3" @click="setModel3"></dz-button>
+    <dz-view grid>
+      <dz-button title="validate" @click="validate"></dz-button>
+      <dz-button title="setEntity" @click="t1"></dz-button>
+      <dz-button title="setEntity" @click="t2"></dz-button>
+      <dz-button title="resetEntity" @click="t3"></dz-button>
+    </dz-view>
     <dz-view col size="w-96" bg="bg-green-400">
       <t-form2
         ref="formDom"
         module-name="Demo/Form"
-        :schema="schema.list"
-        :entityState="entityState"
+        :formState="formState"
         @setValue="setValue"
-      ></t-form2>
+      />
+    </dz-view>
+    <dz-view row>
+      <pre></pre>
+      <pre> {{ formState.entity }}</pre>
+      <pre> {{ formState.schemaData }}</pre>
+      <pre> {{ formState.schemaProps }}</pre>
+      <pre> {{ formState.schemaVersion }}</pre>
     </dz-view>
   </dz-view>
 </template>
