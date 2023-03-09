08bb9063c7ac4e3129fd003d88b93dcb010a224a
Dylan Zhang <987883389@qq.com>
2023-03-09 19:17:39 +0800

    chore

--- src/hooks/useAutoImport/index.ts
+++ src/hooks/useAutoImport/index.ts
@@ -9,7 +9,10 @@ export const useAutoImport = ({ fileMap, toKey, toValue }) => {
         code: 100,
         title: '',
         message: '双下划线的文件夹 存在 index.vue, like: __dir__/index.vue ',
-        suggestion: ['方案一. 如果 index.vue 没有用, 请删除', '方案二. 如果 index.vue 有用, 请改变文件结构, to be: __dir__/CellDemo/index.vue '],
+        suggestion: [
+          '方案一. 如果 index.vue 没有用, 请删除',
+          '方案二. 如果 index.vue 有用, 请改变文件结构, to be: __dir__/CellDemo/index.vue ',
+        ],
       };
     }
     return {};
@@ -33,8 +36,16 @@ export const useAutoImport = ({ fileMap, toKey, toValue }) => {
   };
 
   // 获取结果
-  const toResult = ({ fileMap, toKeyCallback, toValueCallback } = { fileMap: {}, toKeyCallback: toKey, toValueCallback: toValue }) => {
-    const result: AutoImportResult = { modules: {} };
+  const toResult = (
+    { fileMap, toKeyCallback, toValueCallback } = {
+      fileMap: {},
+      toKeyCallback: toKey,
+      toValueCallback: toValue,
+    }
+  ) => {
+    const result: AutoImportResult = {
+      modules: {},
+    };
 
     Object.keys(fileMap).forEach((path: string) => {
       const file = fileMap[path];
@@ -48,7 +59,11 @@ export const useAutoImport = ({ fileMap, toKey, toValue }) => {
     return result;
   };
 
-  const result = toResult({ fileMap, toKeyCallback: toKey, toValueCallback: toValue });
+  const result = toResult({
+    fileMap,
+    toKeyCallback: toKey,
+    toValueCallback: toValue,
+  });
 
   return {
     result,
