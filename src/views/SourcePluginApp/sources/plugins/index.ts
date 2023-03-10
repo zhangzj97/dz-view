ab61097cf65ee81d0face542cc442160abeca02e
Dylan Zhang <987883389@qq.com>
2023-03-10 21:45:49 +0800

    chore

--- src/views/SourcePluginApp/sources/plugins/index.ts
+++ src/views/SourcePluginApp/sources/plugins/index.ts
@@ -3,7 +3,7 @@ import { config } from '../../config';
 const { ScopeCode } = config;
 const { toResult, toKeyWithModules } = useAutoImport();
 
-const fileMap = import.meta.glob(['./modules/*/index.ts'], {
+const fileMap = import.meta.glob(['./modules/**/*/index.vue'], {
   eager: true,
 });
 const toKey = ({ path }) => toKeyWithModules({ path, scope: ScopeCode });
