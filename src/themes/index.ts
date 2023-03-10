a4c10b7090776180576bc122c83ca0b8636a7fbd
Dylan Zhang <987883389@qq.com>
2023-03-10 10:27:39 +0800

    chore: remove

--- src/themes/index.ts
+++
@@ -1,9 +0,0 @@
-import Custom from './modules/Custom';
-import Static from './modules/Static';
-import Remote from './modules/Remote';
-
-export default {
-  Static,
-  Custom,
-  ...Remote,
-};
--- src/themes/modules/Static/Green/index.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  // Dz组件
-  DzAlertOption: {},
-  DzButtonOption: {},
-};
