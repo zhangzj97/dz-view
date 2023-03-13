103ac112fc336eceabcd9ef3045b111e0b81190d
Dylan Zhang <987883389@qq.com>
2023-03-13 22:56:36 +0800

    chore: useConfig

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,20 @@
-export const config = {};
+const SourceApiUrl = {
+  SourceAccessFind: '/Auth/FindSourceAccess',
+  SourceAccessMenuFind: '/Auth/FindSourceAccessMenu',
+  SourceAccessPermissionFind: '/Auth/FindSourceAccessPermission',
+  SourceAccessRouteFind: '/Auth/FindSourceAccessRoute',
+  SourceAccessRouteTagFind: '/Auth/FindSourceAccessRouteTag',
+  SourceApiFind: '/Auth/FindSourceApi',
+  SourceApiServiceFind: '/Auth/FindSourceApiService',
+  SourceEnumFind: '/Auth/FindSourceEnum',
+  SourceAuthFind: '/Auth/FindSourceAuth',
+  SourceAuthLogin: '/Auth/Login',
+  SourceAuthLogout: '/Auth/Logout',
+  SourcePluginFind: '/Auth/FindSourcePlugin',
+  SourceRouteFind: '/Auth/FindSourceRoute',
+  SourceThemeFind: '/Auth/FindSourceTheme',
+};
+
+export const config = {
+  SourceApiUrl,
+};
