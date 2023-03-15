6620a653660bd61c82079515f2c8739cf1b3b956
Dylan Zhang <987883389@qq.com>
2023-03-15 08:51:26 +0800

    chore

--- src/views/Three/layouts/LayoutPageToBArcoAdmin/components/Layout404Left/components/PaneMenu/index.vue
+++ src/views/Three/layouts/LayoutPageToBArcoAdmin/components/Layout404Left/components/PaneMenu/index.vue
@@ -1,52 +1,24 @@
-<script setup lang="ts" name="PaneMenu">
+<script setup lang="ts">
 import MenuAntd from './components/MenuAntd/index.vue';
-
-import { useRouter, useRoute } from 'vue-router';
 import { useTree } from '@/hooks/useTree';
-import {
-  useSourceMenuStore,
-  useSourceMenuStateStore,
-} from '@/hooks/useSourceStore';
-import { storeToRefs } from 'pinia';
+import { useService } from '@/hooks/useService';
 
+const { dispatch } = useService();
 const router = useRouter();
 const route = useRoute();
 
-const { collapsed, mode, theme, openKeys, selectedKeys } = storeToRefs(
-  useSourceMenuStateStore()
-);
-const { changeOpenKeys, changeSelectedKeys } = useSourceMenuStateStore();
+const menuState = reactive<any>({
+  SourceAccessMenuState: {
+    version: {},
+    collapsed: false,
+    mode: 'inline',
+    theme: 'light',
+    openKeys: [],
+    selectedKeys: [],
+  },
+  menuList: [],
+});
 
-const { version } = storeToRefs(useSourceMenuStore());
-const { listToTree } = useTree();
-
-const refreshMenu = () => {
-  const { getSource } = useSourceMenuStore();
-  const { list } = getSource();
-  const menuShowList = list.filter((item: any) => item.showMenu);
-  routeState.menuList = listToTree({ list: menuShowList }).filter(
-    (item: any) => {
-      return !item.parentId || item.parentId === 0 || item.parentId === '0';
-    }
-  );
-};
-
-const refreshMenuKey = () => {
-  if (!route.meta?.menu) return;
-  const { id, showMenu, path } = route.meta.menu as any;
-  const openKeys = path.replace(/,.*?$/, '').split('|');
-  if (showMenu) {
-    changeSelectedKeys({ selectedKeys: [String(id)] });
-  } else {
-    changeSelectedKeys({ selectedKeys: [] });
-  }
-  changeOpenKeys({ openKeys });
-};
-
-const routeState = reactive<any>({ menuList: [], collapsed: false });
-
-// 接收通知
-watch(version, refreshMenu);
 watch(
   () => router.currentRoute.value.path,
   () => {
@@ -54,57 +26,79 @@ watch(
   }
 );
 
+const refreshMenuKey = async () => {
+  const { id, showMenu, path } = route.meta.access as any;
+  if (!showMenu) {
+    await dispatch('SourceAccessMenu.UpdateState', { selectedKeys: [] });
+    return;
+  }
+  const openKeys = path.replace(/,.*?$/, '').split('|');
+  const selectedKeys = [String(id)];
+  await dispatch('SourceAccessMenu.UpdateState', { openKeys, selectedKeys });
+};
+
+const refreshSourceAccreeMneu = async () => {
+  const { data } = await dispatch('SourceAccessMenu.Find', {});
+  menuState.menuList = useTree().listToTree({ list: Object.values(data) });
+};
+
+const refreshSourceAccreeMneuState = async () => {
+  const { data } = await dispatch('SourceAccessMenu.State', {});
+  menuState.SourceAccessMenuState = data;
+};
+
 // 初始化刷新
-onMounted(async () => {
-  refreshMenu();
-  refreshMenuKey();
+onBeforeMount(async () => {
+  await refreshSourceAccreeMneu();
+  await refreshSourceAccreeMneuState();
 });
 
-const handleClick = (item: any) => {
-  const { route } = item;
-  router.push({ path: route });
+const updateSelectedKeys = async (value: any) => {
+  await dispatch('SourceAccessMenu.UpdateState', { selectedKeys: value });
+};
+
+const handleClick = (value: any) => {
+  const { code } = value;
+  router.push({ name: code });
 };
 
 // 根菜单 互斥
-const updateOpenKeys = (item: any) => {
-  const { getSource } = useSourceMenuStore();
-  const { list } = getSource();
-  const rootList = list
+const updateOpenKeysRootUnique = async (value: any) => {
+  // 1. 获取最后的选择
+  const lastCode = value[value.length - 1] || '';
+  // 2. 制作 Map
+  const { data } = await dispatch('SourceAccessMenu.Find', {});
+  const map = Object.fromEntries(
+    Object.values(data).map((item: any) => [item.id, item])
+  );
+  // 3. 剔除所有根级 菜单
+  const codeListWithoutRoot = value
     .filter(
       (item: any) =>
-        !item.parentId || item.parentId === '0' || item.parentId === 0
+        isDefined(map[item]?.parentId) && String(map[item]?.parentId) !== '0'
     )
     .map((item: any) => String(item.id));
-
-  const lastCode = String(item[item.length - 1]) || '';
-  if (!lastCode) {
-    changeOpenKeys({ openKeys: [] });
-  } else if (!rootList.includes(lastCode)) {
-    changeOpenKeys({ openKeys: item });
-  } else if (
-    item.find((item: any) => rootList.includes(item) && item !== lastCode)
-  ) {
-    changeOpenKeys({ openKeys: [lastCode] });
-  } else {
-    changeOpenKeys({ openKeys: item });
-  }
+  // 4. 剔除所有根级 菜单
+  const openKeys = [...codeListWithoutRoot, String(lastCode)];
+  await dispatch('SourceAccessMenu.UpdateState', { openKeys });
 };
 
-const updateSelectedKeys = (item: any) => {
-  changeSelectedKeys({ selectedKeys: item });
-};
+// const updateOpenKeys = async (value: any) => {
+//   const openKeys = value;
+//   await dispatch('SourceAccessMenu.UpdateState', { openKeys });
+// };
 </script>
 
 <template>
   <MenuAntd
-    :menuList="routeState.menuList"
-    :mode="mode"
-    :theme="theme"
-    :collapsed="collapsed"
-    :openKeys="openKeys"
-    :selectedKeys="selectedKeys"
+    :menuList="menuState.menuList"
+    :mode="menuState.SourceAccessMenuState.mode"
+    :theme="menuState.SourceAccessMenuState.theme"
+    :collapsed="menuState.SourceAccessMenuState.collapsed"
+    :openKeys="menuState.SourceAccessMenuState.openKeys"
+    :selectedKeys="menuState.SourceAccessMenuState.selectedKeys"
     @handleClick="handleClick"
-    @handleUpdateOpenKeys="updateOpenKeys"
+    @handleUpdateOpenKeys="updateOpenKeysRootUnique"
     @handleUpdateSelectedKeys="updateSelectedKeys"
   />
 </template>
--- src/views/Three/layouts/LayoutPageToBArcoAdmin/components/Layout407RouteTag/components/PaneRouteTagTab/index.vue
+++ src/views/Three/layouts/LayoutPageToBArcoAdmin/components/Layout407RouteTag/components/PaneRouteTagTab/index.vue
@@ -1,90 +1,143 @@
 <script setup lang="ts">
 import PaneTagTab from './components/PaneTagTab/index.vue';
+import { useService } from '@/hooks/useService';
+import { Item } from 'ant-design-vue/es/menu';
 
-import { useRoute, useRouter } from 'vue-router';
-import {
-  useSourceRouteTabTagStore,
-  useSourceRouteTabTagStateStore,
-} from '@/hooks/useSourceStore';
-import { storeToRefs } from 'pinia';
-
+const { dispatch } = useService();
 const router = useRouter();
 const route = useRoute();
 
-const { version } = storeToRefs(useSourceRouteTabTagStore());
-const { changeSelectedKeys } = useSourceRouteTabTagStateStore();
-const { selectedKeys } = storeToRefs(useSourceRouteTabTagStateStore());
-
-const refresh = () => {
-  const { getSource } = useSourceRouteTabTagStore();
-  const { list } = getSource();
-  routeState.list = list;
-};
-
-const refreshRouteTabTagKey = () => {
-  if (!route.meta.menu) return;
-  const { id } = route.meta.menu as any;
-  changeSelectedKeys({ selectedKeys: [String(id)] });
-};
-
-const routeState = reactive<any>({
-  list: [],
-  selectedKeys: [],
+const routeTagState = reactive<any>({
+  SourceAccessRouteTagState: {
+    version: {},
+    theme: 'light',
+    selectedKeys: [],
+    aliveList: [],
+  },
+  routeTagList: [],
 });
 
-// 接收通知
-watch(version, refresh);
-watch(() => router.currentRoute.value.fullPath, refreshRouteTabTagKey);
+watch(
+  () => router.currentRoute.value.path,
+  () => {
+    refreshAliveList();
+    refreshRouteTagKey();
+  }
+);
+
+const refreshAliveList = async () => {
+  const route: any = router.currentRoute.value;
+
+  // 1.1. 当下的 routeTag
+  const currentRouteTag = {
+    code: route.meta.access?.code,
+    scope: route.meta.access?.scope,
+    tagFixed: route.meta.access?.tagFixed,
+    route: route.path,
+    routeTagCode: route.path,
+  };
+  // 1.2. 重新获取 tagFixed 的 Tag
+  const tagFixedList = routeTagState.routeTagList
+    .filter((item: any) => item.tagFixed)
+    .map((item: any) => ({
+      code: item.code,
+      scope: item.scope,
+      tagFixed: item.tagFixed,
+      route: item.route,
+      routeTagCode: item.route,
+    }));
+  // 1.3. concat 缓存的 Tag, 剔除了 tagFixed
+  const aliveListWithoutFixed =
+    routeTagState.SourceAccessRouteTagState.aliveList
+      .filter((item: any) => !item.tagFixed)
+      .map((item: any) => ({ ...item }));
+  // 1.4. 整合 并去重
+  const routeTagList = Object.values(
+    Object.fromEntries(
+      [...tagFixedList, ...aliveListWithoutFixed, currentRouteTag].map(item => [
+        item.routeTagCode,
+        item,
+      ])
+    )
+  );
+  console.log(routeTagList);
+  // 2.1. 获取权限
+  const { data: AccessPermissionData } = await dispatch(
+    'SourceAccessPermission.Find',
+    { remote: false }
+  );
+  // 2.2. 剔除无权限 Tag
+  const aliveList = routeTagList.filter(
+    (item: any) => AccessPermissionData[item.code]
+  );
+
+  // 3 改变 State
+  await dispatch('SourceAccessRouteTag.UpdateState', {
+    aliveList,
+    selectedKeys: [currentRouteTag.routeTagCode],
+  });
+};
+
+const refreshRouteTagKey = () => {
+  const { id, showMenu, path } = route.meta.access as any;
+};
+
+const refreshSourceAccreeRouteTag = async () => {
+  const { data } = await dispatch('SourceAccessRouteTag.Find', {});
+  routeTagState.routeTagList = Object.values(data);
+};
+
+const refreshSourceAccreeRouteTagState = async () => {
+  const { data } = await dispatch('SourceAccessRouteTag.State', {});
+  routeTagState.SourceAccessRouteTagState = data;
+};
 
 // 初始化刷新
-onMounted(async () => {
-  refresh();
-  refreshRouteTabTagKey();
+onBeforeMount(async () => {
+  await refreshSourceAccreeRouteTag();
+  await refreshSourceAccreeRouteTagState();
+  await refreshAliveList();
 });
 
-const handleClick = (item: any) => {
-  const { route } = item;
+const handleClick = (value: any) => {
+  const { route } = value;
   router.push({ path: route });
 };
 
-const handleCloseUnactive = (item: any) => {
-  const { code } = item;
-  const { setSource, getSource, changeVersion } = useSourceRouteTabTagStore();
-  let { list } = getSource();
-
-  list = list.filter((item: any) => item.code !== code);
-  const map = Object.fromEntries(list.map((item: any) => [item.id, item]));
-
-  setSource({ list, map });
-  changeVersion();
+const updateSelectedKeys = async (value: any) => {
+  await dispatch('SourceAccessRouteTag.UpdateState', { selectedKeys: value });
 };
 
-const config = inject('config');
-const handleCloseActive = (item: any) => {
-  const { RouteHomePageName, ViewName } = config as any;
-  const scope = ViewName;
-  const { code } = item;
-  const { setSource, getSource, changeVersion } = useSourceRouteTabTagStore();
-  let { list } = getSource();
-
-  list = list.filter((item: any) => item.code !== code);
-  const map = Object.fromEntries(list.map((item: any) => [item.id, item]));
-
-  setSource({ list, map });
-  changeVersion();
-
-  router.push({ name: [`@${scope}`, 'ROUTE', RouteHomePageName].join('.') });
+const handleCloseUnactive = async (value: any) => {
+  console.log(value);
+  const { routeTagCode } = value;
+  const aliveList = routeTagState.SourceAccessRouteTagState.aliveList.filter(
+    (item: any) => item.routeTagCode !== routeTagCode
+  );
+  await dispatch('SourceAccessRouteTag.UpdateState', { aliveList });
 };
 
-const updateSelectedKeys = (item: any) => {
-  changeSelectedKeys({ selectedKeys: item });
+const handleCloseActive = async (value: any) => {
+  console.log(value);
+  const { routeTagCode } = value;
+  const aliveList = routeTagState.SourceAccessRouteTagState.aliveList.filter(
+    (item: any) => item.routeTagCode !== routeTagCode
+  );
+  await dispatch('SourceAccessRouteTag.UpdateState', { aliveList });
+
+  const redirct = routeTagState.SourceAccessRouteTagState.aliveList[0];
+  if (redirct) {
+    router.push({ name: redirct.code });
+  } else {
+    router.push({ name: 'Router' });
+  }
 };
 </script>
 
 <template>
   <PaneTagTab
-    :base-list="routeState.list"
-    :selectedKeys="selectedKeys"
+    :base-list="routeTagState.SourceAccessRouteTagState.aliveList"
+    :selectedKeys="routeTagState.SourceAccessRouteTagState.selectedKeys"
     @handleClick="handleClick"
     @handleUpdateSelectedKeys="updateSelectedKeys"
     @handleCloseUnactive="handleCloseUnactive"
