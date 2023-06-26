0f4addbf64c5f1ae4aaa2c2005960dc7198d56da
Dylan Zhang <987883389@qq.com>
2023-06-26 22:55:34 +0800

    chore

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import { i18n } from './locale';
 import { router } from './router';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(i18n);
 
 app.mount('#App');
