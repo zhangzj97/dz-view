eaecb9a6dca9727c0ec35a1638386ae7ddf36042
Dylan Zhang <987883389@qq.com>
2023-06-26 10:58:33 +0800

    chore

--- types/unplugin-vue-components.d.ts
+++ types/unplugin-vue-components.d.ts
@@ -33,8 +33,6 @@ declare module '@vue/runtime-core' {
     DzDataLayout: typeof import('./../src/components/DzDataLayout/index.vue')['default']
     DzDivider: typeof import('./../src/components/DzDivider/index.vue')['default']
     DzFont: typeof import('./../src/components/DzFont/index.vue')['default']
-    DzForm: typeof import('./../src/components/DzForm/index.vue')['default']
-    DzFormItem: typeof import('./../src/components/DzFormItem/index.vue')['default']
     DzIcon: typeof import('./../src/components/DzIcon/index.vue')['default']
     DzModal: typeof import('./../src/components/DzModal/index.vue')['default']
     DzPageTable: typeof import('./../src/components/DzPageTable/index.vue')['default']
@@ -42,11 +40,10 @@ declare module '@vue/runtime-core' {
     DzPopover: typeof import('./../src/components/DzPopover/index.vue')['default']
     DzTable: typeof import('./../src/components/DzTable/index.vue')['default']
     DzView: typeof import('./../src/components/DzView/index.vue')['default']
-    ElDialog: typeof import('element-plus/es')['ElDialog']
-    ElDrawer: typeof import('element-plus/es')['ElDrawer']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TForm: typeof import('./../src/components/TForm/index.vue')['default']
     TForm2: typeof import('./../src/components/TForm2/index.vue')['default']
+    V: typeof import('./../src/components/V/index.vue')['default']
   }
 }
