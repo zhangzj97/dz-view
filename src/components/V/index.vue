c498b7139b8961377430065987eb5da5bd22b3db
Dylan Zhang <987883389@qq.com>
2023-06-26 14:37:51 +0800

    chore: bg-stripes

--- src/components/V/index.vue
+++ src/components/V/index.vue
@@ -39,7 +39,17 @@ defineProps<{
     <slot>
       <div
         v-if="desc"
-        class="flex items-center justify-center flex-grow overflow-auto bg-stripes-cyan scrollbar-hidden"
+        class="flex items-center justify-center flex-grow overflow-auto scrollbar-hidden"
+        :class="[
+          [
+            'bg-stripes-cyan',
+            'bg-stripes-sky',
+            'bg-stripes-blue',
+            'bg-stripes-purple',
+            'bg-stripes-violet',
+            'bg-stripes-pink',
+          ][Math.floor(Math.random() * 5)],
+        ]"
       >
         <div class="flex text-lg italic font-bold text-gray-600">
           {{ desc }}
