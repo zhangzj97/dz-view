a206244b26412235bbbfa7570d94298e25861d74
Dylan Zhang <987883389@qq.com>
2023-06-27 14:53:06 +0800

    chore

--- src/components/ConsoleRouter/components/index.ts
+++
@@ -1,5 +0,0 @@
-import PaneListAll from './PaneListAll/index.vue';
-import PaneListSearch from './PaneListSearch/index.vue';
-import PaneSearch from './PaneSearch/index.vue';
-
-export { PaneListAll, PaneListSearch, PaneSearch };
