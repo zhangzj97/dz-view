5f283367cf06a581a9d3b58c3019a32a3644bc6d
Dylan Zhang <987883389@qq.com>
2023-06-27 22:19:25 +0800

    chore

---
+++ src/apps/BaseLayout/common/components/LayoutUserProfile/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v s="w-48 h-16" hover="hover:bg-gray-100" pointer>
+    <dz-icon
+      s="w-16 h-16"
+      class="scale-50 rounded-full"
+      src="//iconfont.alicdn.com/p/illus/preview_image/lwvMdbm1khWH/63a475cc-2ad6-4048-a268-dea0a41d9517.png"
+    />
+    <v s="w-32 h-16" col>
+      <v s="w-32 h-grow" space></v>
+      <dz-text t="text-sm font-bold">管理员管理员</dz-text>
+      <dz-text t="text-xs text-green-600">在线</dz-text>
+      <v s="w-32 h-grow" space></v>
+    </v>
+  </v>
+</template>
