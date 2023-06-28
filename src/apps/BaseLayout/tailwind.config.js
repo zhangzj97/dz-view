180ee127bf559fc5e8724fb3ef04220e0350b67c
Dylan Zhang <987883389@qq.com>
2023-06-28 21:16:33 +0800

    chore

---
+++ src/apps/BaseLayout/tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-env node */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html', './**/*.{js,jsx,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
