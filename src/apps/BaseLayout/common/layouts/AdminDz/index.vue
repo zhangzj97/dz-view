0dc09dd0cfa71bb5e64982ffa86a9a9df11a72d9
Dylan Zhang <987883389@qq.com>
2023-06-29 12:22:36 +0800

    chore

--- src/apps/BaseLayout/common/layouts/AdminDz/index.vue
+++ src/apps/BaseLayout/common/layouts/AdminDz/index.vue
@@ -1,13 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LayoutLogo from '@/apps/BaseLayout/common/components/LayoutLogo/index.vue';
+import LayoutMenu from '@/apps/BaseLayout/common/components/LayoutMenu/index.vue';
+import LayoutUserProfile from '@/apps/BaseLayout/common/components/LayoutUserProfile/index.vue';
+import LayoutRouteBreadCrumb from '@/apps/BaseLayout/common/components/LayoutRouteBreadCrumb/index.vue';
+</script>
 
 <template>
   <v col s="w-[100vw] h-[100vh]">
     <v s="w-grow h-16" desc="navbar">
-      <v s="w-64 h-16" desc="logo">
-        <v s="w-16 h-16" desc="logo-pic"></v>
-        <v s="w-48 h-16" desc="logo-name"></v>
-      </v>
-      <v s="w-grow h-16" desc="space"></v>
+      <LayoutLogo />
+      <v s="w-grow h-16" desc="other"></v>
       <v s="w-fit h-16" desc="plugin">
         <v s="w-12 h-16" desc="plugin-search"></v>
         <v s="w-12 h-16" desc="plugin-locale"></v>
@@ -17,15 +19,15 @@
         <v s="w-12 h-16" desc="plugin-setting"></v>
         <v s="w-12 h-16" desc="plugin-theme"></v>
       </v>
-      <v s="w-16 h-16" desc="avatar"></v>
+      <LayoutUserProfile />
     </v>
     <v s="w-grow h-grow" desc="body">
-      <v col s="w-64 h-grow" desc="sidebar">
-        <v s="w-64 h-grow" desc="menu"></v>
-        <v s="w-64 h-16" desc="menutool"></v>
+      <v col s="w-fit h-grow" desc="sidebar">
+        <LayoutLogo />
+        <LayoutMenu class="z-50" />
       </v>
       <v col s="w-grow h-grow" desc="body">
-        <v s="w-grow h-16" desc="breadcrumb"></v>
+        <LayoutRouteBreadCrumb />
         <v s="w-grow h-grow" w="overflow-auto" desc="body">
           <slot></slot>
         </v>
