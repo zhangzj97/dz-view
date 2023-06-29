9951fd9ef09dc5c5d838544782c4e237be21262d
Dylan Zhang <987883389@qq.com>
2023-06-29 12:22:24 +0800

    chore

--- src/apps/BaseLayout/common/components/LayoutLogo/index.vue
+++ src/apps/BaseLayout/common/components/LayoutLogo/index.vue
@@ -1,29 +1,30 @@
 <script setup lang="ts">
 const config: any = inject('config');
+// TODO 使用状态机 来处理 logo的收缩
 </script>
 
 <template>
-  <v s="w-64 h-16" class="bg-gray-700">
+  <v s="w-fit h-16" class="transition-all bg-gray-700" w="overflow-hidden">
     <dz-icon
       s="w-16 h-16"
       :src="config.layout.logo.icon1"
       :class="[config.layout.logo.icon1Style]"
     />
-    <v s="w-grow h-16">
+    <v s="w-48 h-16">
       <dz-icon
         v-if="config.layout.logo.icon2"
-        s="w-fit h-16"
+        s="w-16 h-16"
         :src="config.layout.logo.icon2"
         :class="[config.layout.logo.icon2Style]"
       />
       <v v-else s="w-grow h-16">
-        <v s="w-grow h-grow" space />
+        <v s="w-grow h-16" space />
         <dz-text
           :t="config.layout.logo.textStyle"
           :text="config.layout.logo.text"
         />
-        <v s="w-grow h-grow" space />
-        <v s="w-grow h-grow" space />
+        <v s="w-grow h-16" space />
+        <v s="w-grow h-16" space />
       </v>
     </v>
   </v>
