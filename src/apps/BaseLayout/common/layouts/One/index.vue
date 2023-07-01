7649cfe4b0ccfacd7f5dfb13bb59416df9c0299c
Dylan Zhang <987883389@qq.com>
2023-07-01 12:33:28 +0800

    chore

--- src/apps/BaseLayout/common/layouts/One/index.vue
+++ src/apps/BaseLayout/common/layouts/One/index.vue
@@ -2,7 +2,7 @@
 
 <template>
   <v col s="w-[100vw] h-[100vh]">
-    <v s="w-grow h-grow" w="overflow-auto" desc="body">
+    <v s="w-grow h-grow" class="bg-red-500" w="overflow-auto" desc="body">
       <slot></slot>
     </v>
   </v>
