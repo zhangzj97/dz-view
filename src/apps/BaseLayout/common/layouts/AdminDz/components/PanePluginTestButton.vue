ba685b5b64c7ce8465ac6196e4e7ebb69417f4bd
Dylan Zhang <987883389@qq.com>
2023-07-01 19:13:35 +0800

    chore

---
+++ src/apps/BaseLayout/common/layouts/AdminDz/components/PanePluginTestButton.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+const drawerState = reactive<any>({
+  visible: false,
+  fullscreen: false,
+});
+
+const toggleVisible = () => {
+  drawerState.visible = !drawerState.visible;
+};
+</script>
+
+<template>
+  <v
+    s="w-12 h-16"
+    desc="plugin-modal"
+    hover="hover:bg-gray-100"
+    pointer
+    @click="toggleVisible"
+  >
+    <dz-icon s="w-12 h-12" icon="ic:round-desk" class="scale-50" />
+  </v>
+
+  <dz-drawer
+    v-model:state="drawerState"
+    position="top"
+    s="w-[80vw] h-[10vh]"
+    w="max-w-[60vw] min-h-[30vh]"
+    col
+    title="988"
+  >
+    <v s="w-fit h-fit" w="gap-2">
+      <dz-btn title="123" />
+      <dz-btn icon="mdi:ab-testing" title="123" />
+      <dz-btn status="warning" icon="mdi:ab-testing" title="warning" />
+      <dz-btn status="success" icon="mdi:ab-testing" title="success" />
+      <dz-btn status="danger" icon="mdi:ab-testing" title="danger" />
+      <dz-btn
+        type="primary"
+        status="danger"
+        icon="mdi:ab-testing"
+        title="primary"
+      />
+      <dz-btn
+        type="secondary"
+        status="danger"
+        icon="mdi:ab-testing"
+        title="secondary"
+      />
+      <dz-btn
+        type="outline"
+        status="danger"
+        icon="mdi:ab-testing"
+        title="outline"
+      />
+      <dz-btn type="text" status="danger" icon="mdi:ab-testing" title="text" />
+      <dz-btn
+        type="text"
+        :state="{ disabled: true }"
+        status="danger"
+        icon="mdi:ab-testing"
+        title="disabled"
+      />
+
+      <dz-btn
+        type="text"
+        :state="{ loading: true }"
+        status="danger"
+        icon="mdi:ab-testing"
+        title="loading"
+      />
+    </v>
+
+    <template #footer>
+      <v s="w-grow h-10" w="gap-2">
+        <v s="w-grow h-10" space />
+        <dz-btn text="确定" @click="toggleVisible" />
+        <dz-btn text="取消" @click="toggleVisible" />
+        <v s="w-2 h-10" space />
+      </v>
+    </template>
+  </dz-drawer>
+</template>
