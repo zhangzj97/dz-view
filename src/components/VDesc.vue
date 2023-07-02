ec73408639e95c08def3d10f0b052ddc691494f6
Dylan Zhang <987883389@qq.com>
2023-07-02 18:23:26 +0800

    chore

---
+++ src/components/VDesc.vue
@@ -0,0 +1,145 @@
+<script setup lang="ts">
+interface DzBaseProps {
+  s?: string;
+  w?: string;
+  t?: string;
+  hover?: string;
+}
+
+interface DzViewTextProps {
+  text?: string;
+}
+
+interface DzEntityProps {
+  id?: string;
+  icon?: string;
+  avatar?: string;
+  title?: string;
+  bg?: string;
+}
+
+interface DzViewFlexProps {
+  row?: boolean;
+  col?: boolean;
+
+  grid?: boolean;
+}
+
+interface DzViewPositionProps {
+  absolute?:
+    | 'top'
+    | 'bottom'
+    | 'left'
+    | 'right'
+    | 'tl'
+    | 'tr'
+    | 'bl'
+    | 'br'
+    | string;
+  fixed?: string;
+}
+
+interface DzViewCursorProps {
+  pointer?: boolean;
+}
+
+interface DzViewSpaceProps {
+  space?: boolean;
+}
+
+interface DzViewTestProps {
+  desc?: string;
+}
+
+withDefaults(
+  defineProps<
+    DzBaseProps &
+      DzViewFlexProps &
+      DzViewPositionProps &
+      DzViewCursorProps &
+      DzViewSpaceProps &
+      DzViewTestProps
+  >(),
+  {
+    s: 'w-grow h-grow',
+  }
+);
+</script>
+
+<template>
+  <div
+    class="dz-view dz-view-space v202301"
+    :class="[
+      s,
+      w,
+      t,
+      hover,
+      col ? 'dz-view-col' : 'dz-view-row',
+      grid ? 'flex-nowrap' : '',
+      absolute && `dz-view-absolute-${absolute}`,
+      pointer && 'cursor-pointer',
+    ]"
+  >
+    <slot>
+      <div
+        class="flex items-center justify-center flex-grow overflow-auto h-grow w-grow dz-view scrollbar-hidden"
+        :class="[
+          [
+            'bg-stripes-cyan',
+            'bg-stripes-sky',
+            'bg-stripes-blue',
+            'bg-stripes-purple',
+            'bg-stripes-violet',
+            'bg-stripes-pink',
+          ][Math.floor(Math.random() * 5)],
+        ]"
+      >
+        <div class="flex text-lg italic font-bold text-gray-600">
+          {{ desc }}
+        </div>
+      </div>
+    </slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.dz-view.v202301 {
+  position: relative;
+  display: flex;
+  width: auto;
+  height: auto;
+  flex-direction: row;
+  flex-grow: 0;
+  flex-shrink: 0;
+  flex-wrap: nowrap;
+  cursor: auto;
+  user-select: none;
+
+  & > .dz-view-row {
+    flex-direction: row;
+    align-items: center;
+
+    & > .w-grow {
+      width: 0px;
+      flex-grow: 1;
+    }
+    & > .h-grow {
+      height: auto;
+      align-self: stretch;
+    }
+  }
+  & > .dz-view-col {
+    flex-direction: column;
+    align-items: center;
+
+    & > .w-grow {
+      width: auto;
+      align-self: stretch;
+    }
+    & > .h-grow {
+      height: 0px;
+      flex-grow: 1;
+    }
+  }
+}
+</style>
---
+++ src/components/VText.vue
@@ -0,0 +1,128 @@
+<script setup lang="ts">
+interface DzBaseProps {
+  s?: string;
+  w?: string;
+  t?: string;
+  hover?: string;
+}
+
+interface DzViewTextProps {
+  text?: string;
+}
+
+interface DzEntityProps {
+  id?: string;
+  icon?: string;
+  avatar?: string;
+  title?: string;
+  bg?: string;
+}
+
+interface DzViewFlexProps {
+  row?: boolean;
+  col?: boolean;
+
+  grid?: boolean;
+}
+
+interface DzViewPositionProps {
+  absolute?:
+    | 'top'
+    | 'bottom'
+    | 'left'
+    | 'right'
+    | 'tl'
+    | 'tr'
+    | 'bl'
+    | 'br'
+    | string;
+  fixed?: string;
+}
+
+interface DzViewCursorProps {
+  pointer?: boolean;
+}
+
+interface DzViewSpaceProps {
+  space?: boolean;
+}
+
+interface DzViewTestProps {
+  desc?: string;
+}
+
+withDefaults(
+  defineProps<
+    DzBaseProps &
+      DzViewFlexProps &
+      DzViewPositionProps &
+      DzViewCursorProps &
+      DzViewSpaceProps &
+      DzViewTextProps &
+      DzViewTestProps
+  >(),
+  {
+    s: 'w-fit h-fit',
+  }
+);
+</script>
+
+<template>
+  <div
+    class="dz-view dz-view-text v202301"
+    :class="[
+      s,
+      w,
+      t,
+      hover,
+      col ? 'dz-view-col' : 'dz-view-row',
+      grid ? 'flex-nowrap' : '',
+      absolute && `dz-view-absolute-${absolute}`,
+      pointer && 'cursor-pointer',
+    ]"
+  >
+    <slot>{{ text }}</slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.dz-view.v202301 {
+  position: relative;
+  display: flex;
+  width: auto;
+  height: auto;
+  flex-direction: row;
+  flex-grow: 0;
+  flex-shrink: 0;
+  flex-wrap: nowrap;
+  cursor: auto;
+  user-select: none;
+
+  & > .dz-view-row {
+    flex-direction: row;
+    align-items: center;
+
+    & > .w-grow {
+      width: 0px;
+      flex-grow: 1;
+    }
+    & > .h-grow {
+      height: auto;
+      align-self: stretch;
+    }
+  }
+  & > .dz-view-col {
+    flex-direction: column;
+    align-items: center;
+
+    & > .w-grow {
+      width: auto;
+      align-self: stretch;
+    }
+    & > .h-grow {
+      height: 0px;
+      flex-grow: 1;
+    }
+  }
+}
+</style>
