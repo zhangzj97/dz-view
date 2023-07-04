aefef88e5f5c57b4f63d0fab5acfa78ee6588e86
Dylan Zhang <987883389@qq.com>
2023-07-04 10:15:36 +0800

    feat: useTrigger

---
+++ src/hooks/useTrigger/index.ts
@@ -0,0 +1,4 @@
+export { useDrawer } from './useDrawer';
+export { useModal } from './useModal';
+export { usePopover } from './usePopover';
+export { useTrigger } from './useTrigger';
---
+++ src/hooks/useTrigger/useTrigger/index.ts
@@ -0,0 +1,48 @@
+export const useTrigger = () => {
+  const state = reactive({
+    visible: false,
+    fullscreen: false,
+    loading: false,
+    disabled: false,
+  });
+
+  const setVisible = value => {
+    state.visible = value;
+  };
+  const setFullscreen = value => {
+    state.fullscreen = value;
+  };
+  const setLoading = value => {
+    state.loading = value;
+  };
+  const setDisabled = value => {
+    state.disabled = value;
+  };
+
+  const toggleVisible = () => {
+    state.visible = !state.visible;
+  };
+  const toggleFullscreen = () => {
+    state.fullscreen = !state.fullscreen;
+  };
+  const toggleLoading = () => {
+    state.loading = !state.loading;
+  };
+  const toggleDisabled = () => {
+    state.disabled = !state.disabled;
+  };
+
+  return {
+    state,
+
+    setVisible,
+    setFullscreen,
+    setLoading,
+    setDisabled,
+
+    toggleVisible,
+    toggleFullscreen,
+    toggleLoading,
+    toggleDisabled,
+  };
+};
