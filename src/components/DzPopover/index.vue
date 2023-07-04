2d2ca0c91310c1076f99506a24d24f5d4df74e14
Dylan Zhang <987883389@qq.com>
2023-07-05 07:52:04 +0800

    refactor: 修改 dz-popover style

--- src/components/DzPopover/index.vue
+++ src/components/DzPopover/index.vue
@@ -3,81 +3,106 @@ import { Popover, Tooltip } from '@arco-design/web-vue';
 import '@arco-design/web-vue/es/popover/style/css';
 import '@arco-design/web-vue/es/tooltip/style/css';
 
-const props = defineProps<{
-  // Size Css
-  // size: string;
+interface DzBaseProps {
   s?: string;
-  // Wrap Css
   w?: string;
-  // Text Css
   t?: string;
+  trans?: boolean | string;
+}
 
-  // Text
+interface DzViewTextProps {
   text?: string;
-  title?: string;
-  icon?: string;
+}
 
-  // Flex Css
-  // row?: boolean;
+interface DzEntityProps {
+  id?: string;
+  icon?: string;
+  avatar?: string;
+  title?: string;
+  bg?: string;
+}
+
+interface DzViewFlexProps {
+  row?: boolean;
   col?: boolean;
 
-  // state
+  grid?: boolean;
+}
+
+interface DzViewPositionProps {
+  absolute?:
+    | 'top'
+    | 'bottom'
+    | 'left'
+    | 'right'
+    | 'tl'
+    | 'tr'
+    | 'bl'
+    | 'br'
+    | string;
+  fixed?: string;
+}
+
+interface DzViewCursorProps {
+  pointer?: boolean;
+}
+
+interface DzPopoverProps {
   state?: any;
-
-  // cache
   cache?: boolean;
-
-  // trigger
   tooltip?: string;
   trigger?: 'hover' | 'click' | 'focus' | 'contextMenu';
-
-  // position
   placement?: 'top' | 'bottom' | 'left' | 'right' | undefined;
-}>();
+}
+
+interface DzModalProps {
+  state?: any;
+  cache?: boolean;
+}
+
+interface DzDrawerProps {
+  state?: any;
+  cache?: boolean;
+  position?: 'top' | 'bottom' | 'left' | 'right';
+}
+
+withDefaults(
+  defineProps<DzBaseProps & DzEntityProps & DzViewFlexProps & DzPopoverProps>(),
+  { cache: false }
+);
 
 const emit = defineEmits<{
   (e: 'update:state', value: any): void;
 }>();
-
-const close = () => {
-  const state = props.state;
-  state.visible = false;
-  state.fullscreen = false;
-  emit('update:state', state);
-};
-const toogleFullscreen = () => {
-  const state = props.state;
-  state.fullscreen = !state.fullscreen;
-  emit('update:state', state);
-};
 </script>
 
 <template>
-  <Tooltip
-    v-if="tooltip"
-    class="dz-tooltip-v202301 dz-tooltip dz-tooltip-acro"
-    :content="tooltip"
-  >
+  <Tooltip v-if="tooltip" class="dz-tooltip v202301" :content="tooltip">
     <slot></slot>
   </Tooltip>
-  <Popover
-    v-if="!tooltip"
-    class="dz-popover-v202301 dz-popover dz-popover-acro"
-    :trigger="trigger"
-  >
+  <Popover v-if="!tooltip" class="dz-popover v202301" :trigger="trigger">
     <slot></slot>
 
     <template #content>
-      <slot name="content"></slot>
+      <slot name="body"></slot>
     </template>
   </Popover>
 </template>
 
 <style lang="scss">
-.dz-popover-v202301.dz-popover.dz-popover-acro {
-  .arco-popover-popup-content {
-    @apply p-0;
-    @apply w-fit h-fit;
+.dz-popover.v202301.arco-popover {
+  & > .arco-trigger-popup-wrapper {
+    & > .arco-trigger-content {
+      @apply p-0 m-0;
+
+      & > .arco-popover-content {
+        @apply p-0 m-0 relative flex flex-row;
+        @apply w-fit h-fit;
+        & > .dz-view {
+          flex-grow: 1;
+        }
+      }
+    }
   }
 }
 </style>
