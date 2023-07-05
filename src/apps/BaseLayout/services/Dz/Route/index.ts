1268c552ff44c8659301a4ab38df031e632e5ce0
Dylan Zhang <987883389@qq.com>
2023-07-05 16:54:48 +0800

    refactor: 修改 dz route

---
+++ src/apps/BaseLayout/services/Dz/Route/index.ts
@@ -0,0 +1,52 @@
+import { defineStore } from 'pinia';
+import routesList from '@/resources/routes/data.json';
+import menuList from './menu.json';
+import { useCollection } from '@/hooks/useCollection';
+
+const { formatTree } = useCollection();
+
+const { tree, map } = formatTree({ list: menuList });
+
+export const useStore = defineStore('Dz/Route', () => {
+  const routeState = reactive({
+    list: routesList,
+  });
+
+  const menuState = reactive({
+    list: menuList,
+    tree: tree,
+    map: map,
+
+    iconMode: false,
+
+    collapse: {},
+    active: {},
+    show: {},
+  });
+
+  const routeTagState = reactive({
+    list: menuList,
+    map: map,
+
+    collapse: {},
+    active: {},
+    show: {},
+  });
+
+  const Test = payload => {
+    return new Promise(resolve => {
+      resolve({ data: payload });
+    });
+  };
+
+  const GetState = () => ({ menuState, routeState, routeTagState });
+
+  return {
+    routeState,
+    routeTagState,
+    menuState,
+
+    Test,
+    GetState,
+  };
+});
