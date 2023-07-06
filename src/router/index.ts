7a84b72520a124355f13030f13e7d42b1b5e6bb1
Dylan Zhang <987883389@qq.com>
2023-07-06 21:49:22 +0800

    feat: 完善 dz-route
    1. src/router 数据源 src/resource
    2. src/router 添加 tools 来转化 route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import { createRouteGuard } from '@/resources/guards';
-import { routes } from '@/resources/routes';
+import { beforeEach, routes } from '@/resources/routes/auto';
+import { buildRoute } from './tools';
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+  routes: buildRoute(routes),
 });
 
-createRouteGuard(router);
+router.beforeEach(beforeEach);
---
+++ src/router/tools.ts
@@ -0,0 +1,23 @@
+export const buildRoute = routeList =>
+  routeList.map(item => {
+    if (!item.component) return item;
+
+    const component = item.component.replace(/^\/|\/$/g, '');
+    const [c1] = component.split('/');
+
+    return {
+      name: item.name,
+      path: item.path,
+      component: () => import(`../apps/${c1}/index.vue`),
+      beforeEnter: item.beforeEnter,
+      children: item.children.map(item2 => {
+        const component = item2.component.replace(/^\/|\/$/g, '');
+        const [c1, c2, c3, c4] = component.split('/');
+        return {
+          name: item2.name,
+          path: item2.path,
+          component: () => import(`../apps/${c1}/${c2}/${c3}/${c4}/index.vue`),
+        };
+      }),
+    };
+  });
