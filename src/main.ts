d28093104af1a3f5cc44cbc7f982f1967a031e83
Dylan Zhang <987883389@qq.com>
2023-07-06 23:14:51 +0800

    refactor: 添加日志

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from './router';
 
+const { debug } = useLog({ module: 'Main', color: 'blue' });
+
+debug('初始化 App');
 const app = createApp(App);
 
+debug('安装中间件 Pinia');
 app.use(createPinia());
+
+debug('安装中间件 Router');
 app.use(router);
 
+debug('挂载 #App');
 app.mount('#App');
