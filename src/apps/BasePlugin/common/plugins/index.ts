357187bea787ad5887f4992a17b0e5d586f5d992
Dylan Zhang <987883389@qq.com>
2023-07-11 20:23:18 +0800

    refactor: plugin

--- src/apps/BasePlugin/common/plugins/index.ts
+++ src/apps/BasePlugin/common/plugins/index.ts
@@ -1,1 +1,11 @@
-export {};
+import CellText from './Cell/Text/index.vue';
+import ControlInput from './Control/Input/index.vue';
+
+export const plugins = {
+  Cell: {
+    'BasePlugin/Text': CellText,
+  },
+  Control: {
+    'BasePlugin/Input': ControlInput,
+  },
+};
