83af8dbb62cd5e54425cb5e507fa9c2215eff1fe
Dylan Zhang <987883389@qq.com>
2023-07-11 13:11:55 +0800

    refactor: test demo

--- src/apps/AdminApp/pages/PageDemo/Table01/index.vue
+++ src/apps/AdminApp/pages/PageDemo/Table01/index.vue
@@ -22,14 +22,14 @@ const handleVisibleQuery = () => {
   <v s="w-grow h-grow" w="overflow-auto p-4" col>
     <v s="w-grow h-16" text="tab"></v>
     <v s="w-grow h-grow">
-      <v s="w-grow h-grow" col text="list">
+      <v s="w-grow h-grow" col>
         <dz-card s="w-grow h-fit" icon="mdi:close">
           <v s="w-grow h-fit" grid text="query">
-            <v s="w-fit h-16" w="min-w-[160px]" text="form-item1" />
-            <v s="w-fit h-16" w="min-w-[160px]" text="form-item2" />
-            <v s="w-fit h-16" w="min-w-[160px]" text="form-item4" />
-            <v s="w-fit h-16" w="min-w-[160px]" text="form-item4" />
-            <v s="w-fit h-16" w="min-w-[160px]" text="form-item4" />
+            <v s="w-grow h-16" w="min-w-[160px]" text="form-item1" />
+            <v s="w-grow h-16" w="min-w-[160px]" text="form-item2" />
+            <v s="w-grow h-16" w="min-w-[160px]" text="form-item4" />
+            <v s="w-grow h-16" w="min-w-[160px]" text="form-item4" />
+            <v s="w-grow h-16" w="min-w-[160px]" text="form-item4" />
           </v>
 
           <template #action>
@@ -57,6 +57,261 @@ const handleVisibleQuery = () => {
           <dz-btn title="重置" @clic="handleResetForm" />
         </template>
       </dz-card>
+
+      <dz-card s="w-[300px] h-grow" class="card-lg" title="Test">
+        <v s="w-grow h-grow" w=" overflow-auto " row grid class="shadow-inner">
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+        </v>
+
+        <template #action>
+          <dz-btn type="primary" title="确认" @click="handleOkForm" />
+          <dz-btn title="重置" @clic="handleResetForm" />
+        </template>
+      </dz-card>
+
+      <v s="h-grow w-[300px]" w="overflow-auto" class="justify-start" row>
+        <dz-card s="w-[300px] h-fit" class="flex-shrink-0" title="Test">
+          <v s="w-grow h-fit" w=" overflow-auto " row grid class="shadow-inner">
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+          </v>
+
+          <template #action>
+            <dz-btn type="primary" title="确认" @click="handleOkForm" />
+            <dz-btn title="重置" @clic="handleResetForm" />
+          </template>
+        </dz-card>
+      </v>
     </v>
   </v>
 </template>
