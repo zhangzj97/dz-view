7eded60157748d7b9bf02d7312d082f0dc812440
Dylan Zhang <987883389@qq.com>
2023-07-12 14:35:35 +0800

    feat: add test-item

---
+++ src/apps/AdminApp/pages/PageDemo/Form03/components/TestItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+}>();
+</script>
+
+<template>
+  <v s="w-grow h-fit">
+    <v s="w-32 h-fit">
+      <v-space s="w-grow h-fit" />
+      <v-text :text="title" />
+      <v-space s="w-1 h-fit" />
+    </v>
+    <v s="w-grow h-fit" grid>
+      <slot></slot>
+    </v>
+  </v>
+</template>
