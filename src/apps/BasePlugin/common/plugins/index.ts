6ef23b7f36a50edc3a012d7174dc90415d94a384
Dylan Zhang <987883389@qq.com>
2023-07-12 10:19:49 +0800

    feat: add ControlText

--- src/apps/BasePlugin/common/plugins/index.ts
+++ src/apps/BasePlugin/common/plugins/index.ts
@@ -1,11 +1,13 @@
 import CellText from './Cell/Text/index.vue';
 import ControlInput from './Control/Input/index.vue';
+import ControlText from './Control/Text/index.vue';
 
 export const plugins = {
   Cell: {
     'BasePlugin/Text': CellText,
   },
   Control: {
+    'BasePlugin/Text': ControlText,
     'BasePlugin/Input': ControlInput,
   },
 };
