45e11f30f10a430c1c4abe58dd58aa078db884a1
Dylan Zhang <987883389@qq.com>
2023-07-12 14:37:02 +0800

    refactor: add test demo

--- src/apps/AdminApp/pages/PageDemo/Form03/index.vue
+++ src/apps/AdminApp/pages/PageDemo/Form03/index.vue
@@ -1,52 +1,129 @@
 <script setup lang="ts">
-const { schema, data, getValue, bind, setState, setValue, validate, itemVm } =
-  useForm({
-    module: 'Test',
-  });
+import TestItem from './components/TestItem.vue';
+const {
+  schema,
+  data,
+  getValue,
+  bind,
+  setState,
+  setValue,
+  validate,
+  getState,
+  itemVm,
+} = useForm({
+  module: 'Test',
+});
 </script>
 
 <template>
   <v s="w-grow h-grow" w="gap-1">
     <dz-card s="w-grow h-grow" class="card-lg" title="Form">
       <v s="w-grow h-grow" grid>
-        <dz-form-item s="w-full h-fit" v-bind="bind('id')('Input')" />
-        <dz-form-item s="w-full h-fit" v-bind="bind('name')('Input')" />
+        <dz-form-item s="w-full h-fit" v-bind="bind('id')('Text')" />
       </v>
     </dz-card>
 
-    <v s="w-[50vw] h-grow" w="gap-1" col>
+    <v s="w-grow h-grow" w="gap-1" col>
       <dz-card s="w-grow h-grow" class="card-lg" title="Test">
-        <v s="w-grow h-grow" w="gap-1" grid>
-          <dz-btn
-            title="id-required-true"
-            @click="setState('id')({ required: true })"
-          />
-          <dz-btn
-            title="id-required-false"
-            @click="setState('id')({ required: false })"
-          />
-          <dz-btn
-            title="id-visible-true"
-            @click="setState('id')({ visible: true })"
-          />
-          <dz-btn
-            title="id-visible-false"
-            @click="setState('id')({ visible: false })"
-          />
-          <dz-btn title="id-value-1" @click="setValue('id')('1')" />
-          <dz-btn title="id-value-[empty]" @click="setValue('id')('')" />
-          <dz-btn
-            title="id-append-1"
-            @click="setValue('id')(getValue('id') + '1')"
-          />
-          <dz-btn title="id-focus" @click="itemVm('id').focus()" />
-          <dz-btn title="id-blur" @click="itemVm('id').blur()" />
+        <v s="w-grow h-grow" w="gap-1 overflow-auto" col>
+          <!-- state -->
+          <TestItem title="state">
+            <dz-btn
+              v-for="item of ['required', 'disabled', 'visible', 'error']"
+              :key="item"
+              :type="getState('id')?.[item] ? 'primary' : 'outline'"
+              :title="`${item}`"
+              @click="setState('id')({ [item]: !getState('id')?.[item] })"
+            />
+          </TestItem>
+
+          <!-- value -->
+          <TestItem title="value">
+            <dz-btn
+              v-for="(item, index) of [
+                //
+                { value: null, title: 'null' },
+                { value: undefined, title: 'undefined' },
+                { value: {}, title: '对象' },
+                { value: [], title: '数组' },
+                { value: true, title: '布尔' },
+                { value: false, title: '布尔' },
+                { value: 1, title: '数字' },
+                { value: '', title: '空值' },
+                { value: ' ', title: '空格' },
+                { value: '中文', title: '中文' },
+              ]"
+              :key="index"
+              :type="getValue('id') === item.value ? 'primary' : 'outline'"
+              :title="item.title"
+              @click="setValue('id')(item.value)"
+            />
+          </TestItem>
+
+          <!-- append -->
+          <TestItem title="append">
+            <dz-btn
+              v-for="(item, index) of [
+                //
+                { value: null, title: 'null' },
+                { value: undefined, title: 'undefined' },
+                { value: {}, title: '对象' },
+                { value: [], title: '数组' },
+                { value: true, title: '布尔' },
+                { value: false, title: '布尔' },
+                { value: 1, title: '数字' },
+                { value: '', title: '空值' },
+                { value: ' ', title: '空格' },
+                { value: '中文', title: '中文' },
+              ]"
+              :key="index"
+              :title="item.title"
+              @click="setValue('id')(`${getValue('id')}${item.value}`)"
+            />
+          </TestItem>
+
+          <!-- plugin -->
+          <TestItem title="plugin">
+            <dz-btn title="focus" @click="itemVm('id').focus()" />
+            <dz-btn title="blur" @click="itemVm('id').blur()" />
+            <dz-btn title="reset" @click="itemVm('id').reset()" />
+            <dz-btn title="clear" @click="itemVm('id').clear()" />
+            <dz-btn
+              title="validate"
+              @click="itemVm('id').validate({ error: true })"
+            />
+            <dz-btn
+              title="validate"
+              @click="itemVm('id').validate({ error: false })"
+            />
+          </TestItem>
         </v>
       </dz-card>
-      <dz-card s="w-grow h-grow" class="card-lg" :footer="false" title="Data">
-        <v s="w-grow h-grow" text="1">
-          {{ data }}
-          {{ schema }}
+    </v>
+
+    <v s="w-grow h-grow" w="gap-1" col>
+      <dz-card s="w-grow h-grow" class="card-lg" title="Test">
+        <v s="w-grow h-grow" w="gap-1 overflow-auto" col>
+          <dz-btn title="value-1" @click="setValue('id')('1')" />
+          <dz-btn title="value-[empty]" @click="setValue('id')('')" />
+          <dz-btn
+            title="append-1"
+            @click="setValue('id')(getValue('id') + '1')"
+          />
+          <dz-btn title="focus" @click="itemVm('id').focus()" />
+          <dz-btn title="blur" @click="itemVm('id').blur()" />
+        </v>
+      </dz-card>
+      <dz-card
+        s="w-grow h-grow"
+        w="overflow-auto"
+        class="card-lg"
+        :footer="false"
+        title="Data"
+      >
+        <v s="w-grow h-grow" col text="1">
+          <pre>{{ data }}</pre>
+          <pre>{{ schema }}</pre>
         </v>
       </dz-card>
     </v>
