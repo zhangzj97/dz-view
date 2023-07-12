92d3e66e33c55b64ba4ee6cdb0c046a87374fd65
Dylan Zhang <987883389@qq.com>
2023-07-12 20:41:23 +0800

    refactor: format v types

---
+++ src/components/V.md
@@ -0,0 +1,14 @@
+# V
+
+## V
+
+✅
+❌
+
+| Name   | Base | Text | Flex | Position | Cursor | Entity | Experimental |
+| :----- | :--: | :--: | :--: | :------: | :----: | :----: | :----------: |
+| V      |  ✅  |  ✅  |  ✅  |    ✅    |   ✅   |   ✅   |      ✅      |
+| VDesc  |      |      |      |          |        |        |              |
+| VSpace |      |      |      |          |        |        |              |
+| VText  |      |      |      |          |        |        |              |
+| VIcon  |      |      |      |          |        |        |              |
--- src/types/dz-view.d.ts
+++ src/types/dz-view.d.ts
@@ -1,12 +1,12 @@
 /**
  * DzView [Base] 基础样式模块
  *
- * @ s    : 尺寸
- * @ w    : 最值尺寸, 媒体查询尺寸, overflow, gap
- * @ t    : 文本样式
- * @ trans: 过渡 动画
+ *  s    : 尺寸
+ *  w    : 最值尺寸, 媒体查询尺寸, overflow, gap
+ *  t    : 文本样式
+ *  trans: 过渡 动画
  */
-export interface DzViewBaseProps {
+interface DzViewBaseProps {
   s?: string;
   w?: string;
   t?: string;
@@ -14,133 +14,25 @@ export interface DzViewBaseProps {
 }
 
 /**
- * DzView [Base] 基础样式模块
- *
- * @abstract   abstract   abstract
- * @access   access   access
- * @alias   alias   alias
- * @argument   argument   argument
- * @async   async   async
- * @augments   augments   augments
- * @author   author   author
- *
- * @borrows borrows borrows borrows
- *
- * @callback callback callback callback
- *
- * @class class class class
- * @classdesc classdesc classdesc classdesc
- * @constant constant constant constant
- * @constructor constructor constructor constructor
- * @copyright copyright copyright copyright
- *
- * @default default default default
- * @deprecated deprecated deprecated deprecated
- * @description description description description
- *
- * @emits emits emits emits
- * @enum enum enum enum
- * @event event event event
- * @example example example example
- * @exports exports exports exports
- * @extends extends extends extends
- * @external external external external
- *
- * @field field field field
- * @file file file file
- * @fileoverview fileoverview fileoverview fileoverview
- * @fires fires fires fires
- * @function function function function
- *
- * @generator generator generator generator
- * @global global global global
- *
- * @hideconstructor hideconstructor hideconstructor hideconstructor
- * @host host host host
- *
- * @ignore ignore ignore ignore
- * @implements implements implements implements
- * @inheritdoc inheritdoc inheritdoc inheritdoc
- * @inner inner inner inner
- * @instance instance instance instance
- * @interface interface interface interface
- *
- * @kind kind kind kind
- *
- * @lends lends lends lends
- * @license license license license
- * @link link link link
- * @linkcode linkcode linkcode linkcode
- * @linkplain linkplain linkplain linkplain
- * @listens listens listens listens
- *
- * @member member member member
- * @memberof memberof memberof memberof
- * @method method method method
- * @mixes mixes mixes mixes
- * @module module module module
- * @name name name name
- * @namespace namespace namespace namespace
- *
- * @overload overload overload overload
- * @override override override override
- * @package package package package
- * @param param param param
- * @private private private private
- * @prop prop prop prop
- * @property property property property
- * @protected protected protected protected
- * @public public public public
- *
- * @readonly readonly readonly readonly
- * @requires requires requires requires
- * @returns returns returns returns
- *
- * @satisfies satisfies satisfies satisfies
- * @see see see see
- * @since since since since
- * @static static static static
- * @summary summary summary summary
- *
- * @template template template template
- * @this this this this
- * @throws throws throws throws
- * @todo todo todo todo
- * @tutorial tutorial tutorial tutorial
- * @type type type type
- * @typedef typedef typedef typedef
- *
- * @var var var var
- *
- * @variation variation variation variation
- *
- * @version version version version
- * @virtual virtual virtual virtual
- *
- * @yields yields yields yields
- *
- *
- *  */
-export interface DzViewTextProps {
+ * DzView [Text] 文本模块
+ */
+interface DzViewTextProps {
   text?: string;
 }
 
-export interface DzEntityProps {
-  id?: string;
-  icon?: string;
-  avatar?: string;
-  title?: string;
-  bg?: string;
-}
-
-export interface DzViewFlexProps {
+/**
+ * DzView [Flex] Flex布局 模块
+ */
+interface DzViewFlexProps {
   row?: boolean;
   col?: boolean;
-
   grid?: boolean;
 }
 
-export interface DzViewPositionProps {
+/**
+ * DzView [Position] 定位模块
+ */
+interface DzViewPositionProps {
   absolute?:
     | 'top'
     | 'bottom'
@@ -154,14 +46,44 @@ export interface DzViewPositionProps {
   fixed?: string;
 }
 
-export interface DzViewCursorProps {
+/**
+ * DzView [Cursor] 光标模块
+ */
+interface DzViewCursorProps {
   pointer?: boolean;
 }
 
-export interface DzViewSpaceProps {
-  space?: boolean;
-}
-
-export interface DzViewMouseProps {
+/**
+ * DzView [Experimental] 试验性模块
+ */
+interface DzViewExperimentalProps {
   v?: 'mouse-gray';
 }
+
+/**
+ * DzEntity [Base] 基础数据模块
+ */
+export interface DzEntityBaseProps {
+  id?: string;
+  icon?: string;
+  avatar?: string;
+  title?: string;
+  bg?: string;
+}
+/**
+ * DzEntity [Tree] 树形数据模块
+ */
+export interface DzEntityTreeProps {
+  id?: string;
+  parentId?: string;
+  children?: any[];
+  idPath?: string;
+  level?: string;
+}
+
+export type DzViewComponentProps = DzViewBaseProps &
+  DzViewTextProps &
+  DzViewFlexProps &
+  DzViewPositionProps &
+  DzViewCursorProps &
+  DzViewExperimentalProps;
