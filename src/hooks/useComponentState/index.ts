c0b95edd8715ddc02483ac56fcc45910565ba573
Dylan Zhang <987883389@qq.com>
2023-07-14 07:37:30 +0800

    refactor: useComponentState 修改形参

--- src/hooks/useComponentState/index.ts
+++ src/hooks/useComponentState/index.ts
@@ -2,8 +2,8 @@ import type { DzViewStateProps } from '@/types/dz-view';
 
 export const useComponentState = ({ props, emit }) => {
   const getState = () => props.state;
-  const setState = (option: DzViewStateProps) => {
-    Object.assign(props.state, option);
+  const setState = (state: DzViewStateProps) => {
+    Object.assign(props.state, state);
     // TODO 思考
     // emit('update:state', state);
   };
