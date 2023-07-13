1c0b8146ec4fd8da52a00719a0bec6c67fd64457
Dylan Zhang <987883389@qq.com>
2023-07-13 19:25:57 +0800

    refactor: useStateStore BindCode

--- src/hooks/useStateStore/index.ts
+++ src/hooks/useStateStore/index.ts
@@ -59,23 +59,5 @@ export const useStateStore = (option = {}) => {
   ];
 };
 
-type BindCode =
-  | 'P1'
-  | 'P2'
-  | 'P3'
-  | 'P4'
-  | 'P5'
-  | 'P6'
-  | 'P7'
-  | 'P8'
-  | 'P9'
-  | 'B1'
-  | 'B2'
-  | 'B3'
-  | 'B4'
-  | 'B5'
-  | 'B6'
-  | 'B7'
-  | 'B8'
-  | 'B9'
-  | string;
+// prettier-ignore
+type BindCode = 'P1' | 'P2' | 'P3' | 'P4' | 'P5' | 'P6' | 'P7' | 'P8' | 'P9' | 'B1' | 'B2' | 'B3' | 'B4' | 'B5' | 'B6' | 'B7' | 'B8' | 'B9' | string;
