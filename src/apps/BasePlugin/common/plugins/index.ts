74b5c226347fc8d43ac6280fbe1157e8c088ecae
Dylan Zhang <987883389@qq.com>
2023-07-14 15:21:21 +0800

    feat: ControlNumber

--- src/apps/BasePlugin/common/plugins/index.ts
+++ src/apps/BasePlugin/common/plugins/index.ts
@@ -1,6 +1,7 @@
 import CellText from './Cell/Text/index.vue';
 import ControlInput from './Control/Input/index.vue';
 import ControlText from './Control/Text/index.vue';
+import ControlNumber from './Control/Number/index.vue';
 
 export const plugins = {
   Cell: {
@@ -9,5 +10,6 @@ export const plugins = {
   Control: {
     'BasePlugin/Text': ControlText,
     'BasePlugin/Input': ControlInput,
+    'BasePlugin/Number': ControlNumber,
   },
 };
