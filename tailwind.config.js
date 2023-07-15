4448cb3b5ba7a358980fd38610ded92f7d124a0a
Dylan Zhang <987883389@qq.com>
2023-07-16 06:40:25 +0800

    feat: add shakeX

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,24 @@ module.exports = {
       scale: {
         25: '25%',
       },
+
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        shakeX: {
+          'from, to': {
+            transform: 'translate3d(0, 0, 0)',
+          },
+          '10%, 30%, 50%, 70%, 90%': {
+            transform: 'translate3d(-10px, 0, 0)',
+          },
+          '20%, 40%, 60%, 80%': {
+            transform: 'translate3d(10px, 0, 0)',
+          },
+        },
+      },
     },
   },
   plugins: [],
