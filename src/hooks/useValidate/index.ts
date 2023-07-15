7a280c8f67719c87fb35984b350dfbae498fee77
Dylan Zhang <987883389@qq.com>
2023-07-16 07:14:39 +0800

    refactor: change type

--- src/hooks/useValidate/index.ts
+++ src/hooks/useValidate/index.ts
@@ -1,22 +1,25 @@
 export const useValidate = () => {
-  const isString = (value: any) => typeof value === 'string';
+  const isUndefined = (v: unknown): v is undefined => v === undefined;
+  const isNull = (v: unknown): v is null => v === null;
+  const isString = (v: unknown): v is string => typeof v === 'string';
+  const isNumber = (v: unknown): v is number => typeof v === 'number';
+  const isBoolean = (v: unknown): v is boolean => typeof v === 'boolean';
 
-  const isBoolean = (value: any) => typeof value === 'boolean';
+  const isArray = (v: unknown): v is unknown[] => Array.isArray(v);
+  const isArrayExist = (v: unknown): v is unknown[] =>
+    Array.isArray(v) && v.length > 0;
 
-  const isUndefined = (value: any) => typeof value === 'undefined';
-
-  const isEmpty = (value: any) => {
-    if (!Array.isArray(value)) return true;
-    return value.length > 0;
+  const findDefined = (list: unknown[]) => {
+    return list.find(item => !isUndefined(item));
   };
-
-  const findDefined = (list: any[]) => list.find(item => !isUndefined(item));
-
   return {
+    isUndefined,
+    isNull,
     isString,
     isBoolean,
-    isUndefined,
-    isEmpty,
+    isNumber,
+    isArray,
+    isArrayExist,
 
     findDefined,
   };
