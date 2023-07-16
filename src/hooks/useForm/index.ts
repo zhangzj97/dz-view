ed904b81f2782c8f92e6c600c64763701c8f3e5b
Dylan Zhang <987883389@qq.com>
2023-07-16 08:46:49 +0800

    feat: add  initPluginEvent

--- src/hooks/useForm/index.ts
+++ src/hooks/useForm/index.ts
@@ -8,6 +8,7 @@ export const useForm = ({ schema = {}, service = '' }, option = {}) => {
     field: {},
     pluginCode: {},
     pluginOption: {},
+    pluginEvent: {},
 
     tooltip: {},
     validator: {},
@@ -20,7 +21,7 @@ export const useForm = ({ schema = {}, service = '' }, option = {}) => {
   // prettier-ignore
   const initField =
     (code: string) =>
-    (pluginCode, pluginOption = {}) => 
+    (pluginCode, pluginOption = {}, pluginEvent = {}) =>
     (state, option = {}) => {
       store.field[code] = {
         code : findDefined([code,                                                        ]),
@@ -33,7 +34,7 @@ export const useForm = ({ schema = {}, service = '' }, option = {}) => {
   // prettier-ignore
   const initPluginCode =
     (code: string) =>
-    (pluginCode, pluginOption = {}) => 
+    (pluginCode, pluginOption = {}, pluginEvent = {}) =>
     (state, option = {}) => {
       const pluginFullname = pluginCode.match(/\//g) ? pluginCode : `BasePlugin/${pluginCode}`;
       store.pluginCode[code] = findDefined([pluginFullname, schema?.[code]?.pluginCode, 'BasePlugin/Text']);
@@ -43,23 +44,30 @@ export const useForm = ({ schema = {}, service = '' }, option = {}) => {
   // prettier-ignore
   const initPluginOption =
     (code: string) =>
-    (pluginCode, pluginOption = {}) => 
+    (pluginCode, pluginOption = {}, pluginEvent = {}) =>
     (state, option = {}) => {
       store.pluginOption[code] = findDefined([pluginOption, schema?.[code]?.pluginOption, {}]);
       return store.pluginCode[code];
     }
 
   // prettier-ignore
+  const initPluginEvent =
+    (code: string) =>
+    (pluginCode, pluginOption = {}, pluginEvent = {}) =>
+    (state, option = {}) => {
+      store.pluginEvent[code] = findDefined([pluginEvent, schema?.[code]?.pluginEvent, {}]);
+      return store.pluginEvent[code];
+    }
+
+  // prettier-ignore
   const initValidator =
     (code: string) =>
-    (pluginCode, pluginOption = {}) => 
+    (pluginCode, pluginOption = {}, pluginEvent = {}) =>
     (state, option = {}) => {
-      console.log(option?.validtor?.rule, 9999)
       store.validator[code] = {
         result: findDefined([{ error: false, message: null, list: [] }]),
         rule  : findDefined([option?.validator?.rule, schema?.[code]?.validator.rule, []])
       }
-      console.log(store.validator[code])
 
       return store.validator[code];
     }
@@ -67,7 +75,7 @@ export const useForm = ({ schema = {}, service = '' }, option = {}) => {
   // prettier-ignore
   const initState =
     (code: string) =>
-    (pluginCode, pluginOption = {}) => 
+    (pluginCode, pluginOption = {}, pluginEvent = {}) =>
     (state, option = {}) => {
       store.state[code] = {
         required : findDefined([state?.required, schema?.[code]?.state?.required, false]),
@@ -81,7 +89,7 @@ export const useForm = ({ schema = {}, service = '' }, option = {}) => {
   // prettier-ignore
   const initValue =
     (code: string) =>
-    (pluginCode, pluginOption = {}) =>
+    (pluginCode, pluginOption = {}, pluginEvent = {}) =>
     (state, option = {}) => {
       store.value[code] = findDefined([schema?.[code]?.value, option?.value]);
       return store.value[code];
@@ -90,7 +98,7 @@ export const useForm = ({ schema = {}, service = '' }, option = {}) => {
   // prettier-ignore
   const initData =
     (code: string) =>
-    (pluginCode, pluginOption = {}) =>
+    (pluginCode, pluginOption = {}, pluginEvent = {}) =>
     (state, option = {}) => {
       return store.value;
     };
@@ -98,13 +106,14 @@ export const useForm = ({ schema = {}, service = '' }, option = {}) => {
   // prettier-ignore
   const bind =
     (code: string) =>
-    (pluginCode, pluginOption = {}) =>
+    (pluginCode, pluginOption = {}, pluginEvent = {}) =>
     (state, option = {}) => {
       if (store.dom[code])
         return {
           field        : store.field[code],
           pluginCode   : store.pluginCode[code],
           pluginOption : store.pluginOption[code],
+          pluginEvent  : store.pluginEvent[code],
           validator    : store.validator[code],
           state        : store.state[code],
           value        : store.value[code],
@@ -114,13 +123,14 @@ export const useForm = ({ schema = {}, service = '' }, option = {}) => {
         };
 
       return {
-        field        : initField(       code)(pluginCode, pluginOption)(state, option),
-        pluginCode   : initPluginCode(  code)(pluginCode, pluginOption)(state, option),
-        pluginOption : initPluginOption(code)(pluginCode, pluginOption)(state, option),
-        validator    : initValidator(   code)(pluginCode, pluginOption)(state, option),
-        state        : initState(       code)(pluginCode, pluginOption)(state, option),
-        value        : initValue(       code)(pluginCode, pluginOption)(state, option),
-        data         : initData(        code)(pluginCode, pluginOption)(state, option),
+        field        : initField(       code)(pluginCode, pluginOption, pluginEvent)(state, option),
+        pluginCode   : initPluginCode(  code)(pluginCode, pluginOption, pluginEvent)(state, option),
+        pluginOption : initPluginOption(code)(pluginCode, pluginOption, pluginEvent)(state, option),
+        pluginEvent  : initPluginEvent( code)(pluginCode, pluginOption, pluginEvent)(state, option),
+        validator    : initValidator(   code)(pluginCode, pluginOption, pluginEvent)(state, option),
+        state        : initState(       code)(pluginCode, pluginOption, pluginEvent)(state, option),
+        value        : initValue(       code)(pluginCode, pluginOption, pluginEvent)(state, option),
+        data         : initData(        code)(pluginCode, pluginOption, pluginEvent)(state, option),
         ref          : el => (store.dom[code] = el),
         'onUpdate:value': (value: any) => store.value[code] = value
       };
