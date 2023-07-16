d9d4fe90a6e05f2e4eca1113102582e702b1e5a6
Dylan Zhang <987883389@qq.com>
2023-07-16 16:29:13 +0800

    refactor: control

--- src/apps/BasePlugin/common/plugins/index.ts
+++ src/apps/BasePlugin/common/plugins/index.ts
@@ -1,15 +1,28 @@
-import CellText from './Cell/Text/index.vue';
-import ControlInput from './Control/Input/index.vue';
 import ControlText from './Control/Text/index.vue';
 import ControlNumber from './Control/Number/index.vue';
+import ControlPassword from './Control/Password/index.vue';
+import ControlDate from './Control/Date/index.vue';
+import ControlTime from './Control/Time/index.vue';
+import ControlDatetime from './Control/Datetime/index.vue';
+import ControlWeek from './Control/Week/index.vue';
+import ControlMonth from './Control/Month/index.vue';
+import ControlFile from './Control/File/index.vue';
+import ControlRange from './Control/Range/index.vue';
+import ControlColor from './Control/Color/index.vue';
 
 export const plugins = {
-  Cell: {
-    'BasePlugin/Text': CellText,
-  },
+  Cell: {},
   Control: {
     'BasePlugin/Text': ControlText,
-    'BasePlugin/Input': ControlInput,
     'BasePlugin/Number': ControlNumber,
+    'BasePlugin/Password': ControlPassword,
+    'BasePlugin/Date': ControlDate,
+    'BasePlugin/Time': ControlTime,
+    'BasePlugin/Datetime': ControlDatetime,
+    'BasePlugin/Week': ControlWeek,
+    'BasePlugin/Month': ControlMonth,
+    'BasePlugin/File': ControlFile,
+    'BasePlugin/Range': ControlRange,
+    'BasePlugin/Color': ControlColor,
   },
 };
