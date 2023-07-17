b6de6a3d9c9447354e47af46b01991041436cb74
Dylan Zhang <987883389@qq.com>
2023-07-17 14:46:09 +0800

    refactor: add test

--- src/apps/PluginRaw/common/components/PluginControl.vue
+++ src/apps/PluginRaw/common/components/PluginControl.vue
@@ -1,17 +1,55 @@
 <script setup lang="ts">
 withDefaults(
-  defineProps<{
-    state: any;
-    validator: any;
-  }>(),
+  defineProps<
+    { state: any; validator: any } & {
+      validate?: any;
+      reset?: any;
+      setState?: any;
+      setValue?: any;
+    }
+  >(),
   {}
 );
+
+const { bind0 } = useStateStore();
 </script>
 
 <template>
   <v s="w-grow h-grow" col>
     <v s="w-grow h-fit">
       <slot></slot>
+
+      <!-- prettier-ignore -->
+      <v s="w-fit h-8" w="px-1" v="mouse-gray" :class="[state.required && 'bg-blue-400']"
+        @click="setState({ required: !state.required })">
+        <v-text text="必" />
+      </v>
+
+      <!-- prettier-ignore -->
+      <v s="w-fit h-8" w="px-1" v="mouse-gray" :class="[state.disabled && 'bg-blue-400']"
+        @click="setState({ disabled: !state.disabled })">
+        <v-text text="禁" />
+      </v>
+
+      <!-- prettier-ignore -->
+      <v s="w-fit h-8" w="px-1" v="mouse-gray" :class="[state.error && 'bg-blue-400']"
+        @click="setState({ error: !state.error })">
+        <v-text text="错" />
+      </v>
+
+      <!-- prettier-ignore -->
+      <v s="w-fit h-8" w="px-1" v="mouse-gray" @click="validate({ error: true })"><v-text text="验" /></v>
+      <!-- prettier-ignore -->
+      <v s="w-fit h-8" w="px-1" v="mouse-gray" @click="reset"><v-text text="清" /></v>
+
+      <dz-popover v-bind="bind0('P1')()" trigger="hover">
+        <v s="w-fit h-fit" v="mouse-gray">
+          <v-icon v="8-50" icon="mdi:test-tube" />
+        </v>
+        <template #body>
+          <slot name="test"></slot>
+        </template>
+      </dz-popover>
     </v>
 
     <v
