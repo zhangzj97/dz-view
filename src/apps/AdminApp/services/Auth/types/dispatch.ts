12002e6f6dda2303625bccd5d45627f8b52f4547
Dylan Zhang <987883389@qq.com>
2023-08-09 21:39:37 +0800

    chore

--- src/apps/AdminApp/services/Auth/types/dispatch.ts
+++ src/apps/AdminApp/services/Auth/types/dispatch.ts
@@ -1,26 +1,26 @@
 // prettier-ignore
 export interface Dispatch {
-  (service: { code: 'Auth/Bar.count' }       ) : ( payload: Payload<BarQuery>) => Promise<R<Selector<Long>>>;
-  (service: { code: 'Auth/Bar.create' }      ) : ( payload: Payload<BarCmd>)   => Promise<R<CmdR<Long>>>;
-  (service: { code: 'Auth/Bar.createSimple' }) : ( payload: Payload<BarCmd>)   => Promise<R<CmdR<Long>>>;
-  (service: { code: "Auth/Bar.delete" }      ) : ( payload: Payload<Object>)   => Promise<R<CmdR<Boolean>>>;
-  (service: { code: 'Auth/Bar.deleteHard' }  ) : ( payload: Payload<Object>)   => Promise<R<CmdR<Boolean>>>;
-  (service: { code: 'Auth/Bar.find' }        ) : ( payload: Payload<BarQuery>) => Promise<R<BarEntity>>;
-  (service: { code: 'Auth/Bar.restore' }     ) : ( payload: Payload<Object>)   => Promise<R<CmdR<Boolean>>>;
-  (service: { code: 'Auth/Bar.select' }      ) : ( payload: Payload<BarQuery>) => Promise<R<Selector<BarEntity>>>;
-  (service: { code: 'Auth/Bar.update' }      ) : ( payload: Payload<BarCmd>)   => Promise<R<CmdR<Boolean>>>;
-  (service: { code: 'Auth/Bar.updateSimple' }) : ( payload: Payload<BarCmd>)   => Promise<R<CmdR<Boolean>>>;
+  (service: { code: 'Auth/Bar.count' }       ) : ( payload: Payload<Partial<BarQuery>>) => Promise<R<Selector<Long>>>;
+  (service: { code: 'Auth/Bar.create' }      ) : ( payload: Payload<Partial<BarCmd>>)   => Promise<R<CmdR<Long>>>;
+  (service: { code: 'Auth/Bar.createSimple' }) : ( payload: Payload<Partial<BarCmd>>)   => Promise<R<CmdR<Long>>>;
+  (service: { code: "Auth/Bar.delete" }      ) : ( payload: Payload<Partial<Object>>)   => Promise<R<CmdR<Boolean>>>;
+  (service: { code: 'Auth/Bar.deleteHard' }  ) : ( payload: Payload<Partial<Object>>)   => Promise<R<CmdR<Boolean>>>;
+  (service: { code: 'Auth/Bar.find' }        ) : ( payload: Payload<Partial<BarQuery>>) => Promise<R<BarEntity>>;
+  (service: { code: 'Auth/Bar.restore' }     ) : ( payload: Payload<Partial<Object>>)   => Promise<R<CmdR<Boolean>>>;
+  (service: { code: 'Auth/Bar.select' }      ) : ( payload: Payload<Partial<BarQuery>>) => Promise<R<Selector<BarEntity>>>;
+  (service: { code: 'Auth/Bar.update' }      ) : ( payload: Payload<Partial<BarCmd>>)   => Promise<R<CmdR<Boolean>>>;
+  (service: { code: 'Auth/Bar.updateSimple' }) : ( payload: Payload<Partial<BarCmd>>)   => Promise<R<CmdR<Boolean>>>;
 
-  (service: { code: 'Auth/Foo.count' }       ) : ( payload: Payload<FooQuery>) => Promise<R<Selector<Long>>>;
-  (service: { code: 'Auth/Foo.create' }      ) : ( payload: Payload<FooCmd>)   => Promise<R<CmdR<Long>>>;
-  (service: { code: 'Auth/Foo.createSimple' }) : ( payload: Payload<FooCmd>)   => Promise<R<CmdR<Long>>>;
-  (service: { code: "Auth/Foo.delete" }      ) : ( payload: Payload<Object>)   => Promise<R<CmdR<Boolean>>>;
-  (service: { code: 'Auth/Foo.deleteHard' }  ) : ( payload: Payload<Object>)   => Promise<R<CmdR<Boolean>>>;
-  (service: { code: 'Auth/Foo.find' }        ) : ( payload: Payload<FooQuery>) => Promise<R<FooEntity>>;
-  (service: { code: 'Auth/Foo.restore' }     ) : ( payload: Payload<Object>)   => Promise<R<CmdR<Boolean>>>;
-  (service: { code: 'Auth/Foo.select' }      ) : ( payload: Payload<FooQuery>) => Promise<R<Selector<FooEntity>>>;
-  (service: { code: 'Auth/Foo.update' }      ) : ( payload: Payload<FooCmd>)   => Promise<R<CmdR<Boolean>>>;
-  (service: { code: 'Auth/Foo.updateSimple' }) : ( payload: Payload<FooCmd>)   => Promise<R<CmdR<Boolean>>>;
+  (service: { code: 'Auth/Foo.count' }       ) : ( payload: Payload<Partial<FooQuery>>) => Promise<R<Selector<Long>>>;
+  (service: { code: 'Auth/Foo.create' }      ) : ( payload: Payload<Partial<FooCmd>>)   => Promise<R<CmdR<Long>>>;
+  (service: { code: 'Auth/Foo.createSimple' }) : ( payload: Payload<Partial<FooCmd>>)   => Promise<R<CmdR<Long>>>;
+  (service: { code: "Auth/Foo.delete" }      ) : ( payload: Payload<Partial<Object>>)   => Promise<R<CmdR<Boolean>>>;
+  (service: { code: 'Auth/Foo.deleteHard' }  ) : ( payload: Payload<Partial<Object>>)   => Promise<R<CmdR<Boolean>>>;
+  (service: { code: 'Auth/Foo.find' }        ) : ( payload: Payload<Partial<FooQuery>>) => Promise<R<FooEntity>>;
+  (service: { code: 'Auth/Foo.restore' }     ) : ( payload: Payload<Partial<Object>>)   => Promise<R<CmdR<Boolean>>>;
+  (service: { code: 'Auth/Foo.select' }      ) : ( payload: Payload<Partial<FooQuery>>) => Promise<R<Selector<FooEntity>>>;
+  (service: { code: 'Auth/Foo.update' }      ) : ( payload: Payload<Partial<FooCmd>>)   => Promise<R<CmdR<Boolean>>>;
+  (service: { code: 'Auth/Foo.updateSimple' }) : ( payload: Payload<Partial<FooCmd>>)   => Promise<R<CmdR<Boolean>>>;
 }
 
 type FooCmd = {
