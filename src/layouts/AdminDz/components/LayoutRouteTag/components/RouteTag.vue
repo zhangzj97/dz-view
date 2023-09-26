0e4767bacd4fd41bb2bcac503c36a478b03f95eb
Dylan Zhang <987883389@qq.com>
2023-09-26 20:43:21 +0800

    refactor: RouteTag

--- src/layouts/AdminDz/components/LayoutRouteTag/components/RouteTag.vue
+++ src/layouts/AdminDz/components/LayoutRouteTag/components/RouteTag.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+// TODO 事件穿透
 defineProps<{
   id?: string;
   avatar?: string;
@@ -27,29 +28,16 @@ const clickRouteTag = (item: any) => {
     s="w-fit h-8"
     w="overflow-hidden"
     class="border-[1px] border-gray-300 rounded-md group"
-    hover="hover:bg-gray-100"
+    :class="[active ? 'bg-blue-200' : 'bg-gray-100']"
+    :hover="[active ? 'hover:bg-blue-100' : 'hover:bg-gray-100']"
     pointer
-    @click="clickRouteTag({ id })"
+    trans
   >
-    <v-space s="w-4 h-grow" />
-    <v-text
-      t="text-gray-600 text-sm"
-      trans="group-hover:text-blue-300"
-      :text="title"
-    />
-    <v-space s="w-4 h-grow" />
-    <v
-      v-if="showRightIcon"
-      s="w-8 h-8"
-      v="mouse-gray"
-      @click="removeRouteTag({ id })"
-    >
-      <v-icon
-        v="8-50"
-        t="text-gray-300"
-        class="opacity-0 group-hover:opacity-100"
-        icon="ic:round-close"
-      />
+    <v-space s="w-4 h-grow" @click="clickRouteTag({ id })" />
+    <v-text t="text-gray-600 text-sm" trans="group-hover:text-blue-300" :text="title" @click="clickRouteTag({ id })" />
+    <v-space s="w-4 h-grow" @click="clickRouteTag({ id })" />
+    <v v-if="showRightIcon" s="w-8 h-8" v="mouse-gray" @click="removeRouteTag({ id })">
+      <v-icon v="8-50" t="text-gray-300" class="opacity-0 group-hover:opacity-100" icon="ic:round-close" />
     </v>
   </v>
 </template>
--- src/layouts/AdminDz/components/LayoutRouteTag/index.vue
+++ src/layouts/AdminDz/components/LayoutRouteTag/index.vue
@@ -3,38 +3,65 @@ import RouteTag from './components/RouteTag.vue';
 
 const { dispatch, getState, Service } = useService();
 
-const { routeTagState, menuState } = getState('Dz/Route');
+const { routeTagState, menuState, versionState } = getState('Dz/Route');
 
 const router: any = useRouter();
 
-onMounted(async () => {
-  const toId = router.currentRoute?.value?.meta?.menu?.id;
-  await dispatch(Service.Dz.Route.AddRouteTag)({ id: toId });
-});
+const findMenuByPath = (path: string) => {
+  return menuState.list.find(item => item.path === path);
+};
+
+watch(
+  () => versionState.version.value,
+  async () => {
+    const path = router.currentRoute?.value.path;
+    const menu = findMenuByPath(path);
+    if (menu.fixedTag) return;
+    await dispatch(Service.Dz.Route.AddRouteTag)({ id: menu.id });
+  }
+);
 
 onBeforeRouteUpdate(async (to, form, next) => {
-  const toId = to?.meta?.menu?.id;
-  await dispatch(Service.Dz.Route.AddRouteTag)({ id: toId });
+  const path = to.matched.slice(-1)[0]?.path;
+  const menu = findMenuByPath(path);
+  await dispatch(Service.Dz.Route.AddRouteTag)({ id: menu.id });
   next();
 });
 
 const clickRouteTag = ({ id }: any) => {
-  const item = menuState.map[id];
-  const { code } = item;
-
-  router.push({ name: code });
+  const path = menuState.map[id]?.path;
+  router.push({ path });
 };
 
 const removeRouteTag = async ({ id }: any) => {
-  await dispatch(Service.Dz.Route.RemoveRouteTag)({ id });
+  const path = router.currentRoute?.value.path;
+  const menu = findMenuByPath(path);
+  await dispatch(Service.Dz.Route.RemoveRouteTag)({ id: menu.id });
+
+  if (menu.id === id) {
+    const id2 = routeTagState.fixed[0];
+    const path2 = menuState.map[id2]?.path;
+    router.push({ path: path2 });
+  }
 };
 
 const removeAll = async () => {
-  await dispatch(Service.Dz.Route.RemoveRouteTag)({});
+  routeTagState.open.forEach(async (id: string) => {
+    await dispatch(Service.Dz.Route.RemoveRouteTag)({ id });
+  });
+
+  const id2 = routeTagState.fixed[0];
+  const path2 = menuState.map[id2]?.path;
+  router.push({ path: path2 });
 };
 
-const openList = computed(() => {
-  return [...routeTagState.fixed, ...routeTagState.open];
+const fixedList = computed(() => routeTagState.fixed);
+const openList = computed(() => routeTagState.open);
+
+const activeId = computed(() => {
+  const path = router.currentRoute?.value.path;
+  const menu = findMenuByPath(path);
+  return menu.id;
 });
 </script>
 
@@ -42,14 +69,26 @@ const openList = computed(() => {
   <v s="w-grow h-8" class="shadow-lg">
     <v s="w-grow h-grow">
       <RouteTag
+        v-for="(id, index) of fixedList"
+        :key="index"
+        class="scale-90"
+        :id="menuState.map[id].id"
+        :avatar="menuState.map[id].avatar"
+        :title="menuState.map[id].title"
+        :active="activeId === id"
+        :showRightIcon="false"
+        @clickRouteTag="clickRouteTag"
+        @removeRouteTag="removeRouteTag"
+      />
+      <RouteTag
         v-for="(id, index) of openList"
         :key="index"
         class="scale-90"
         :id="menuState.map[id].id"
         :avatar="menuState.map[id].avatar"
         :title="menuState.map[id].title"
-        :active="!!routeTagState.active[id]"
-        :showRightIcon="!menuState.map[id].fixedTag"
+        :active="activeId === id"
+        :showRightIcon="true"
         @clickRouteTag="clickRouteTag"
         @removeRouteTag="removeRouteTag"
       />
