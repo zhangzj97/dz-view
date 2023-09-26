ae46eac955c283bb19ee1780afaa78dbd05ba868
Dylan Zhang <987883389@qq.com>
2023-09-27 06:52:06 +0800

    refactor: test

---
+++ src/pages/Test/Btn/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { bind } = useBind();
+</script>
+
+<template>
+  <v s="w-grow h-grow" text="Btn">
+    <dz-btn title="test1" v-bind="bind('b1', true)"> </dz-btn>
+    <dz-btn title="disable1" @click="() => bind('b1').setState({ disabled: true })"></dz-btn>
+    <dz-btn title="disable2" @click="() => bind('b1').setState({ disabled: false })"></dz-btn>
+    <dz-btn title="loading1" @click="() => bind('b1').setState({ loading: true })"></dz-btn>
+    <dz-btn title="loading2" @click="() => bind('b1').setState({ loading: false })"></dz-btn>
+  </v>
+</template>
---
+++ src/pages/Test/Popup/index.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+const { bind } = useBind();
+
+const a = ref();
+</script>
+
+<template>
+  <v s="w-grow h-grow" col text="popup">
+    <v s="w-grow h-grow" text="drawer">
+      <dz-btn title="disable1" @click="() => bind('b1').setState({ disabled: true })"></dz-btn>
+      <dz-btn title="disable2" @click="() => bind('b1').setState({ disabled: false })"></dz-btn>
+      <dz-btn title="loading1" @click="() => bind('b1').setState({ loading: true })"></dz-btn>
+      <dz-btn title="loading2" @click="() => bind('b1').setState({ loading: false })"></dz-btn>
+      <dz-btn title="visible1" @click="() => bind('b1').setState({ visible: true })"></dz-btn>
+      <dz-btn title="visible2" @click="() => bind('b1').setState({ visible: false })"></dz-btn>
+
+      <dz-drawer v-bind="bind('b1', true)" />
+    </v>
+
+    <v s="w-grow h-grow" text="modal">
+      <dz-btn title="disable1" @click="() => bind('b2').setState({ disabled: true })"></dz-btn>
+      <dz-btn title="disable2" @click="() => bind('b2').setState({ disabled: false })"></dz-btn>
+      <dz-btn title="loading1" @click="() => bind('b2').setState({ loading: true })"></dz-btn>
+      <dz-btn title="loading2" @click="() => bind('b2').setState({ loading: false })"></dz-btn>
+      <dz-btn title="visible1" @click="() => bind('b2').setState({ visible: true })"></dz-btn>
+      <dz-btn title="visible2" @click="() => bind('b2').setState({ visible: false })"></dz-btn>
+      <dz-btn title="visible2" @click="a = true"></dz-btn>
+
+      <dz-modal v-bind="bind('b2', true)" />
+    </v>
+
+    <v s="w-grow h-grow" text="tooltip">
+      <dz-btn title="disable1" @click="() => bind('b3').setState({ disabled: true })"></dz-btn>
+      <dz-btn title="disable2" @click="() => bind('b3').setState({ disabled: false })"></dz-btn>
+      <dz-btn title="loading1" @click="() => bind('b3').setState({ loading: true })"></dz-btn>
+      <dz-btn title="loading2" @click="() => bind('b3').setState({ loading: false })"></dz-btn>
+      <dz-btn title="visible1" @click="() => bind('b3').setState({ visible: true })"></dz-btn>
+      <dz-btn title="visible2" @click="() => bind('b3').setState({ visible: false })"></dz-btn>
+
+      <dz-btn title="31-v1" @click="() => bind('b31').setState({ visible: true })"></dz-btn>
+      <dz-btn title="31-v2" @click="() => bind('b31').setState({ visible: false })"></dz-btn>
+
+      <dz-btn title="32-v1" @click="() => bind('b32').setState({ visible: true })"></dz-btn>
+      <dz-btn title="32-v2" @click="() => bind('b32').setState({ visible: false })"></dz-btn>
+      <dz-btn title="33-v1" @click="() => bind('b33').setState({ visible: true })"></dz-btn>
+      <dz-btn title="33-v2" @click="() => bind('b33').setState({ visible: false })"></dz-btn>
+
+      <dz-popover v-bind="bind('b31', true)">
+        <v s="w-fit h-fit" v="mouse-gray">
+          <v-icon v="8-75" icon="mdi:ab-testing" />
+        </v>
+        <template #body> 123</template>
+      </dz-popover>
+
+      <dz-popover trigger="click" v-bind="bind('b32', true)">
+        <v s="w-fit h-fit" v="mouse-gray">
+          <v-icon v="8-75" icon="mdi:ab-testing" />
+        </v>
+        <template #body> 123</template>
+      </dz-popover>
+
+      <dz-popover tooltip="0000" v-bind="bind('b33', true)">
+        <v s="w-fit h-fit" v="mouse-gray">
+          <v-icon v="8-75" icon="mdi:ab-testing" />
+        </v>
+      </dz-popover>
+
+      <dz-popover confirm="0000" v-bind="bind('b41', true)">
+        <v s="w-fit h-fit" v="mouse-gray">
+          <v-icon v="8-75" icon="mdi:ab-testing" />
+        </v>
+      </dz-popover>
+    </v>
+  </v>
+</template>
