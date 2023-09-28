67c8653d44bc237f3b3add36bc18e623e270689b
Dylan Zhang <987883389@qq.com>
2023-09-28 15:54:09 +0800

    refactor: input

--- src/components/Control/Input/index.vue
+++ src/components/Control/Input/index.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
-defineOptions({ name: 'ControlInput' });
-
-import ControlWrapper from '../ControlWrapper.vue';
-import type { ControlProps, ControlEmits } from '@/types/dz-view'; // prettier-ignore
-type Option = {};
-type Event = {};
-const props = withDefaults(defineProps<ControlProps<Option>>(), {});
-const emits = defineEmits<ControlEmits & Event>();
+import type { ControlProps, ControlEmits } from '@/types/dz-view';
+const props = withDefaults(defineProps<ControlProps<{}>>(), {});
+const emits = defineEmits<ControlEmits & {}>();
 
 const { isString, isNumber, isBoolean } = useValidate();
 const getValue = (): string | null => props.value;
@@ -18,33 +13,30 @@ const setValue = (value: unknown) => {
   emits('update:value', newValue);
 };
 
-const { pluginDom, ExposeMethod, CommonEvent, modelValue } = usePluginControl({ props, emits, getValue, setValue }); // prettier-ignore
+const { el, exposeMethod, commonEvent } = usePluginControl({ props, emits });
+defineExpose({ ...exposeMethod, getValue, setValue });
 
-defineExpose({ ...ExposeMethod });
-
-onMounted(() => emits('update:value', null));
+onMounted(() => setValue(null));
 </script>
 
 <template>
-  <ControlWrapper :state="state" :option="option" v-bind="ExposeMethod">
-    <input
-      ref="pluginDom"
-      :class="[
-        'w-full h-8',
-        'dz-plugin-control-input',
-        state?.error && 'dz-plugin-control-input--error',
-        state?.disabled && 'dz-plugin-control-input--disabled',
-      ]"
-      type="text"
-      :disabled="state.disabled"
-      :value="modelValue"
-      @input="CommonEvent.onInput"
-      @focus="CommonEvent.onFocus"
-      @blur="CommonEvent.onBlur"
-    />
+  <input
+    ref="el"
+    :class="[
+      'w-full h-8',
+      'dz-plugin-control-input',
+      state?.error && 'dz-plugin-control-input--error',
+      state?.disabled && 'dz-plugin-control-input--disabled',
+    ]"
+    type="text"
+    :disabled="state?.disabled"
+    :value="getValue()"
+    @input="commonEvent.onInput"
+    @focus="commonEvent.onFocus"
+    @blur="commonEvent.onBlur"
+  />
 
-    <v s="w-fit h-fit" v="mouse-gray" @click="ExposeMethod.reset">
-      <v-icon v="8-50" icon="mdi:close-circle-outline" />
-    </v>
-  </ControlWrapper>
+  <v s="w-fit h-fit" v="mouse-gray" @click="exposeMethod.reset">
+    <v-icon v="8-50" icon="mdi:close-circle-outline" />
+  </v>
 </template>
