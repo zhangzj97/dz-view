4ddf36c43a3333e6b3ef03e398e91db12344f167
Dylan Zhang <987883389@qq.com>
2023-09-29 06:47:02 +0800

    refactor: route data

--- src/router/route.ts
+++ src/router/route.ts
@@ -6,20 +6,71 @@ export const routes = [
   {
     name: 'Root',
     path: '/',
-    redirect: '/AdminApp',
+    redirect: '/User',
     children: [],
   },
   {
     name: 'NotFound',
     path: '/404',
-    component: 'AdminApp',
+    redirect: '/',
     children: [],
-    beforeEnter,
   },
   {
     name: 'Other',
     path: '/:pathMatch(.*)',
-    redirect: '/AdminApp',
-    children: [],
+    redirect: '/404',
+  },
+  {
+    name: 'Tree',
+    path: '/Tree',
+    redirect: '/Tree/Tree100',
+    component: () => import('@/pages/Tree/index.vue'),
+    children: [
+      { name: 'Tree/Tree100', path: '/Tree/Tree100', component: () => import('@/pages/Tree/Tree100/index.vue') },
+      { name: 'Tree/Tree200', path: '/Tree/Tree200', component: () => import('@/pages/Tree/Tree200/index.vue') },
+      { name: 'Tree/Tree210', path: '/Tree/Tree210', component: () => import('@/pages/Tree/Tree210/index.vue') },
+      { name: 'Tree/Tree220', path: '/Tree/Tree220', component: () => import('@/pages/Tree/Tree220/index.vue') },
+      { name: 'Tree/Tree300', path: '/Tree/Tree300', component: () => import('@/pages/Tree/Tree300/index.vue') },
+      { name: 'Tree/Tree310', path: '/Tree/Tree310', component: () => import('@/pages/Tree/Tree310/index.vue') },
+      { name: 'Tree/Tree320', path: '/Tree/Tree320', component: () => import('@/pages/Tree/Tree320/index.vue') },
+      { name: 'Tree/Tree311', path: '/Tree/Tree311', component: () => import('@/pages/Tree/Tree311/index.vue') },
+      { name: 'Tree/Tree312', path: '/Tree/Tree312', component: () => import('@/pages/Tree/Tree312/index.vue') },
+      { name: 'Tree/Tree321', path: '/Tree/Tree321', component: () => import('@/pages/Tree/Tree321/index.vue') },
+      { name: 'Tree/Tree322', path: '/Tree/Tree322', component: () => import('@/pages/Tree/Tree322/index.vue') },
+    ],
+  },
+
+  {
+    name: 'Test',
+    path: '/Test',
+    redirect: '/Test/Btn',
+    component: () => import('@/pages/Test/index.vue'),
+    children: [
+      { name: 'Test/Btn', path: '/Test/Btn', component: () => import('@/pages/Test/Btn/index.vue') },
+      { name: 'Test/Popup', path: '/Test/Popup', component: () => import('@/pages/Test/Popup/index.vue') },
+    ],
+  },
+  {
+    name: 'Form',
+    path: '/Form',
+    redirect: '/Form/Btn',
+    component: () => import('@/pages/Form/index.vue'),
+    children: [
+      { name: 'Form/Control', path: '/Form/Control', component: () => import('@/pages/Form/Control/index.vue') },
+    ],
+  },
+  {
+    name: 'User',
+    path: '/User',
+    redirect: '/User/User',
+    component: () => import('@/pages/User/index.vue'),
+    children: [
+      { name: 'User/User', path: '/User/User', component: () => import('@/pages/User/User/index.vue') },
+      {
+        name: 'User/UserProfile',
+        path: '/User/UserProfile',
+        component: () => import('@/pages/User/UserProfile/index.vue'),
+      },
+    ],
   },
 ];
