2a6e703dab0116356144ee5a7f36fcaec6c38021
Dylan Zhang <987883389@qq.com>
2023-09-29 06:47:12 +0800

    refactor: route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { beforeEach, routes } from './route';
-import { buildRoute } from './tools';
 
 const { debug } = useLog({ module: 'SrcRouter', color: 'blue' });
 
@@ -8,7 +7,7 @@ debug('使用 History 模式');
 debug('使用 @/resources/routes 初步构建路由');
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: buildRoute(routes),
+  routes: routes,
 });
 
 debug('使用 @/resources/routes 注册 beforeEach');
