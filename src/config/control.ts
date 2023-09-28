c4228864bdbea93918b7d45e8d5e96e648cf4e9b
Dylan Zhang <987883389@qq.com>
2023-09-28 16:01:44 +0800

    refactor: input to string

---
+++ src/config/control.ts
@@ -0,0 +1,52 @@
+import Hidden from '@/components/Control/Hidden/index.vue';
+import Text from '@/components/Control/Text/index.vue';
+import Textarea from '@/components/Control/Textarea/index.vue';
+import String from '@/components/Control/String/index.vue';
+import Number from '@/components/Control/Number/index.vue';
+import Password from '@/components/Control/Password/index.vue';
+import Date from '@/components/Control/Date/index.vue';
+import Time from '@/components/Control/Time/index.vue';
+import Datetime from '@/components/Control/Datetime/index.vue';
+import Week from '@/components/Control/Week/index.vue';
+import Month from '@/components/Control/Month/index.vue';
+import File from '@/components/Control/File/index.vue';
+import Range from '@/components/Control/Range/index.vue';
+import Color from '@/components/Control/Color/index.vue';
+import Checkbox from '@/components/Control/Checkbox/index.vue';
+import Radio from '@/components/Control/Radio/index.vue';
+import Select from '@/components/Control/Select/index.vue';
+import SelectMul from '@/components/Control/SelectMul/index.vue';
+
+import SqlField from '@/components/Control/SqlField/index.vue';
+import Json from '@/components/Control/Json/index.vue';
+
+const build = component => option => {
+  option.component = () => component;
+  return option;
+};
+
+export const Control = {
+  Base: {
+    Hidden: build(Hidden),
+    Text: build(Text),
+    Textarea: build(Textarea),
+    String: build(String),
+    Number: build(Number),
+    Password: build(Password),
+    Date: build(Date),
+    Time: build(Time),
+    Datetime: build(Datetime),
+    Week: build(Week),
+    Month: build(Month),
+    File: build(File),
+    Range: build(Range),
+    Color: build(Color),
+    Checkbox: build(Checkbox),
+    Radio: build(Radio),
+    Select: build(Select),
+    SelectMul: build(SelectMul),
+
+    SqlField: build(SqlField),
+    Json: build(Json),
+  },
+} as const;
