1676babd2fc5a5db95be9f1936e9d629a27e1881
Dylan Zhang <987883389@qq.com>
2023-09-28 14:18:18 +0800

    fix: useComponentState

--- src/hooks/useComponentState/index.ts
+++ src/hooks/useComponentState/index.ts
@@ -3,7 +3,7 @@ import type { DzViewStateProps } from '@/types/dz-view';
 export const useComponentState = ({ props, emit }) => {
   const getState = (): DzViewStateProps => props.state;
   const setState = (state: DzViewStateProps) => {
-    emit('update:state', props.bindId, state);
+    emit('update:state', props.code, state);
   };
 
   return { setState, getState };
