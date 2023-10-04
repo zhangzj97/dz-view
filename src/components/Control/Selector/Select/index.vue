dea004a6598375ba68e3b96c3630fa55e4f27de6
Dylan Zhang <987883389@qq.com>
2023-10-04 15:57:45 +0800

    refactor: select

---
+++ src/components/Control/Selector/Select/index.vue
@@ -0,0 +1,125 @@
+<script setup lang="ts">
+defineOptions({ name: 'ControlSelect' });
+
+import type { ControlProps, ControlEmits } from '@/types/dz-view';
+const props = withDefaults(defineProps<ControlProps<{}>>(), {});
+const emits = defineEmits<ControlEmits>();
+
+const { is, el, methods, events } = usePluginControl({ props, emits });
+
+const getValue = (): string => (!is.Empty(props.value) ? props.value : '');
+const setValue = (value: unknown) => {
+  let newValue = null;
+  if (is.String(value) || is.Number(value) || is.Boolean(value)) {
+    newValue = String(value);
+  }
+  emits('update:value', newValue);
+  setCheck(newValue);
+};
+
+const { serviceStore, setService, checkStore, setCheck } = useControlService({ props, emits });
+
+const getService = () => {
+  return serviceStore;
+};
+
+defineExpose({ ...methods, getValue, setValue, getService });
+
+onMounted(() => {
+  setValue(null);
+  setService([
+    { id: '1', title: 't1', data: { id: 1, name: 't1' } },
+    { id: '2', title: 't2', data: { id: 2, name: 't2' } },
+  ]);
+});
+
+const check = (value: string) => {
+  const newValue = checkStore.value === value ? null : value;
+  setValue(newValue);
+};
+
+const getValueTitle = computed(() => {
+  const value = checkStore.value;
+  if (!value) return null;
+  if (!getService().map[value]) return null;
+  return getService().map[value].title;
+});
+
+const test1 = () => {
+  // setInterval(() => {
+  const length = Math.random() * 10;
+  setService(Array.from({ length }).map((i, index) => ({ id: String(index), title: index + 'p' })));
+  // }, 1000);
+};
+
+const test2 = () => {
+  const id = String(Date.now());
+  setValue(id);
+};
+
+const isExistOption = computed(() => {
+  const value = checkStore.value;
+  if (!value) return true;
+
+  if (!getService().map[value]) return false;
+  return true;
+});
+</script>
+
+<template>
+  <v s="w-grow h-fit" w="gap-1" col>
+    <dz-popover :payload="{ position: 'bl', trigger: 'hover' }">
+      <textarea
+        ref="el"
+        :class="[
+          'w-grow h-8',
+          'dz-plugin-control-input',
+          payload.error && 'dz-plugin-control-input--error',
+          payload.disabled && 'dz-plugin-control-input--disabled',
+          'focus:outline-none',
+          'pr-8',
+        ]"
+        :style="{
+          cursor: 'pointer',
+        }"
+        type="text"
+        :readonly="true"
+        :disabled="payload.disabled"
+        :value="getValueTitle"
+        :placeholder="payload.placeholder || '请选择'"
+        @input="events.onInput"
+        @focus="events.onFocus"
+        @blur="events.onBlur"
+      ></textarea>
+
+      <template #body>
+        <v s="w-grow h-fit" w="gap-1 p-2 min-w-[256px]" grid>
+          <template v-for="(item, index) of serviceStore.list" :key="index">
+            <dz-btn
+              :title="item.title"
+              :payload="{ type: getValue() === item.id ? 'primary' : 'outline' }"
+              @click="check(item.id)"
+            />
+          </template>
+        </v>
+      </template>
+    </dz-popover>
+
+    <v s="w-grow h-fit">
+      <v-space s="w-grow h-fit" />
+      <dz-popover v-if="!isExistOption" :payload="{ tooltip: `[不存在选项] value : ${getValue()}` }">
+        <dz-btn :class="['text-red-500', 'opacity-50 scale-90']" icon="mdi:alert" @click="methods.reset" />
+      </dz-popover>
+
+      <dz-btn
+        :class="[getValue() ? 'opacity-50 scale-90' : 'opacity-0 scale-0']"
+        icon="mdi:close-circle-outline"
+        @click="methods.reset"
+      />
+
+      <dz-btn :class="['scale-90']" icon="mdi:refresh" @click="methods.reset" />
+      <dz-btn :class="['scale-90']" icon="mdi:dice-1-outline" @click="test1" />
+      <dz-btn :class="['scale-90']" icon="mdi:dice-2-outline" @click="test2" />
+    </v>
+  </v>
+</template>
