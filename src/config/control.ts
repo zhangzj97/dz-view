8c4bb5cfc35c62c71021d7a32da2b1e109be4f7d
Dylan Zhang <987883389@qq.com>
2023-10-05 19:33:59 +0800

    refactor: picker

--- src/config/control.ts
+++ src/config/control.ts
@@ -5,21 +5,26 @@ import String from '@/components/Control/Input/String/index.vue';
 import Number from '@/components/Control/Input/Number/index.vue';
 import Password from '@/components/Control/Input/Password/index.vue';
 
-import Date from '@/components/Control/Date/index.vue';
-import Time from '@/components/Control/Time/index.vue';
-import Datetime from '@/components/Control/Datetime/index.vue';
-import Week from '@/components/Control/Week/index.vue';
-import Month from '@/components/Control/Month/index.vue';
+import Date from '@/components/Control/TimePicker/Date/index.vue';
+import Time from '@/components/Control/TimePicker/Time/index.vue';
+import Datetime from '@/components/Control/TimePicker/Datetime/index.vue';
+import Week from '@/components/Control/TimePicker/Week/index.vue';
+import Month from '@/components/Control/TimePicker/Month/index.vue';
+
+import Picker from '@/components/Control/Picker/Picker/index.vue';
+import PickerList from '@/components/Control/Picker/PickerList/index.vue';
+import PopPicker from '@/components/Control/Picker/PopPicker/index.vue';
+import PopPickerList from '@/components/Control/Picker/PopPickerList/index.vue';
+
 import File from '@/components/Control/File/index.vue';
 import Range from '@/components/Control/Range/index.vue';
 import Color from '@/components/Control/Color/index.vue';
-import Checkbox from '@/components/Control/Checkbox/index.vue';
-import Radio from '@/components/Control/Radio/index.vue';
-import Select from '@/components/Control/Select/index.vue';
-import SelectMul from '@/components/Control/SelectMul/index.vue';
+// import Checkbox from '@/components/Control/Checkbox/index.vue';
+// import Radio from '@/components/Control/Radio/index.vue';
+// import SelectMul from '@/components/Control/SelectMul/index.vue';
 
-import SqlField from '@/components/Control/SqlField/index.vue';
-import Json from '@/components/Control/Json/index.vue';
+// import SqlField from '@/components/Control/SqlField/index.vue';
+import Json from '@/components/Control/Other/Json/index.vue';
 
 const build = component => payload => {
   return { ...payload, component: () => component };
@@ -41,12 +46,17 @@ export const Control = {
     File: build(File),
     Range: build(Range),
     Color: build(Color),
-    Checkbox: build(Checkbox),
-    Radio: build(Radio),
-    Select: build(Select),
-    SelectMul: build(SelectMul),
+    Picker: build(Picker),
+    PickerList: build(PickerList),
+    PopPicker: build(PopPicker),
+    PopPickerList: build(PopPickerList),
+  },
 
-    SqlField: build(SqlField),
+  Json: {
     Json: build(Json),
   },
+
+  // Sql: {
+  //   SqlField: build(SqlField),
+  // },
 } as const;
---
+++ src/pages/Form/Control/index.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+const { bind, Control } = useBind();
+</script>
+
+<template>
+  <v s="w-grow h-grow" col>
+    <v s="w-grow h-fit">
+      <dz-btn title="i1" v-bind="bind('i1', {})" />
+      <dz-btn title="i1 disabled" @click="bind('i1').setPayload({ disabled: !bind('i1').getPayload().disabled })" />
+      <dz-btn title="i1 loading" @click="bind('i1').setPayload({ loading: !bind('i1').getPayload().loading })" />
+    </v>
+    <v-space s="w-grow h-2" class="bg-red-500" />
+
+    <v s="w-grow h-fit">
+      <dz-modal v-bind="bind('modal', {})"> <dz-btn title="btn" /> </dz-modal>
+      <dz-drawer v-bind="bind('drawer', {})"> <dz-btn title="btn" /> </dz-drawer>
+
+      <dz-btn title="modal" @click="bind('modal').setPayload({ visible: !bind('modal').getPayload().visible })" />
+      <dz-btn title="drawer" @click="bind('drawer').setPayload({ visible: !bind('drawer').getPayload().visible })" />
+    </v>
+    <v-space s="w-grow h-2" class="bg-red-500" />
+
+    <v s="w-grow h-fit">
+      <dz-popover v-bind="bind('p1', {})">
+        <dz-btn title="p1" />
+        <template #body> 123</template>
+      </dz-popover>
+      <dz-popover v-bind="bind('p2', { trigger: 'click' })">
+        <dz-btn title="p2" />
+        <template #body> 123</template>
+      </dz-popover>
+      <dz-popover v-bind="bind('p3', { tooltip: '0000' })">
+        <dz-btn title="p3" />
+      </dz-popover>
+      <dz-popover v-bind="bind('p4', { confirm: '0000' })">
+        <dz-btn title="p4" />
+      </dz-popover>
+
+      <dz-btn title="p1" @click="bind('p1').setPayload({ visible: !bind('p1').getPayload().visible })" />
+      <dz-btn title="p2" @click="bind('p2').setPayload({ visible: !bind('p2').getPayload().visible })" />
+      <dz-btn title="p3" @click="bind('p3').setPayload({ visible: !bind('p3').getPayload().visible })" />
+      <dz-btn title="p4" @click="bind('p4').setPayload({ visible: !bind('p4').getPayload().visible })" />
+    </v>
+    <v-space s="w-grow h-2" class="bg-red-500" />
+
+    <v s="w-grow h-fit" grid w="px-4">
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Picker1', Control.Base.Picker({ test: true }))" />
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Picker2', Control.Base.PickerList({ test: true }))" />
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Picker3', Control.Base.PopPicker({ test: true }))" />
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Picker4', Control.Base.PopPickerList({ test: true }))" />
+    </v>
+    <v-space s="w-grow h-2" class="bg-red-500" />
+
+    <v s="w-grow h-fit" grid w="px-4">
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Date', Control.Base.Date({ test: true }))" />
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Datetime', Control.Base.Datetime({ test: true }))" />
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Week', Control.Base.Week({ test: true }))" />
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Month', Control.Base.Month({ test: true }))" />
+    </v>
+    <v-space s="w-grow h-2" class="bg-red-500" />
+
+    <v s="w-grow h-fit" grid w="px-4">
+      <dz-control s="w-1/4 h-fit" v-bind="bind('String', Control.Base.String({ test: true }))" />
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Number', Control.Base.Number({ test: true }))" />
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Password', Control.Base.Password({ test: true }))" />
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Textarea', Control.Base.Textarea({ test: true }))" />
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Text', Control.Base.Text({ test: true }))" />
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Hidden', Control.Base.Hidden({ test: true }))" />
+    </v>
+    <v-space s="w-grow h-2" class="bg-red-500" />
+
+    <v s="w-grow h-fit" grid w="px-4">
+      <dz-control s="w-1/4 h-fit" v-bind="bind('JSON', Control.Json.Json({ test: true }))" />
+    </v>
+    <v-space s="w-grow h-2" class="bg-red-500" />
+  </v>
+</template>
