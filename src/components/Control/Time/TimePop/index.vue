ab247222d201d26290897c63762b60e512a33053
Dylan Zhang <987883389@qq.com>
2023-10-08 14:55:00 +0800

    refactor: time

--- src/components/Control/Time/TimePop/index.vue
+++ src/components/Control/Time/TimePop/index.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import TriggerText from '../../TriggerText.vue';
+import CacheText from '../../CacheText.vue';
 import dayjs from 'dayjs';
+
+import { useDatePicker } from './useDatePicker';
 
 import { DatePicker } from '@arco-design/web-vue';
 import '@arco-design/web-vue/dist/arco.css';
@@ -7,102 +11,115 @@ import '@arco-design/web-vue/dist/arco.css';
 defineOptions({ name: 'ControlDatetime' });
 
 import type { ControlProps, ControlEmits } from '@/types/dz-view';
+import { document } from 'postcss';
 const props = withDefaults(defineProps<ControlProps<{}>>(), {});
 const emits = defineEmits<ControlEmits>();
 
-const { is, el, methods, events } = useControlBase({ props, emits });
+const { is } = useValidate();
+const { el, methods, events } = useControlBase({ props, emits });
+const { cache, handleCache } = useControlCache({ props, emits });
+const { service, handleService } = useControlService({ props, emits });
 
-const getValue = (): string | string[] | null | any => {
-  return props.value;
-};
-const setValue = (value: unknown) => {
-  let newValue = null;
-  if (is.Number(value) || is.Number(Number(value))) {
-    newValue = value;
-  }
-  emits('update:value', newValue);
-};
+defineExpose({ ...methods });
 
-defineExpose({ ...methods, getValue, setValue });
+const { datePickerEvents } = useDatePicker();
 
 onMounted(() => {
-  if (is.Undefined(getValue())) {
-    setValue(null);
-  }
+  emits('update:value', props.payload.defaultValue ?? null);
+  handleService.set([
+    { id: 1, title: '现在', data: { value: () => dayjs() } },
+    { id: 2, title: 'a week later', data: { value: () => dayjs().add(1, 'week') } },
+  ]);
 });
 
-const reset = () => {
-  emits('update:value', null);
+watch(
+  () => props.value,
+  v => handleCache.set([v])
+);
+
+const computedTriggerText = computed(() => {
+  const v = props.value;
+  return v ? dayjs(Number(v)).format('YYYY-MM-DD HH:mm:ss') : '';
+});
+
+const computedCacheText = computed(() => {
+  const v = cache.value[0];
+  return v ? dayjs(Number(v)).format('YYYY-MM-DD HH:mm:ss') : '';
+});
+
+const computedServiceList = computed(() => {
+  const func = ({ id, title, data }: any) => ({ id, label: title, value: data.value });
+  return service.list.map(func);
+});
+
+const datePickerEventsOk = (_dateStr: string, date: Date) => {
+  const v = String(date.valueOf());
+  emits('update:value', v);
+};
+const datePickerEventsSelect = (_dateStr: string, date: Date) => {
+  const v = String(date.valueOf());
+  handleCache.set([v]);
+};
+const datePickerEventsSelectShortcut = (shortcut: any) => {
+  const v = String(dayjs(shortcut.value()).valueOf());
+  emits('update:value', v);
 };
 
-const pickerValue = value => {
-  emits('update:value', String(dayjs(value).valueOf()));
+const ok = () => {
+  const v = cache.value[0];
+  emits('update:value', v);
 };
-
-const valueTime = computed(() => {
-  if (is.Empty(getValue())) return null;
-  return dayjs(Number(getValue()));
-});
-
-const triggerText = computed(() => {
-  if (is.Empty(getValue())) return '';
-  return dayjs(Number(getValue())).format('YYYY-MM-DD HH:mm:ss');
-});
 </script>
 
 <template>
-  <v s="w-grow h-fit" class="group">
-    <DatePicker
-      ref="el"
-      :error="payload.error"
-      :allowClear="false"
-      shortcutsPosition="right"
-      :pickerValue="valueTime"
-      :shortcuts="[
-        {
-          label: '现在',
-          value: () => dayjs(),
-        },
-      ]"
-      :showTime="!payload.hiddenTime"
-      @update:modelValue="pickerValue"
-    >
-      <input
-        :class="[
-          'w-grow h-8',
-          'dz-plugin-control-input',
-          payload.error && 'dz-plugin-control-input--error',
-          payload.disabled && 'dz-plugin-control-input--disabled',
-          'focus:outline-none',
-          'pr-8',
-        ]"
-        type="text"
-        :disabled="payload.disabled"
-        :value="triggerText"
-        :placeholder="payload.placeholder"
-        @input="events.onInput"
-        @focus="events.onFocus"
-        @blur="events.onBlur"
-      />
+  <dz-popover :payload="{ embed: false, position: 'bl' }">
+    <TriggerText
+      :payload="payload"
+      :text="computedTriggerText"
+      :value="value"
+      :warning="null"
+      @reset="methods.reset"
+      @undo="methods.undo"
+    />
 
-      <template #suffix-icon>
-        <v-space />
-      </template>
-    </DatePicker>
+    <template #body>
+      <v s="w-grow h-fit" w="py-2 gap-1" col>
+        <v s="w-grow h-fit">
+          <CacheText :payload="payload" :value="computedCacheText" />
+          <dz-popover :payload="{ tooltip: '自定义选择时间后 需要点击确认' }">
+            <dz-btn v-if="!value && !cache.value[0]" icon="mdi:information-outline" />
+            <dz-btn v-else-if="value !== cache.value[0]" title="确认" :payload="{ type: 'primary' }" @click="ok" />
+            <dz-btn v-else icon="mdi:check" />
+          </dz-popover>
+        </v>
 
-    <v s="w-fit h-fit" class="absolute top-0 right-0">
-      <dz-btn
-        :class="['scale-90', 'group-hover:opacity-0', getValue() ? 'opacity-0' : 'opacity-90']"
-        :payload="{ type: 'icon' }"
-        icon="mdi:calendar-month-outline"
-      />
-    </v>
-    <v s="w-fit h-fit" class="absolute top-0 right-0">
-      <dz-btn
-        :class="['scale-90', 'group-hover:opacity-80', getValue() ? 'opacity-50' : 'opacity-0']"
-        icon="mdi:close-circle-outline"
-        @click="reset"
-      />
-    </v>
-  </v>
+        <DatePicker
+          ref="el"
+          :showTime="!payload.hiddenTime"
+          :allowClear="false"
+          :popupVisible="true /* bug : 时间滚动效果 */"
+          :hide-trigger="true"
+          shortcutsPosition="right"
+          :shortcuts="computedServiceList"
+          :modelValue="Number(value)"
+          @update:pickerValue="datePickerEvents.updatePickerValue"
+          @update:modelValue="datePickerEvents.updateModelValue"
+          @change="datePickerEvents.change"
+          @select="datePickerEventsSelect"
+          @popupVisibleChange="datePickerEvents.popupVisibleChange"
+          @ok="datePickerEventsOk"
+          @clear="datePickerEvents.clear"
+          @selectShortcut="datePickerEventsSelectShortcut"
+          @pickerValueChange="datePickerEvents.pickerValueChange"
+        >
+        </DatePicker>
+      </v>
+    </template>
+  </dz-popover>
 </template>
+
+<style lang="scss" scoped>
+:deep(.arco-picker-panel-wrapper > .arco-picker-footer) {
+  height: 0px;
+}
+</style>
