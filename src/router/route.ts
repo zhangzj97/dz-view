120c00970f4f8b937d3535101115134aaffd984e
Dylan Zhang <987883389@qq.com>
2023-10-10 06:59:50 +0800

    refactor: auto route

--- src/router/route.ts
+++ src/router/route.ts
@@ -1,5 +1,33 @@
 export { beforeEnter, beforeEach } from './guards';
 
+const files1 = import.meta.glob('@/pages/*/index.vue');
+const files2 = import.meta.glob('@/pages/*/*/index.vue');
+
+const routeMap1 = {};
+Object.entries(files1).forEach(([key, value]) => {
+  const key2 = key.replace(/\/src\/pages\/|\/index.vue/g, '');
+  routeMap1[key2] = {
+    name: key2,
+    path: `/${key2}`,
+    redirect: '', // todo
+    component: value,
+    children: [],
+  };
+});
+
+Object.entries(files2).forEach(([key, value]) => {
+  const key3 = key.replace(/\/src\/pages\/|\/index.vue/g, '');
+  const key2 = key3.replace(/\/.*/, '');
+  if (!routeMap1[key2]) return;
+  routeMap1[key2].children.push({
+    name: key3,
+    path: `/${key3}`,
+    component: value,
+  });
+});
+
+console.log({ files1, files2 });
+
 export const routes = [
   {
     name: 'Root',
@@ -18,60 +46,7 @@ export const routes = [
     path: '/:pathMatch(.*)',
     redirect: '/404',
   },
-  {
-    name: 'Tree',
-    path: '/Tree',
-    redirect: '/Tree/Tree100',
-    component: () => import('@/pages/Tree/index.vue'),
-    children: [
-      { name: 'Tree/Tree100', path: '/Tree/Tree100', component: () => import('@/pages/Tree/Tree100/index.vue') },
-      { name: 'Tree/Tree200', path: '/Tree/Tree200', component: () => import('@/pages/Tree/Tree200/index.vue') },
-      { name: 'Tree/Tree210', path: '/Tree/Tree210', component: () => import('@/pages/Tree/Tree210/index.vue') },
-      { name: 'Tree/Tree220', path: '/Tree/Tree220', component: () => import('@/pages/Tree/Tree220/index.vue') },
-      { name: 'Tree/Tree300', path: '/Tree/Tree300', component: () => import('@/pages/Tree/Tree300/index.vue') },
-      { name: 'Tree/Tree310', path: '/Tree/Tree310', component: () => import('@/pages/Tree/Tree310/index.vue') },
-      { name: 'Tree/Tree320', path: '/Tree/Tree320', component: () => import('@/pages/Tree/Tree320/index.vue') },
-      { name: 'Tree/Tree311', path: '/Tree/Tree311', component: () => import('@/pages/Tree/Tree311/index.vue') },
-      { name: 'Tree/Tree312', path: '/Tree/Tree312', component: () => import('@/pages/Tree/Tree312/index.vue') },
-      { name: 'Tree/Tree321', path: '/Tree/Tree321', component: () => import('@/pages/Tree/Tree321/index.vue') },
-      { name: 'Tree/Tree322', path: '/Tree/Tree322', component: () => import('@/pages/Tree/Tree322/index.vue') },
-    ],
-  },
 
-  {
-    name: 'Test',
-    path: '/Test',
-    redirect: '/Test/Btn',
-    component: () => import('@/pages/Test/index.vue'),
-    children: [
-      { name: 'Test/Btn', path: '/Test/Btn', component: () => import('@/pages/Test/Btn/index.vue') },
-      { name: 'Test/Popup', path: '/Test/Popup', component: () => import('@/pages/Test/Popup/index.vue') },
-    ],
-  },
-  {
-    name: 'Form',
-    path: '/Form',
-    redirect: '/Form/Btn',
-    component: () => import('@/pages/Form/index.vue'),
-    children: [
-      { name: 'Form/Control', path: '/Form/Control', component: () => import('@/pages/Form/Control/index.vue') },
-      { name: 'Form/Time', path: '/Form/Time', component: () => import('@/pages/Form/Time/index.vue') },
-      { name: 'Form/String', path: '/Form/String', component: () => import('@/pages/Form/String/index.vue') },
-      { name: 'Form/Picker', path: '/Form/Picker', component: () => import('@/pages/Form/Picker/index.vue') },
-    ],
-  },
-  {
-    name: 'User',
-    path: '/User',
-    redirect: '/User/User',
-    component: () => import('@/pages/User/index.vue'),
-    children: [
-      { name: 'User/User', path: '/User/User', component: () => import('@/pages/User/User/index.vue') },
-      {
-        name: 'User/UserProfile',
-        path: '/User/UserProfile',
-        component: () => import('@/pages/User/UserProfile/index.vue'),
-      },
-    ],
-  },
+  ...Object.values(routeMap1),
 ];
+console.log([...Object.values(routeMap1)]);
