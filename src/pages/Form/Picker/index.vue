744450ca8518512a3ae66354d6649e66d5d64c50
Dylan Zhang <987883389@qq.com>
2023-10-09 19:36:32 +0800

    refactor: test

--- src/pages/Form/Picker/index.vue
+++ src/pages/Form/Picker/index.vue
@@ -6,13 +6,15 @@ const { bind, Control } = useBind();
   <v s="w-grow h-grow" col>
     <v s="w-grow h-fit" grid w="px-4">
       <dz-btn
-        :title="'1'"
+        :title="'Picker2'"
         @click="
           bind('Picker2').setValue(['1']);
           bind('Picker2').setPayload({ defaultValue: ['1', '2', '3'] });
         "
       />
-      <dz-btn :title="'2'" @click="bind('String').setValue(String(Date.now()))" />
+    </v>
+
+    <v s="w-grow h-fit" grid w="px-4">
       <dz-control s="w-1/4 h-fit" v-bind="bind('Picker1', Control.Base.Picker({ test: true }))" />
       <dz-control s="w-1/4 h-fit" v-bind="bind('Picker2', Control.Base.PickerList({ test: true }))" />
       <dz-control s="w-1/4 h-fit" v-bind="bind('Picker3', Control.Base.PickerPanel({ test: true }))" />
--- src/pages/Form/String/index.vue
+++ src/pages/Form/String/index.vue
@@ -1,23 +1,39 @@
 <script setup lang="ts">
-const { bind, Control } = useBind();
+const { bind, Control } = useBind({ payload: { test: true } });
 </script>
 
 <template>
   <v s="w-grow h-grow" col>
     <v s="w-grow h-fit" grid w="px-4">
-      <!-- <dz-btn
-        :title="'1'"
+      <dz-btn
+        :title="'String'"
         @click="
-          bind('String').setValue(String(123));
-          bind('String').setPayload({ defaultValue: String(Date.now()) });
+          bind('String').setValue([String(123)]);
+          bind('String').setPayload({ defaultValue: ['1232'] });
         "
       />
-      <dz-btn :title="'2'" @click="bind('String').setValue(String(Date.now()))" /> -->
-      <dz-control s="w-1/4 h-fit" v-bind="bind('String', Control.Base.String({ test: true }))" />
-      <dz-control s="w-1/4 h-fit" v-bind="bind('Password', Control.Base.Password({ test: true }))" />
-      <dz-control s="w-1/4 h-fit" v-bind="bind('Textarea', Control.Base.Textarea({ test: true }))" />
-      <dz-control s="w-1/4 h-fit" v-bind="bind('Text', Control.Base.Text({ test: true }))" />
-      <dz-control s="w-1/4 h-fit" v-bind="bind('Number', Control.Base.Number({ test: true }))" />
+      <dz-btn
+        :title="'Number'"
+        @click="
+          bind('Number').setValue([String(123)]);
+          bind('Number').setPayload({ defaultValue: ['1232'] });
+        "
+      />
+      <dz-btn
+        :title="'Textarea'"
+        @click="
+          bind('Textarea').setValue([String(123)]);
+          bind('Textarea').setPayload({ defaultValue: ['1232'] });
+        "
+      />
+    </v>
+
+    <v s="w-grow h-fit" grid w="px-4">
+      <dz-control s="w-1/4 h-fit" v-bind="bind('String', Control.Base.String())" />
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Password', Control.Base.Password())" />
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Textarea', Control.Base.Textarea())" />
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Text', Control.Base.Text())" />
+      <dz-control s="w-1/4 h-fit" v-bind="bind('Number', Control.Base.Number())" />
     </v>
   </v>
 </template>
